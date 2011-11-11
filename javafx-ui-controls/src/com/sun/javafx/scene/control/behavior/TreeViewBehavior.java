6b8da8bc6b82af6dc7e3f75bac3d137d1cf62e46
Jonathan Giles <jgiles@openjdk.org>
2011-11-11 15:47:50 +1000

    RT-17833: [TreeView] Selection disappear somewhere, if press "left" arrow key

--- javafx-ui-controls/src/com/sun/javafx/scene/control/behavior/TreeViewBehavior.java
+++ javafx-ui-controls/src/com/sun/javafx/scene/control/behavior/TreeViewBehavior.java
@@ -467,8 +467,16 @@ public class TreeViewBehavior<T> extends BehaviorBase<TreeView<T>> {
         
         // Fix for RT-17233 where we could hide all items in a tree with no visible
         // root by pressing the left-arrow key too many times
-        if (! getControl().isShowRoot() && ! treeItem.isExpanded() && root.equals(treeItem.getParent())) return;
-
+        if (! getControl().isShowRoot() && ! treeItem.isExpanded() && root.equals(treeItem.getParent())) {
+            return;
+        }
+        
+        // Fix for RT-17833 where the selection highlight could disappear unexpectedly from
+        // the root node in certain circumstances
+        if (root.equals(treeItem) && (! root.isExpanded() || root.getChildren().isEmpty())) {
+            return;
+        }
+        
         // If we're on a leaf or the branch is not expanded, move up to the parent,
         // otherwise collapse the branch.
         if (treeItem.isLeaf() || ! treeItem.isExpanded()) {
