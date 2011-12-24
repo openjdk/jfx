7feab0ccb3f7716204cd3c85a479944f796b4b97
Richard Bair <rbair@openjdk.org>
2011-12-23 17:49:29 -0800

    Fixed JavaDocs for WorkerStateEvent, corrected type param on constructor

--- javafx-concurrent/src/javafx/concurrent/WorkerStateEvent.java
+++ javafx-concurrent/src/javafx/concurrent/WorkerStateEvent.java
@@ -7,12 +7,10 @@ package javafx.concurrent;
 import javafx.event.Event;
 import javafx.event.EventTarget;
 import javafx.event.EventType;
-import javafx.scene.input.InputEvent;
-import javafx.scene.input.MouseEvent;
 
 /**
- *
- * @author Richard
+ * An event which occurs whenever the state changes on a Worker. Both
+ * {@link Task} and {@link Service} support listening to state events.
  */
 public class WorkerStateEvent extends Event {
     /**
@@ -63,7 +61,15 @@ public class WorkerStateEvent extends Event {
     public static final EventType<WorkerStateEvent> WORKER_STATE_FAILED =
             new EventType<WorkerStateEvent>(WorkerStateEvent.ANY, "WORKER_STATE_FAILED");
 
-    public WorkerStateEvent(Worker worker, EventType<? extends Event> eventType) {
+    /**
+     * Create a new WorkerStateEvent. Specify the worker and the event type.
+     *
+     * @param worker The Worker which is firing the event. The Worker really
+     *               should be an EventTarget, otherwise the EventTarget
+     *               for the event will be null.
+     * @param eventType The type of event. This should not be null.
+     */
+    public WorkerStateEvent(Worker worker, EventType<? extends WorkerStateEvent> eventType) {
         super(worker, worker instanceof EventTarget ? (EventTarget) worker : null, eventType);
     }
 
