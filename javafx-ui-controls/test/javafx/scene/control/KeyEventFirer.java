a80b123a6761e322dd14d27f66e94d9c8b341480
Jonathan Giles <jgiles@openjdk.org>
2012-01-04 16:00:52 +1000

    [TEST ONLY] Added simple test API that allows for easy keyboard event testing

---
+++ javafx-ui-controls/test/javafx/scene/control/KeyEventFirer.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ */
+package javafx.scene.control;
+
+import java.util.Arrays;
+import java.util.List;
+import javafx.event.EventType;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.event.Event;
+import javafx.event.EventTarget;
+
+
+public class KeyEventFirer {
+    
+    private final EventTarget target;
+    
+    public KeyEventFirer(EventTarget target) {
+        this.target = target;
+    }
+    
+    public void doUpArrowPress(KeyModifier... modifiers) {
+        doKeyPress(KeyCode.UP, modifiers);
+    }
+    
+    public void doDownArrowPress(KeyModifier... modifiers) {
+        doKeyPress(KeyCode.DOWN, modifiers);
+    }
+    
+    public void doLeftArrowPress(KeyModifier... modifiers) {
+        doKeyPress(KeyCode.LEFT, modifiers);
+    }
+    
+    public void doRightArrowPress(KeyModifier... modifiers) {
+        doKeyPress(KeyCode.RIGHT, modifiers);
+    }
+    
+    public void doKeyPress(KeyCode keyCode, KeyModifier... modifiers) {
+        fireEvents(createMirroredEvents(keyCode, modifiers));
+    }
+    
+    private void fireEvents(KeyEvent... events) {
+        for (KeyEvent evt : events) {
+            Event.fireEvent(target, evt);
+        }
+    }
+    
+    private KeyEvent[] createMirroredEvents(KeyCode keyCode, KeyModifier... modifiers) {
+        KeyEvent[] events = new KeyEvent[2];
+        events[0] = createEvent(keyCode, KeyEvent.KEY_PRESSED, modifiers);
+        events[1] = createEvent(keyCode, KeyEvent.KEY_RELEASED, modifiers);
+        return events;
+    }
+    
+    private KeyEvent createEvent(KeyCode keyCode, EventType<KeyEvent> evtType, KeyModifier... modifiers) {
+        List<KeyModifier> modifiersList = Arrays.asList(modifiers);
+        
+        return KeyEvent.impl_keyEvent(
+                target, // EventTarget
+                null,     // Character (unused unless KeyCode == KEY_TYPED
+                null,     // text
+                keyCode.impl_getCode(), // KeyCode
+                modifiersList.contains(KeyModifier.SHIFT),    // shiftDown
+                modifiersList.contains(KeyModifier.CTRL),     // ctrlDown
+                modifiersList.contains(KeyModifier.ALT),      // altDown
+                modifiersList.contains(KeyModifier.META),     // metaData
+                evtType);  // eventType
+    }
+}
---
+++ javafx-ui-controls/test/javafx/scene/control/ListViewKeyInputTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ */
+package javafx.scene.control;
+
+import static org.junit.Assert.*;
+
+import javafx.scene.control.KeyEventFirer;
+import javafx.scene.control.KeyModifier;
+
+import java.util.Arrays;
+import java.util.List;
+import javafx.event.EventType;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import org.junit.After;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListViewKeyInputTest {
+    private ListView<String> listView;
+    private MultipleSelectionModel<String> sm;
+    private FocusModel<String> fm;
+    
+    private KeyEventFirer keyboard;
+    
+    private Stage stage;
+    private Scene scene;
+    private Group group;
+    
+    @Before public void setup() {
+        listView = new ListView<String>();
+        sm = listView.getSelectionModel();
+        fm = listView.getFocusModel();
+        
+        sm.setSelectionMode(SelectionMode.MULTIPLE);
+        
+        keyboard = new KeyEventFirer(listView);
+        
+        group = new Group();
+        scene = new Scene(group);
+        
+        stage = new Stage();
+        stage.setScene(scene);
+        
+        group.getChildren().setAll(listView);
+        stage.show();
+
+        listView.getItems().setAll("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+        sm.clearAndSelect(0);
+    }
+    
+    @After public void tearDown() {
+        stage.hide();
+    }
+    
+    @Test public void testInitialState() {
+        assertTrue(sm.isSelected(0));
+        assertEquals(1, sm.getSelectedIndices().size());
+        assertEquals(1, sm.getSelectedItems().size());
+    }
+    
+    @Test public void testDownArrowChangesSelection() {
+        sm.clearAndSelect(0);
+        keyboard.doDownArrowPress();
+        assertFalse(sm.isSelected(0));
+        assertTrue(sm.isSelected(1));
+    }
+    
+    @Test public void testUpArrowChangesSelection() {
+        sm.clearAndSelect(1);
+        keyboard.doUpArrowPress();
+        assertFalse(sm.isSelected(1));
+        assertTrue(sm.isSelected(0));
+    }
+    
+    @Test public void testLeftArrowDoesNotChangeState() {
+        keyboard.doLeftArrowPress();
+        testInitialState();
+    }
+    
+    @Test public void testRightArrowDoesNotChangeState() {
+        keyboard.doRightArrowPress();
+        testInitialState();
+    }
+    
+    @Test public void testShiftDownArrowIncreasesSelection() {
+        sm.clearAndSelect(0);
+        keyboard.doDownArrowPress(KeyModifier.SHIFT);
+        assertTrue(sm.isSelected(0));
+        assertTrue(sm.isSelected(1));
+    }
+    
+    @Test public void testShiftUpArrowIncreasesSelection() {
+        sm.clearAndSelect(1);
+        keyboard.doUpArrowPress(KeyModifier.SHIFT);
+        assertTrue(sm.isSelected(0));
+        assertTrue(sm.isSelected(1));
+    }
+}
