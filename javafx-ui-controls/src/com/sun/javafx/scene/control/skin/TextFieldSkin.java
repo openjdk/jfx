57df6f9e706217cf3ebe6c35a1922c08c22e219a
Leif Samuelsson <leifs@openjdk.org>
2012-01-23 11:34:42 -0800

    Fixzed RT-19102: TextField vertical alignment is wrong

--- javafx-ui-controls/src/com/sun/javafx/scene/control/skin/TextFieldSkin.java
+++ javafx-ui-controls/src/com/sun/javafx/scene/control/skin/TextFieldSkin.java
@@ -627,19 +627,20 @@ public class TextFieldSkin extends TextInputControlSkin<TextField, TextFieldBeha
         if (textNode != null) {
             double textY;
             Insets insets = getInsets();
+            FontMetrics fm = fontMetrics.get();
             switch (getSkinnable().getAlignment().getVpos()) {
               case TOP:
-                textY = insets.getTop() + fontMetrics.get().getMaxAscent();
+                textY = insets.getTop() + fm.getMaxAscent();
                 break;
 
               case CENTER:
-                textY = (getHeight() - insets.getBottom() - insets.getTop()
-                         + fontMetrics.get().getLineHeight()) / 2;
+                textY = (insets.getTop() + fm.getMaxAscent() +
+                         getHeight() - insets.getBottom() - fm.getMaxDescent()) / 2;
                 break;
 
               case BOTTOM:
               default:
-                textY = getHeight() - insets.getBottom() - fontMetrics.get().getMaxDescent();
+                textY = getHeight() - insets.getBottom() - fm.getMaxDescent();
             }
             textNode.setY(textY);
             if (promptNode != null) {
