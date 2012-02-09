5ba193aee7fb648312869fa8577cb83887c6a7b4
Kinsley Wong <kwwong@openjdk.org>
2012-02-09 13:49:54 -0800

    RT-17899: [Toolbar] Ctrl-F5 should move focus to the window toolbar (Mac)

--- javafx-ui-controls/src/com/sun/javafx/scene/control/behavior/ToolBarBehavior.java
+++ javafx-ui-controls/src/com/sun/javafx/scene/control/behavior/ToolBarBehavior.java
@@ -25,7 +25,10 @@
 
 package com.sun.javafx.scene.control.behavior;
 
+import java.util.ArrayList;
+import java.util.List;
 import javafx.scene.control.ToolBar;
+import javafx.scene.input.KeyCode;
 
 /**
  * A Behavior implementation for ToolBars.
@@ -35,6 +38,35 @@ public class ToolBarBehavior extends BehaviorBase<ToolBar> {
 
     public ToolBarBehavior(ToolBar toolbar) {        
         super(toolbar);
+    }
+    
+    /***************************************************************************
+     *                                                                         *
+     * Key event handling                                                      *
+     *                                                                         *
+     **************************************************************************/
+    
+    private static final String CTRL_F5 = "Ctrl_F5";
+    
+    protected static final List<KeyBinding> TOOLBAR_BINDINGS = new ArrayList<KeyBinding>();
+    static {
+        TOOLBAR_BINDINGS.addAll(TRAVERSAL_BINDINGS);
+        TOOLBAR_BINDINGS.add(new KeyBinding(KeyCode.F5, CTRL_F5).ctrl());    
+    }
+    
+    @Override protected List<KeyBinding> createKeyBindings() {
+        return TOOLBAR_BINDINGS;
+    }    
+    
+    @Override protected void callAction(String name) {            
+        if (CTRL_F5.equals(name)) {
+            ToolBar toolbar = getControl();
+            if (!toolbar.getItems().isEmpty()) {
+                toolbar.getItems().get(0).requestFocus();
+            }
+        } else {
+            super.callAction(name);
+        }
     }
 }
 
