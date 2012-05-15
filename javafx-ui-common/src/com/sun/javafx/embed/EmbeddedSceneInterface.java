fb0f421fe9f4c2140909ecab432ad50916254f99
Steve Northover <snorthov@openjdk.org>
2012-05-15 17:02:41 -0400

    RT-20815: Text-like controls, ContextMenu] cm doesn't appear at text like controls

--- javafx-ui-common/src/com/sun/javafx/embed/EmbeddedSceneInterface.java
+++ javafx-ui-common/src/com/sun/javafx/embed/EmbeddedSceneInterface.java
@@ -60,7 +60,12 @@ public interface EmbeddedSceneInterface {
      * A notification about key event received by host container.
      */
     public void keyEvent(int type, int key, char[] chars, int modifiers);
-
+    
+    /*
+     * A notification about menu event received by host container.
+     */
+    public void menuEvent(int x, int y, int xAbs, int yAbs, boolean isKeyboardTrigger);
+    
     public boolean traverseOut(Direction dir);
     
     public void setDragStartListener(EmbeddedSceneDragStartListenerInterface l);
