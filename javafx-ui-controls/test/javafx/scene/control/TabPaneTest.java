4faba89ba28a96e65e66de6572d590139324520a
Lubomir Nerad <lnerad@openjdk.org>
2012-07-20 11:56:43 +0200

    Fix for RT-18561: fixed more unit tests

--- javafx-ui-controls/test/javafx/scene/control/TabPaneTest.java
+++ javafx-ui-controls/test/javafx/scene/control/TabPaneTest.java
@@ -803,6 +803,7 @@ public class TabPaneTest {
         
         stage.setScene(scene1);
         stage.show();
+        stage.requestFocus();
 
         final KeyEvent tabEvent = KeyEvent.impl_keyEvent(null, "", "", 0x09,
                                                          false, false, false, false,
