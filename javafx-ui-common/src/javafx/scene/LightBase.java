293263d09583c9a9e6b7d19c178ac8e08949367b
Kevin Rushforth <kcr@openjdk.org>
2013-02-18 14:17:44 -0800

    RT-25869: Update copyright date to 2013; fix missing or malformed copyrights

--- javafx-ui-common/src/javafx/scene/LightBase.java
+++ javafx-ui-common/src/javafx/scene/LightBase.java
@@ -22,6 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 package javafx.scene;
 
 import com.sun.javafx.collections.TrackableObservableList;
--- javafx-ui-common/src/javafx/scene/paint/PhongMaterial.java
+++ javafx-ui-common/src/javafx/scene/paint/PhongMaterial.java
@@ -22,6 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 package javafx.scene.paint;
 
 import com.sun.javafx.sg.PGPhongMaterial;
