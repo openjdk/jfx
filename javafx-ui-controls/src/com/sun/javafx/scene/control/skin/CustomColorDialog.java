38124c7d9be6d97549d3d8ffd4dfe41b8cab85d6
Alexander Kouznetsov <mrkam@openjdk.org>
2013-04-03 15:12:45 -0700

    CustomColorDialog: Fixed RT-29428 CustomColorDialog: problems with mouse

--- javafx-ui-controls/src/com/sun/javafx/scene/control/skin/CustomColorDialog.java
+++ javafx-ui-controls/src/com/sun/javafx/scene/control/skin/CustomColorDialog.java
@@ -285,6 +285,7 @@ public class CustomColorDialog extends HBox {
             colorRectIndicator = new Region();
             colorRectIndicator.setId("color-rect-indicator");
             colorRectIndicator.setManaged(false);
+            colorRectIndicator.setMouseTransparent(true);
             colorRectIndicator.setCache(true);
         
             final Pane colorRectOpacityContainer = new StackPane();
@@ -345,7 +346,7 @@ public class CustomColorDialog extends HBox {
                     new Stop(0, Color.rgb(0, 0, 0, 0)), new Stop(1, Color.rgb(0, 0, 0, 1))), 
                     CornerRadii.EMPTY, Insets.EMPTY)));
             colorRectOverlayTwo.setOnMouseDragged(rectMouseHandler);
-            colorRectOverlayTwo.setOnMouseClicked(rectMouseHandler);
+            colorRectOverlayTwo.setOnMousePressed(rectMouseHandler);
             
             Pane colorRectBlackBorder = new Pane();
             colorRectBlackBorder.setMouseTransparent(true);
@@ -358,6 +359,7 @@ public class CustomColorDialog extends HBox {
 
             colorBarIndicator = new Region();
             colorBarIndicator.setId("color-bar-indicator");
+            colorBarIndicator.setMouseTransparent(true);
             colorBarIndicator.setCache(true);
             
             colorRectIndicator.layoutXProperty().bind(sat.divide(100).multiply(colorRect.widthProperty()));
@@ -373,7 +375,7 @@ public class CustomColorDialog extends HBox {
             };
             
             colorBar.setOnMouseDragged(barMouseHandler);
-            colorBar.setOnMouseClicked(barMouseHandler);
+            colorBar.setOnMousePressed(barMouseHandler);
         
             colorBar.getChildren().setAll(colorBarIndicator);
             colorRectOpacityContainer.getChildren().setAll(colorRectHue, colorRectOverlayOne, colorRectOverlayTwo);
