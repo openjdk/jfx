f33f993f989f99d58994b56c11034e3b3b627afb
Debra Masada <dmasada@openjdk.org>
2013-04-16 16:22:21 -0700

    RT-29798 Ensemble8: Port SwingInterop to Ensemble8

---
+++ apps/samples/Ensemble8/src/samples/ensemble/samples/swing/swinginterop/SampleTableModel.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2008, 2013 Oracle and/or its affiliates.
+ * All rights reserved. Use is subject to license terms.
+ *
+ * This file is available and licensed under the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the distribution.
+ *  - Neither the name of Oracle Corporation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package ensemble.samples.swing.swinginterop;
+
+import java.util.Arrays;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.chart.BarChart;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * SampleTableModel
+ */
+public class SampleTableModel extends AbstractTableModel {
+
+    private static ObservableList<BarChart.Series> bcData;
+    private final String[] names = {"2007", "2008", "2009"};
+    private Object[][] data = {
+        {new Double(567), new Double(956), new Double(1154)},
+        {new Double(1292), new Double(1665), new Double(1927)},
+        {new Double(1292), new Double(2559), new Double(2774)}
+    };
+
+    public double getTickUnit() {
+        return 1000;
+    }
+
+    public List<String> getColumnNames() {
+        return Arrays.asList(names);
+    }
+
+    @Override
+    public int getRowCount() {
+        return data.length;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return names.length;
+    }
+
+    @Override
+    public Object getValueAt(int row, int column) {
+        return data[row][column];
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return names[column];
+    }
+
+    @Override
+    public Class getColumnClass(int column) {
+        return getValueAt(0, column).getClass();
+    }
+
+    @Override
+    public boolean isCellEditable(int row, int column) {
+        return true;
+    }
+
+    @Override
+    public void setValueAt(Object value, int row, int column) {
+        if (value instanceof Double) {
+            data[row][column] = (Double) value;
+        }
+
+        fireTableCellUpdated(row, column);
+    }
+
+    public ObservableList<BarChart.Series> getBarChartData() {
+        if (bcData == null) {
+            bcData = FXCollections.<BarChart.Series>observableArrayList();
+            for (int row = 0; row < getRowCount(); row++) {
+                ObservableList<BarChart.Data> series = FXCollections.<BarChart.Data>observableArrayList();
+                for (int column = 0; column < getColumnCount(); column++) {
+                    series.add(new BarChart.Data(getColumnName(column), getValueAt(row, column)));
+                }
+                bcData.add(new BarChart.Series(series));
+            }
+        }
+        return bcData;
+    }
+}
