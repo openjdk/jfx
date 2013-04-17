17fdd59efc074a86391659c812dc395287e799ac
Martin Sladecek <msladecek@openjdk.org>
2013-04-17 09:19:58 +0200

    [JAVADOC] RT-29803 InvalidationListener operates with Observable only but refers to ObservableValue in its javadoc

--- javafx-beans/src/javafx/beans/InvalidationListener.java
+++ javafx-beans/src/javafx/beans/InvalidationListener.java
@@ -29,32 +29,33 @@ import javafx.beans.value.ObservableValue;
 
 /**
  * An {@code InvalidationListener} is notified whenever an
- * {@link ObservableValue} becomes invalid. It can be registered and
- * unregistered with {@link ObservableValue#addListener(InvalidationListener)}
- * respectively {@link ObservableValue#removeListener(InvalidationListener)}
+ * {@link Observable} becomes invalid. It can be registered and
+ * unregistered with {@link Observable#addListener(InvalidationListener)}
+ * respectively {@link Observable#removeListener(InvalidationListener)}
  * <p>
  * For an in-depth explanation of invalidation events and how they differ from
  * change events, see the documentation of {@code ObservableValue}.
  * <p>
  * The same instance of {@code InvalidationListener} can be registered to listen
- * to multiple {@code ObservableValues}.
- * 
+ * to multiple {@code Observables}.
+ *
+ * @see Observable
  * @see ObservableValue
- * 
- * 
+ *
+ *
  */
 public interface InvalidationListener {
 
     /**
      * This method needs to be provided by an implementation of
-     * {@code InvalidationListener}. It is called if an {@link ObservableValue}
+     * {@code InvalidationListener}. It is called if an {@link Observable}
      * becomes invalid.
      * <p>
      * In general is is considered bad practice to modify the observed value in
      * this method.
-     * 
+     *
      * @param observable
-     *            The {@code ObservableValue} that became invalid
+     *            The {@code Observable} that became invalid
      */
     public void invalidated(Observable observable);
 }
