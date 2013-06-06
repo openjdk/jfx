e03025b544d3d06715496af15e5bae65babfd06c
Alexander Kouznetsov <mrkam@openjdk.org>
2013-06-06 01:02:56 -0700

    3DViewer optimizer: clean up repeating points and texCoords

--- apps/experiments/3DViewer/src/main/java/com/javafx/experiments/importers/Optimizer.java
+++ apps/experiments/3DViewer/src/main/java/com/javafx/experiments/importers/Optimizer.java
@@ -31,19 +31,25 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
-import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.beans.property.Property;
 import javafx.beans.value.WritableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableFloatArray;
+import javafx.collections.ObservableIntegerArray;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.SortedList;
+import javafx.geometry.Point2D;
+import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.shape.MeshView;
+import javafx.scene.shape.TriangleMesh;
+import static javafx.scene.shape.TriangleMesh.*;
 import javafx.scene.transform.Transform;
 
 /**
@@ -56,29 +62,31 @@ public class Optimizer {
     private Node root;
     private Set<Transform> bound = new HashSet<>();
     private List<Parent> emptyParents = new ArrayList<>();
+    private List<MeshView> meshViews = new ArrayList<>();
 
     public Optimizer(Timeline timeline, Node root) {
         this.timeline = timeline;
         this.root = root;
     }
 
-    private int removed, total, groupsTotal, candidate, emptyTransforms;
+    private int trRemoved, trTotal, groupsTotal, trCandidate, trEmpty;
 
     public void optimize() {
-        removed = 0;
-        total = 0;
-        candidate = 0;
-        emptyTransforms = 0;
+        trRemoved = 0;
+        trTotal = 0;
+        trCandidate = 0;
+        trEmpty = 0;
         groupsTotal = 0;
         emptyParents.clear();
 
         parseTimeline();
         optimize(root);
         removeEmptyGroups();
+        optimizeMeshes();
 
-        System.out.printf("removed %d (%.2f%%) out of total %d transforms\n", removed, 100d * removed / total, total);
-        System.out.printf("there are %d more multiplications that can be done of matrices that never change\n", candidate);
-        System.out.printf("there are %d (%.2f%%) out of total %d groups with no transforms in them\n", emptyTransforms, 100d * emptyTransforms / groupsTotal, groupsTotal);
+        System.out.printf("removed %d (%.2f%%) out of total %d transforms\n", trRemoved, 100d * trRemoved / trTotal, trTotal);
+        System.out.printf("there are %d more multiplications that can be done of matrices that never change\n", trCandidate);
+        System.out.printf("there are %d (%.2f%%) out of total %d groups with no transforms in them\n", trEmpty, 100d * trEmpty / groupsTotal, groupsTotal);
     }
 
     private void optimize(Node node) {
@@ -87,16 +95,16 @@ public class Optimizer {
         boolean prevIsStatic = false;
         while (iterator.hasNext()) {
             Transform transform = iterator.next();
-            total++;
+            trTotal++;
             if (transform.isIdentity()) {
                 if (timeline == null || !bound.contains(transform)) {
                     iterator.remove();
-                    removed++;
+                    trRemoved++;
                 }
             } else {
                 if (timeline == null || !bound.contains(transform)) {
                     if (prevIsStatic) {
-                        candidate++;
+                        trCandidate++;
                     }
                     prevIsStatic = true;
                 } else {
@@ -113,7 +121,7 @@ public class Optimizer {
             if (transforms.isEmpty()) {
                 Parent parent = p.getParent();
                 if (parent instanceof Group) {
-                    emptyTransforms++;
+                    trEmpty++;
 //                    System.out.println("Empty group = " + node.getId());
                     emptyParents.add(p);
                 } else {
@@ -121,6 +129,178 @@ public class Optimizer {
                 }
             }
         }
+        if (node instanceof MeshView) {
+            meshViews.add((MeshView) node);
+        }
+    }
+
+    private void optimizeMeshes() {
+        optimizePoints();
+        optimizeTexCoords();
+        optimizeFaces();
+    }
+
+    private void optimizeFaces() {
+        int total = 0, sameIndexes = 0, samePoints = 0, smallArea = 0;
+        for (MeshView meshView : meshViews) {
+            TriangleMesh mesh = (TriangleMesh) meshView.getMesh();
+            ObservableIntegerArray faces = mesh.getFaces();
+            ObservableFloatArray points = mesh.getPoints();
+            for (int i = 0; i < faces.size(); i += NUM_COMPONENTS_PER_FACE) {
+                total++;
+                int i1 = faces.get(i) * NUM_COMPONENTS_PER_POINT;
+                int i2 = faces.get(i + 2) * NUM_COMPONENTS_PER_POINT;
+                int i3 = faces.get(i + 4) * NUM_COMPONENTS_PER_POINT;
+                if (i1 == i2 || i1 == i3 || i2 == i3) {
+                    sameIndexes++;
+                    continue;
+                }
+                Point3D p1 = new Point3D(points.get(i1), points.get(i1 + 1), points.get(i1 + 2));
+                Point3D p2 = new Point3D(points.get(i2), points.get(i2 + 1), points.get(i2 + 2));
+                Point3D p3 = new Point3D(points.get(i3), points.get(i3 + 1), points.get(i3 + 2));
+                if (p1.equals(p2) || p1.equals(p3) || p2.equals(p3)) {
+                    samePoints++;
+                    continue;
+                }
+                double a = p1.distance(p2);
+                double b = p2.distance(p3);
+                double c = p3.distance(p1);
+                double p = (a + b + c) / 2;
+                double area = p * (p - a) * (p - b) * (p - c);
+
+                final float DEAD_FACE = 1.f/1024/1024/1024/1024;
+
+                if (area < DEAD_FACE) {
+                    smallArea++;
+                    System.out.printf("a = %e, b = %e, c = %e, area = %e\n"
+                            + "p1 = %s\np2 = %s\np3 = %s\n", a, b, c, area, p1.toString(), p2.toString(), p3.toString());
+                }
+            }
+        }
+        int badTotal = sameIndexes + samePoints + smallArea;
+        System.out.printf("There are %d (%.2f%%) faces with same point indexes, "
+                + "%d (%.2f%%) faces with same points, "
+                + "%d (%.2f%%) faces with small area. "
+                + "Total %d (%.2f%%) bad faces out of %d total.\n",
+                sameIndexes, 100d * sameIndexes / total,
+                samePoints, 100d * samePoints / total,
+                smallArea, 100d * smallArea / total,
+                badTotal, 100d * badTotal / total, total);
+    }
+
+    private void optimizePoints() {
+        int total = 0, duplicates = 0, check = 0;
+
+        Map<Point3D, Integer> pp = new HashMap<>();
+        ObservableIntegerArray reindex = FXCollections.observableIntegerArray();
+        ObservableFloatArray newPoints = FXCollections.observableFloatArray();
+
+        for (MeshView meshView : meshViews) {
+            TriangleMesh mesh = (TriangleMesh) meshView.getMesh();
+            ObservableFloatArray points = mesh.getPoints();
+            int os = points.size() / NUM_COMPONENTS_PER_POINT;
+
+            pp.clear();
+            newPoints.clear();
+            newPoints.ensureCapacity(points.size());
+            reindex.clear();
+            reindex.resize(os);
+
+            for (int i = 0, oi = 0, ni = 0; i < points.size(); i += NUM_COMPONENTS_PER_POINT, oi++) {
+                float x = points.get(i);
+                float y = points.get(i + 1);
+                float z = points.get(i + 2);
+                Point3D p = new Point3D(x, y, z);
+                Integer index = pp.get(p);
+                if (index == null) {
+                    pp.put(p, ni);
+                    reindex.set(oi, ni);
+                    newPoints.addAll(x, y, z);
+                    ni++;
+                } else {
+                    reindex.set(oi, index);
+                }
+            }
+
+            int ns = newPoints.size() / NUM_COMPONENTS_PER_POINT;
+
+            int d = os - ns;
+            duplicates += d;
+            total += os;
+
+            points.setAll(newPoints);
+            points.trimToSize();
+
+            ObservableIntegerArray faces = mesh.getFaces();
+            for (int i = 0; i < faces.size(); i += 2) {
+                faces.set(i, reindex.get(faces.get(i)));
+            }
+
+//            System.out.printf("There are %d (%.2f%%) duplicate points out of %d total for mesh '%s'.\n",
+//                    d, 100d * d / os, os, meshView.getId());
+
+            check += mesh.getPoints().size() / NUM_COMPONENTS_PER_POINT;
+        }
+        System.out.printf("There are %d (%.2f%%) duplicate points out of %d total.\n",
+                duplicates, 100d * duplicates / total, total);
+        System.out.printf("Now we have %d points.\n", check);
+    }
+
+    private void optimizeTexCoords() {
+        int total = 0, duplicates = 0, check = 0;
+
+        Map<Point2D, Integer> pp = new HashMap<>();
+        ObservableIntegerArray reindex = FXCollections.observableIntegerArray();
+        ObservableFloatArray newTexCoords = FXCollections.observableFloatArray();
+
+        for (MeshView meshView : meshViews) {
+            TriangleMesh mesh = (TriangleMesh) meshView.getMesh();
+            ObservableFloatArray texcoords = mesh.getTexCoords();
+            int os = texcoords.size() / NUM_COMPONENTS_PER_TEXCOORD;
+
+            pp.clear();
+            newTexCoords.clear();
+            newTexCoords.ensureCapacity(texcoords.size());
+            reindex.clear();
+            reindex.resize(os);
+
+            for (int i = 0, oi = 0, ni = 0; i < texcoords.size(); i += NUM_COMPONENTS_PER_TEXCOORD, oi++) {
+                float x = texcoords.get(i);
+                float y = texcoords.get(i + 1);
+                Point2D p = new Point2D(x, y);
+                Integer index = pp.get(p);
+                if (index == null) {
+                    pp.put(p, ni);
+                    reindex.set(oi, ni);
+                    newTexCoords.addAll(x, y);
+                    ni++;
+                } else {
+                    reindex.set(oi, index);
+                }
+            }
+
+            int ns = newTexCoords.size() / NUM_COMPONENTS_PER_TEXCOORD;
+
+            int d = os - ns;
+            duplicates += d;
+            total += os;
+
+            texcoords.setAll(newTexCoords);
+            texcoords.trimToSize();
+
+            ObservableIntegerArray faces = mesh.getFaces();
+            for (int i = 1; i < faces.size(); i += 2) {
+                faces.set(i, reindex.get(faces.get(i)));
+            }
+
+//            System.out.printf("There are %d (%.2f%%) duplicate texcoords out of %d total for mesh '%s'.\n",
+//                    d, 100d * d / os, os, meshView.getId());
+
+            check += mesh.getTexCoords().size() / NUM_COMPONENTS_PER_TEXCOORD;
+        }
+        System.out.printf("There are %d (%.2f%%) duplicate texcoords out of %d total.\n",
+                duplicates, 100d * duplicates / total, total);
+        System.out.printf("Now we have %d texcoords.\n", check);
     }
 
     private static class KeyInfo {
