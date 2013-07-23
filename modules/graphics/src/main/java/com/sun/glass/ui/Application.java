04530d8107cf2df16fbb64b5a25571b94580d3a9
Kevin Rushforth <kcr@openjdk.org>
2013-07-23 23:29:10 +0400

    Backed out fix for RT-26891 until RT-31879 (IllegalStateException in SwingInterop) is fixed
    Backed out 8fa67aa324c8

--- modules/graphics/src/main/java/com/sun/glass/ui/Application.java
+++ modules/graphics/src/main/java/com/sun/glass/ui/Application.java
@@ -97,7 +97,7 @@ public abstract class Application {
         AccessController.doPrivileged(new PrivilegedAction<Boolean>() {
             public Boolean run() {
                 final String str =
-                        System.getProperty("glass.disableThreadChecks", "false");
+                        System.getProperty("glass.disableThreadChecks", "true");
                 return "true".equalsIgnoreCase(str);
             }
         });
