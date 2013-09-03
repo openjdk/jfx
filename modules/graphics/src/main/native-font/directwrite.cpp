717bec516826169480d5077c6dcfb30cc734a391
Felipe Heidrich <fheidric@openjdk.org>
2013-09-03 15:38:51 -0700

    RT-32674: Win: Fatal JNI error in DirectWrite code

--- modules/graphics/src/main/native-font/directwrite.cpp
+++ modules/graphics/src/main/native-font/directwrite.cpp
@@ -204,7 +204,7 @@ DWRITE_GLYPH_RUN *getDWRITE_GLYPH_RUNFields(JNIEnv *env, jobject lpObject, DWRIT
     lpStruct->fontFace = (IDWriteFontFace *)env->GetLongField(lpObject, DWRITE_GLYPH_RUNFc.fontFace);
     lpStruct->fontEmSize = env->GetFloatField(lpObject, DWRITE_GLYPH_RUNFc.fontEmSize);
     ((jshort*)lpStruct->glyphIndices)[0] = env->GetShortField(lpObject, DWRITE_GLYPH_RUNFc.glyphIndices);
-    ((float*)lpStruct->glyphAdvances)[0] = env->GetShortField(lpObject, DWRITE_GLYPH_RUNFc.glyphAdvances);
+    ((float*)lpStruct->glyphAdvances)[0] = env->GetFloatField(lpObject, DWRITE_GLYPH_RUNFc.glyphAdvances);
     ((float)lpStruct->glyphOffsets[0].advanceOffset) = env->GetFloatField(lpObject, DWRITE_GLYPH_RUNFc.advanceOffset);
     ((float)lpStruct->glyphOffsets[0].ascenderOffset) = env->GetFloatField(lpObject, DWRITE_GLYPH_RUNFc.ascenderOffset);
     lpStruct->isSideways = env->GetBooleanField(lpObject, DWRITE_GLYPH_RUNFc.isSideways);
