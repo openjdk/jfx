6a738255316981c67015b3f796cba0c5436e34c1
Alexander Kouznetsov <mrkam@openjdk.org>
2013-10-07 15:20:16 +0400

    Ensemble8: Additional fix for RT-31427 Cursor sample in Ensemble8 doesn't look well with latest Lombard bits

--- apps/samples/Ensemble8/src/samples/java/ensemble/samples/scenegraph/events/cursor/CursorApp.java
+++ apps/samples/Ensemble8/src/samples/java/ensemble/samples/scenegraph/events/cursor/CursorApp.java
@@ -38,7 +38,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
@@ -53,12 +52,9 @@ public class CursorApp extends Application {
     
     public Parent createContent() {
 
-        ScrollPane root = new ScrollPane();
-        root.setPrefSize(435, 269);
-        root.setMinSize(ScrollPane.USE_PREF_SIZE, ScrollPane.USE_PREF_SIZE);
-        root.setMaxSize(ScrollPane.USE_PREF_SIZE, ScrollPane.USE_PREF_SIZE);
-
         TilePane tilePaneRoot = new TilePane(5, 5);
+        tilePaneRoot.setMinSize(TilePane.USE_PREF_SIZE, TilePane.USE_PREF_SIZE);
+        tilePaneRoot.setMaxSize(TilePane.USE_PREF_SIZE, TilePane.USE_PREF_SIZE);
         tilePaneRoot.setHgap(2);
         tilePaneRoot.setVgap(2);
         tilePaneRoot.getChildren().addAll(
@@ -82,14 +78,12 @@ public class CursorApp extends Application {
                 createBox(Cursor.H_RESIZE),
                 createBox(Cursor.V_RESIZE),
                 createBox(Cursor.NONE));
-        root.setContent(tilePaneRoot);
-        return root;
+        return tilePaneRoot;
     }
 
     private Node createBox(Cursor cursor) {
         Label label = new Label(cursor.toString());
         label.setAlignment(Pos.CENTER);
-        label.setWrapText(true);
         label.setPrefSize(85, 65);
         label.setStyle("-fx-border-color: #aaaaaa; -fx-background-color: #dddddd;");
         label.setCursor(cursor);
