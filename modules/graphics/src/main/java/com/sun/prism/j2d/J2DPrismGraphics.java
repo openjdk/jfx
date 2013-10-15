7f052b1fcd4c19cd7306d86acffc03f332aa2417
Phil Race <prr@openjdk.org>
2013-10-15 09:16:20 -0700

    Fixed RT-28501: Button Printed improperly
    Reviewed-by: kcr, jgodinez

--- modules/graphics/src/main/java/com/sun/prism/j2d/J2DPrismGraphics.java
+++ modules/graphics/src/main/java/com/sun/prism/j2d/J2DPrismGraphics.java
@@ -973,6 +973,9 @@ public class J2DPrismGraphics
                                    float sx1, float sy1, float sx2, float sy2,
                                    float dh1, float dh2, float sh1, float sh2)
     {
+        // Workaround for problems in NGRegion which may pass zero-width
+        // source image area.
+        if (sh1 + 0.1f > sh2) sh2 += 1;
         drawTexture(tex, dx1, dy1, dh1, dy2, sx1, sy1, sh1, sy2);
         drawTexture(tex, dh1, dy1, dh2, dy2, sh1, sy1, sh2, sy2);
         drawTexture(tex, dh2, dy1, dx2, dy2, sh2, sy1, sx2, sy2);
@@ -984,6 +987,9 @@ public class J2DPrismGraphics
                                    float sx1, float sy1, float sx2, float sy2,
                                    float dv1, float dv2, float sv1, float sv2)
     {
+        // Workaround for problems in NGRegion which may pass zero-height
+        // source image area.
+        if (sv1 +0.1f > sv2) sv2 += 1;
         drawTexture(tex, dx1, dy1, dx2, dv1, sx1, sy1, sx2, sv1);
         drawTexture(tex, dx1, dv1, dx2, dv2, sx1, sv1, sx2, sv2);
         drawTexture(tex, dx1, dv2, dx2, dy2, sx1, sv2, sx2, sy2);
@@ -996,6 +1002,10 @@ public class J2DPrismGraphics
                                   float dh1, float dv1, float dh2, float dv2,
                                   float sh1, float sv1, float sh2, float sv2)
     {
+        // Workaround for problems in NGRegion which may pass zero-width
+        // or zero height source image area.
+        if (sh1 + 0.1f > sh2) sh2 += 1;
+        if (sv1 + 0.1f > sv2) sv2 += 1;
         drawTexture(tex, dx1, dy1, dh1, dv1, sx1, sy1, sh1, sv1);
         drawTexture(tex, dh1, dy1, dh2, dv1, sh1, sy1, sh2, sv1);
         drawTexture(tex, dh2, dy1, dx2, dv1, sh2, sy1, sx2, sv1);
