4efc94af84352485e5354f4322359416b958b9ae
David Grieve <dgrieve@openjdk.org>
2013-11-25 10:00:45 -0500

    [TEST-ONLY] Ignore Node_cssStyleMap_Test pending RT-33463

--- modules/graphics/src/test/java/com/sun/javafx/css/Node_cssStyleMap_Test.java
+++ modules/graphics/src/test/java/com/sun/javafx/css/Node_cssStyleMap_Test.java
@@ -50,6 +50,7 @@ import static org.junit.Assert.*;
 import org.junit.Ignore;
 import org.junit.Test;
 
+@Ignore ("Pending RT-34463")
 public class Node_cssStyleMap_Test {
     
     public Node_cssStyleMap_Test() {
