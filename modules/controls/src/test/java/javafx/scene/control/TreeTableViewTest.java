7361338bb6753ec9d712873e311fc1eb14692165
Jonathan Giles <jgiles@openjdk.org>
2013-12-09 14:04:17 +1300

    RT-34694: Incorrect item is selected on TreeItem's list changing

--- modules/controls/src/test/java/javafx/scene/control/TreeTableViewTest.java
+++ modules/controls/src/test/java/javafx/scene/control/TreeTableViewTest.java
@@ -2907,4 +2907,38 @@ public class TreeTableViewTest {
         assertEquals(table.getRoot(), table.getSelectionModel().getSelectedItem());
         assertEquals(table.getRoot(), table.getFocusModel().getFocusedItem());
     }
+
+    @Test public void test_rt34694() {
+        TreeItem treeNode = new TreeItem("Controls");
+        treeNode.getChildren().addAll(
+                new TreeItem("Button"),
+                new TreeItem("ButtonBar"),
+                new TreeItem("LinkBar"),
+                new TreeItem("LinkButton"),
+                new TreeItem("PopUpButton"),
+                new TreeItem("ToggleButtonBar")
+        );
+
+        final TreeTableView<String> table = new TreeTableView<>();
+        table.setRoot(treeNode);
+        treeNode.setExpanded(true);
+
+        table.getSelectionModel().select(0);
+        assertTrue(table.getSelectionModel().isSelected(0));
+        assertTrue(table.getFocusModel().isFocused(0));
+
+        treeNode.getChildren().clear();
+        treeNode.getChildren().addAll(
+                new TreeItem("Button1"),
+                new TreeItem("ButtonBar1"),
+                new TreeItem("LinkBar1"),
+                new TreeItem("LinkButton1"),
+                new TreeItem("PopUpButton1"),
+                new TreeItem("ToggleButtonBar1")
+        );
+        Toolkit.getToolkit().firePulse();
+
+        assertTrue(table.getSelectionModel().isSelected(0));
+        assertTrue(table.getFocusModel().isFocused(0));
+    }
 }
