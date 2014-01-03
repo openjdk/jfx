007a9d3fad074d17216f21a25c580e3f36d67356
Stefan Fuchs <snfuchs@gmx.de>
2014-01-03 12:23:27 -0500

    RT-35147: [Android, Ensemble8] App should react to hardware buttons on Android
    Reviewed-by: snorthov

--- apps/samples/Ensemble8/src/app/java/ensemble/PlatformFeatures.java
+++ apps/samples/Ensemble8/src/app/java/ensemble/PlatformFeatures.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2013 Oracle and/or its affiliates.
+ * Copyright (c) 2012, 2014 Oracle and/or its affiliates.
  * All rights reserved. Use is subject to license terms.
  *
  * This file is available and licensed under the following license:
@@ -43,14 +43,17 @@ public class PlatformFeatures {
     private static final boolean WINDOWS = os.startsWith("Windows");
     private static final boolean MAC = os.startsWith("Mac");
     private static final boolean LINUX = os.startsWith("Linux");
-    private static final boolean IOS = os.startsWith("iOS");   
-    private static final boolean ARM = arch.equals("arm");
+    private static final boolean ANDROID = "android".equals(System.getProperty("javafx.platform")) || "Dalvik".equals(System.getProperty("java.vm.name"));
+    private static final boolean IOS = os.startsWith("iOS");
+    private static final boolean EMBEDDED = "arm".equals(arch) && !IOS && !ANDROID;
     
-    public static final boolean SUPPORTS_BENDING_PAGES = !ARM;
+    public static final boolean SUPPORTS_BENDING_PAGES = !EMBEDDED;
     public static final boolean HAS_HELVETICA = MAC || IOS;
     public static final boolean USE_IOS_THEME = IOS;
-    public static final boolean START_FULL_SCREEN = ARM;
-    public static final boolean EMBEDDED = ARM || IOS;
+    public static final boolean START_FULL_SCREEN = EMBEDDED || IOS || ANDROID;
+    public static final boolean LINK_TO_SOURCE = !(EMBEDDED || IOS || ANDROID);
+    public static final boolean DISPLAY_PLAYGROUND = !(EMBEDDED || IOS || ANDROID);
+    public static final boolean USE_EMBEDDED_FILTER = EMBEDDED || IOS || ANDROID;
     public static final boolean WEB_SUPPORTED = Platform.isSupported(ConditionalFeature.WEB);
         
     private PlatformFeatures(){}
