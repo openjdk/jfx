92116a1cedd255043bdfe12c1e4b9b90930003b2
Steve Northover <snorthov@openjdk.org>
2014-01-13 15:51:59 -0500

    RT-4169: [Stage] Setting 'icons' to null do not reset the Stage icon
    The code to clean the icon in Glass was not being called by Quantum.  When the code was called, there were problems in native glass on Mac and GTK that needed to be fixed.
    
    Reviewed-by: anthony

--- modules/graphics/src/main/java/com/sun/javafx/tk/quantum/WindowStage.java
+++ modules/graphics/src/main/java/com/sun/javafx/tk/quantum/WindowStage.java
@@ -302,6 +302,7 @@ class WindowStage extends GlassStage {
         }
 
         if (icons == null || icons.size() < 1) { //no icons passed in
+            platformWindow.setIcon(null);
             return;
         }
 
