5f8a0d0b516a574848414b2e4650018abcb21c81
David Grieve <dgrieve@openjdk.org>
2014-01-16 19:13:20 -0500

    RT-17395: if ScrollBarPolicy value is null, treat it as AS_NEEDED. Also changed comparison of enum using equals method to using ==

--- modules/controls/src/main/java/com/sun/javafx/scene/control/skin/ScrollPaneSkin.java
+++ modules/controls/src/main/java/com/sun/javafx/scene/control/skin/ScrollPaneSkin.java
@@ -950,8 +950,10 @@ public class ScrollPaneSkin extends BehaviorSkinBase<ScrollPane, ScrollPaneBehav
             return (tempVisibility && (nodeWidth > contentw));
         }
         else {
-            return (getSkinnable().getHbarPolicy().equals(ScrollBarPolicy.NEVER)) ? false :
-                ((getSkinnable().getHbarPolicy().equals(ScrollBarPolicy.ALWAYS)) ? true :
+            // RT-17395: ScrollBarPolicy might be null. If so, treat it as "AS_NEEDED", which is the default
+            ScrollBarPolicy hbarPolicy = sp.getHbarPolicy();
+            return (ScrollBarPolicy.NEVER == hbarPolicy) ? false :
+                ((ScrollBarPolicy.ALWAYS == hbarPolicy) ? true :
                  ((getSkinnable().isFitToWidth() && scrollNode != null ? scrollNode.isResizable() : false) ?
                   (nodeWidth > contentw && scrollNode.minWidth(-1) > contentw) : (nodeWidth > contentw)));
         }
@@ -964,8 +966,10 @@ public class ScrollPaneSkin extends BehaviorSkinBase<ScrollPane, ScrollPaneBehav
             return (tempVisibility && (nodeHeight > contenth));
         }
         else {
-            return (getSkinnable().getVbarPolicy().equals(ScrollBarPolicy.NEVER)) ? false :
-                ((getSkinnable().getVbarPolicy().equals(ScrollBarPolicy.ALWAYS)) ? true :
+            // RT-17395: ScrollBarPolicy might be null. If so, treat it as "AS_NEEDED", which is the default
+            ScrollBarPolicy vbarPolicy = sp.getVbarPolicy();
+            return (ScrollBarPolicy.NEVER == vbarPolicy) ? false :
+                ((ScrollBarPolicy.ALWAYS == vbarPolicy) ? true :
                  ((getSkinnable().isFitToHeight() && scrollNode != null ? scrollNode.isResizable() : false) ?
                   (nodeHeight > contenth && scrollNode.minHeight(-1) > contenth) : (nodeHeight > contenth)));
         }
