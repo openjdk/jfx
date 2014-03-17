62ac6020d4507e6a3df5d6162a5a8c7d76572fa7
Kevin Rushforth <kcr@openjdk.org>
2014-03-17 05:28:19 -0700

    RT-36141: Gtk: SecurityException using WebView when running app with security manager
    Reviewed-by: anthony

--- modules/graphics/src/main/java/com/sun/glass/ui/Application.java
+++ modules/graphics/src/main/java/com/sun/glass/ui/Application.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -216,7 +216,12 @@ public abstract class Application {
      */
     public String getDataDirectory() {
         checkEventThread();
-        return System.getProperty("user.home") + File.separator + "." + name + File.separator;
+        String userHome = AccessController.doPrivileged(new PrivilegedAction<String>() {
+            @Override public String run() {
+                return System.getProperty("user.home");
+            }
+        });
+        return userHome + File.separator + "." + name + File.separator;
     }
 
     private void notifyWillFinishLaunching() {
