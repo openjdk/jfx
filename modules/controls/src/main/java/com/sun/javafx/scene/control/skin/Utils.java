179282abca3026323861121797988f1e9496ecfe
David Grieve <dgrieve@openjdk.org>
2014-03-18 11:51:33 -0400

    RT-30868: from computeClippedWrappedText if ellipsis doesn't fit, return text

--- modules/controls/src/main/java/com/sun/javafx/scene/control/skin/Utils.java
+++ modules/controls/src/main/java/com/sun/javafx/scene/control/skin/Utils.java
@@ -363,7 +363,7 @@ public class Utils {
 
         if (width < eWidth || height < eHeight) {
             // The ellipsis doesn't fit.
-            return "";
+            return text; // RT-30868 - return text, not empty string.
         }
 
         helper.setText(text);
