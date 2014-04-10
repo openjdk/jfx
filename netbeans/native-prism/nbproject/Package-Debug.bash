8e64133b97710f0a2b6e2b97d98735db90864fc6
Chien Yang <ckyang@openjdk.org>
2014-04-10 13:22:57 -0700

    Update nb project files to Netbeans 8.0 FCS and update native-glass project to the recent accessibility work

--- netbeans/native-prism/nbproject/Package-Debug.bash
+++ netbeans/native-prism/nbproject/Package-Debug.bash
@@ -6,16 +6,16 @@
 
 # Macros
 TOP=`pwd`
-CND_PLATFORM=Cygwin_4.x-Windows
+CND_PLATFORM=GNU-Linux-x86
 CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
-CND_DLIB_EXT=dll
+CND_DLIB_EXT=so
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
 OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libnative-prism.${CND_DLIB_EXT}
 OUTPUT_BASENAME=libnative-prism.${CND_DLIB_EXT}
-PACKAGE_TOP_DIR=libnative-prism.dll/
+PACKAGE_TOP_DIR=libnative-prism.so/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/libnative-prism.dll/lib"
+makeDirectory "${NBTMPDIR}/libnative-prism.so/lib"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}lib/${OUTPUT_BASENAME}" 0644
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/libnative-prism.dll.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/libnative-prism.so.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/libnative-prism.dll.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/libnative-prism.so.tar *
 checkReturnCode
 
 # Cleanup
