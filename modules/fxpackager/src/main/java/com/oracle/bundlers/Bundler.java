e813fbfee64f40155b8396a149e0b833568b25cf
Danno Ferrin <shemnon@openjdk.org>
2014-04-24 12:42:07 -0600

    Clarify that execute may still "succeed" even if validate fails, and that those results are not to be trusted
    [javadocs] RT-36758: [Bundlers] DMG bundler doesn't support SERVICE_HINT option

--- modules/fxpackager/src/main/java/com/oracle/bundlers/Bundler.java
+++ modules/fxpackager/src/main/java/com/oracle/bundlers/Bundler.java
@@ -71,14 +71,23 @@ public interface Bundler {
      * @throws UnsupportedPlatformException If the bundler cannot run on this
      *         platform (i.e. creating mac apps on windows)
      * @throws ConfigException If the configuration params are incorrect.  The
-     *         exception will contain advice to bring the parameters into compliance.
+     *         exception may contain advice on how to modify the params map
+     *         to make it valid.
      */
     boolean validate(Map<String, ? super Object> params) throws UnsupportedPlatformException, ConfigException;
 
     /**
      * Creates a bundle from existing content.
+     *
+     * If a call to {@link #validate(java.util.Map)} date} returns true with the
+     * parameters map, then you can expect a valid output.  However if an exception
+     * was thrown out of validate or it returned false then you should not
+     * expect sensible results from this call.  It may or may not return a value,
+     * and it may or may not throw an exception.  But any output should not
+     * be considered valid or sane.
+     *
      * @param params The parameters as specified by getBundleParameters.
-     *               Keyed by the id from the ParamInfo.  Validation may
+     *               Keyed by the id from the ParamInfo.  Execution may
      *               modify the map, so if you are going to be using the
      *               same map across multiple bundlers you should pass
      *               in a deep copy.
@@ -93,8 +102,8 @@ public interface Bundler {
      * be the location of that specific directory (.app file, etc).
      *
      * For a bundler that produce multiple files, this will be a parent
-     * directory of those files (linux, and windows, images), whose name is not
-     * relavent to the result.
+     * directory of those files (linux and windows images), whose name is not
+     * relevant to the result.
      *
      * @throws java.lang.IllegalArgumentException for any of the following
      * reasons:
@@ -106,7 +115,6 @@ public interface Bundler {
      *      <li>Bundler specific incompatibilities with the parameters, for
      *      example a bad version number format or an application id with
      *      forward slashes.</li>
-     *      <li>(?) a parameter not listed in getBundleParameters - or do we allow it? (?)</li>
      *  </ul>
      */
     public File execute(Map<String, ? super Object> params, File outputParentDir);
