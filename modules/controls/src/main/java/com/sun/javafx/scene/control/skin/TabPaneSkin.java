b58e873be3271458dc1c62b1f3cadec939f06492
Jonathan Giles <jgiles@openjdk.org>
2014-05-08 09:50:11 +1200

    RT-37009: Removing and instantly re-adding a Tab to the same TabPane does not work

--- modules/controls/src/main/java/com/sun/javafx/scene/control/skin/TabPaneSkin.java
+++ modules/controls/src/main/java/com/sun/javafx/scene/control/skin/TabPaneSkin.java
@@ -346,13 +346,17 @@ public class TabPaneSkin extends BehaviorSkinBase<TabPane, TabPaneBehavior> {
             removeTabs(tabsToRemove);
 
             // and add in any new tabs (that we don't already have showing)
-            for (TabContentRegion tabContentRegion : tabContentRegions) {
-                if (tabsToAdd.contains(tabContentRegion.getTab())) {
-                    tabsToAdd.remove(tabContentRegion.getTab());
+            if (! tabsToAdd.isEmpty()) {
+                for (TabContentRegion tabContentRegion : tabContentRegions) {
+                    Tab tab = tabContentRegion.getTab();
+                    TabHeaderSkin tabHeader = tabHeaderArea.getTabHeaderSkin(tab);
+                    if (!tabHeader.isClosing && tabsToAdd.contains(tabContentRegion.getTab())) {
+                        tabsToAdd.remove(tabContentRegion.getTab());
+                    }
                 }
-            }
 
-            addTabs(tabsToAdd, insertPos == -1 ? tabContentRegions.size() : insertPos);
+                addTabs(tabsToAdd, insertPos == -1 ? tabContentRegions.size() : insertPos);
+            }
 
             // Fix for RT-34692
             getSkinnable().requestLayout();
