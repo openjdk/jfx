bab27d96cf55831a917b3ee8554180c79952fbbf
Phil Race <prr@openjdk.org>
2014-05-08 09:01:51 -0700

    Fixed RT-35414: Printing] PrintJob.printPage() never returns on Mac OS X
    Reviewed-by: kcr

--- modules/graphics/src/main/java/com/sun/prism/j2d/print/J2DPrinterJob.java
+++ modules/graphics/src/main/java/com/sun/prism/j2d/print/J2DPrinterJob.java
@@ -87,6 +87,7 @@ public class J2DPrinterJob implements PrinterJobImpl {
     
     private JobSettings settings;
     private PrintRequestAttributeSet printReqAttrSet;
+    private volatile Object elo = null;
 
     public J2DPrinterJob(javafx.print.PrinterJob fxJob) {
 
@@ -103,6 +104,8 @@ public class J2DPrinterJob implements PrinterJobImpl {
         // dialog selection is a JDK 1.7 attribute.
         // We expect to run on 1.8 and above so this should be fine.
         printReqAttrSet.add(DialogTypeSelection.NATIVE);
+        j2dPageable = new J2DPageable();
+        pJob2D.setPageable(j2dPageable);
     }
 
     public boolean showPrintDialog(Window owner) {
@@ -694,8 +697,6 @@ public class J2DPrinterJob implements PrinterJobImpl {
 
         if (!jobRunning) {
             checkPermissions();
-            j2dPageable = new J2DPageable();
-            pJob2D.setPageable(j2dPageable);
             syncSettingsToAttributes();
             PrintJobRunnable runnable = new PrintJobRunnable();
             Thread prtThread = new Thread(runnable, "Print Job Thread");
@@ -728,6 +729,13 @@ public class J2DPrinterJob implements PrinterJobImpl {
                 }
                 jobError = true;
                 jobDone = true;
+            }
+            /*
+             * If the job ends because its reached a page range limit
+             * rather than calling getPage() we need to exit the nested loop.
+             */
+            if (elo != null) {
+                Application.invokeLater(new ExitLoopRunnable(elo, null));
             }
         }
     }
@@ -885,7 +893,7 @@ public class J2DPrinterJob implements PrinterJobImpl {
 
             if (elo != null && currPageInfo != null) {
                 Application.invokeLater(new ExitLoopRunnable(elo, null));
-            }
+	    }
 
             if (currPageInfo != null) {
                 if (Toolkit.getToolkit().isFxUserThread()) {
@@ -964,8 +972,6 @@ public class J2DPrinterJob implements PrinterJobImpl {
             boolean nextPage = false;
             if (pageIndex > currPageIndex) {
                 nextPage = waitForNextPage(pageIndex);
-            } else if (pageIndex < currPageIndex) {
-                System.err.println("PAGE INDEX DECREASED");
             }
             return nextPage;
         }
@@ -1018,9 +1024,6 @@ public class J2DPrinterJob implements PrinterJobImpl {
             return Pageable.UNKNOWN_NUMBER_OF_PAGES;
         }
 
-
-        private volatile Object elo = null;
-
         /*
          * Executed on the application's thread.
          * Messages over to the printing thread.
@@ -1070,7 +1073,9 @@ public class J2DPrinterJob implements PrinterJobImpl {
                     return jobDone;
                 }
             } catch (IllegalStateException e) {
-                System.err.println("Internal Error " + e);
+                if (com.sun.prism.impl.PrismSettings.debug) {
+                    System.err.println("Internal Error " + e);
+                }
             }
         } else {
             return false;
@@ -1090,7 +1095,9 @@ public class J2DPrinterJob implements PrinterJobImpl {
                     monitor.notify();
                 }
             } catch (IllegalStateException e) {
-                System.err.println("Internal Error " + e);
+                if (com.sun.prism.impl.PrismSettings.debug) {
+                    System.err.println("Internal Error " + e);
+                }
             }
         }
     }
