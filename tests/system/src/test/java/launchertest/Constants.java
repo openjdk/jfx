145c129eac86c1e6b341dcfc648a348a18e73096
Kevin Rushforth <kcr@openjdk.org>
2014-08-06 16:59:51 -0700

    RT-38218: Gtk: Need unit test for RT-37974

--- tests/system/src/test/java/launchertest/Constants.java
+++ tests/system/src/test/java/launchertest/Constants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -56,4 +56,6 @@ public class Constants {
     static final int ERROR_PRELOADER_INIT_WRONG_THREAD = 20;
     static final int ERROR_PRELOADER_START_WRONG_THREAD = 21;
     static final int ERROR_PRELOADER_STOP_WRONG_THREAD = 22;
+
+    static final int ERROR_LAUNCH_SUCCEEDED = 23;
 }
