64a0de4a1a0d0753fe19bbc641de925eace82d75
Vadim Pakhnushev <vadim@openjdk.org>
2014-08-09 05:36:49 +0400

    RT-37577: [Mac, Retina] Image loading for @2x images may try to append @2x suffix to wrong part of URL
    Reviewed-by: flar, snorthov

--- modules/graphics/src/test/java/com/sun/javafx/iio/ImageStorageTest.java
+++ modules/graphics/src/test/java/com/sun/javafx/iio/ImageStorageTest.java
@@ -25,7 +25,9 @@
 
 package com.sun.javafx.iio;
 
+import com.sun.javafx.iio.common.ImageTools;
 import static org.junit.Assert.assertNotNull;
+import org.junit.ComparisonFailure;
 import org.junit.Test;
 
 public class ImageStorageTest {
@@ -33,5 +35,26 @@ public class ImageStorageTest {
     public void createImageFromNoExtensionURL() throws ImageStorageException {
         String path = this.getClass().getResource("testpngnoextension").toString();
         assertNotNull(ImageStorage.loadAll(path, null, 0, 0, true, 2.0f, true));
+    }
+
+    @Test
+    public void testImageNames() {
+        String [][]imageNames = new String[][] {
+            { "image", "image@2x" },
+            { "image.ext", "image@2x.ext" },
+            { "dir/image", "dir/image@2x" },
+            { "/dir.ext/image.ext", "/dir.ext/image@2x.ext" },
+            { "file:image", "file:image@2x" },
+            { "file:image.ext", "file:image@2x.ext" },
+            { "http://test.com/image", "http://test.com/image@2x" },
+            { "http://test.com/dir.ext/image", "http://test.com/dir.ext/image@2x" },
+            { "http://test.com/image.ext", "http://test.com/image@2x.ext" },
+            { "http://test.com/dir.ext/image.ext", "http://test.com/dir.ext/image@2x.ext" },
+        };
+        for (String[] names : imageNames) {
+            String name2x = ImageTools.getScaledImageName(names[0]);
+            if (name2x.equals(names[1])) continue;
+            throw new ComparisonFailure("Scaled image names don't match", names[1], name2x);
+        }
     }
 }
