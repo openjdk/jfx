81556ba3fbc64cdf4e6c153458489543b1587fb3
Felipe Heidrich <fheidric@openjdk.org>
2014-08-14 13:34:18 -0700

    [Accessibility] JavaDoc for AccessibleAction updated

--- modules/graphics/src/main/java/javafx/scene/AccessibleAction.java
+++ modules/graphics/src/main/java/javafx/scene/AccessibleAction.java
@@ -26,82 +26,179 @@
 package javafx.scene;
 
 /**
- * Experimental API - Do not use (will be removed).
+ * The {@code AccessibleAction} enum defines the actions assistive technology,
+ * such as screen readers, can request nodes in the scene graph.<br>
+ * The actions each node must support depends on its {@link AccessibleRole}.
+ * <p>Action can be augmented by parameters or not.</p>
  *
- * @treatAsPrivate
+ * @see Node#executeAccessibleAction(AccessibleAction, Object...)
+ * @see AccessibleRole
+ * @see AccessibleAttribute#ROLE
+ * 
+ * @since JavaFX 8u40
  */
 public enum AccessibleAction {
 
     /**
-     * Decrements the node (if it support BLOCK_DECREMENT) by its larger block decrement
-     * value. A smaller decrement can be performed by using {@link #DECREMENT}.
+     * Decrements the node by its larger block decrement value.
+     * A smaller decrement can be performed by using {@link #DECREMENT}.
+     * <p>Used by Slider, ScrollBar, etc </p>
+     * 
+     * Parameters:
+     * <ul>
+     * </ul>
      */
     BLOCK_DECREMENT,
 
     /**
-     * Increments the node (if it support BLOCK_INCREMENT) by its larger block increment
-     * value. A smaller increment can be performed by using {@link #INCREMENT}.
+     * Increments the node by its larger block increment value. 
+     * A smaller increment can be performed by using {@link #INCREMENT}.
+     * <p>Used by Slider, ScrollBar, etc </p>
+     * 
+     * Parameters:
+     * <ul>
+     * </ul>
      */
     BLOCK_INCREMENT,
 
+    /**
+     * Request the node to collapse itself.
+     * <p>Used by TreeItem, TitledPane, etc </p>
+     * 
+     * Parameters:
+     * <ul>
+     * </ul>
+     */
     COLLAPSE,
 
     /**
-     * Decrements the node (if it support DECREMENT) by its smaller unit decrement
-     * value. A larger decrement can be performed by using {@link #BLOCK_DECREMENT}.
+     * Decrements the node by its smaller unit decrement value.
+     * A larger decrement can be performed by using {@link #BLOCK_DECREMENT}.
+     * <p>Used by Slider, ScrollBar, etc </p>
+     * 
+     * Parameters:
+     * <ul>
+     * </ul>
      */
     DECREMENT,
 
+    /**
+     * Request the node to expand itself.
+     * <p>Used by TreeItem, TitledPane, etc </p>
+     * 
+     * Parameters:
+     * <ul>
+     * </ul>
+     */
     EXPAND,
 
+    /**
+     * Fire the node.
+     * <p>Used by Button, Hyperlink, etc </p>
+     * 
+     * Parameters:
+     * <ul>
+     * </ul>
+     */
     FIRE,
 
     /**
-     * Increments the node (if it support INCREMENT) by its smaller unit increment
-     * value. A larger increment can be performed by using {@link #BLOCK_INCREMENT}.
+     * Increments the node by its smaller unit increment value.
+     * A larger increment can be performed by using {@link #BLOCK_INCREMENT}.
+     * <p>Used by Slider, ScrollBar, etc </p>
+     * 
+     * Parameters:
+     * <ul>
+     * </ul>
      */
     INCREMENT,
 
+    /**
+     * Indicate the node to request focus.
+     * <p>Used by Node, TabItem, etc </p>
+     * 
+     * Parameters:
+     * <ul>
+     * </ul>
+     */
     REQUEST_FOCUS,
 
     /**
      * Requests the view to show an item, scrolling if required.
-     * Parameter: Node
+     * <p>Used by ListView, TreeView, etc </p>
+     * 
+     * Parameters:
+     * <ul>
+     * <li> {@link Node} the item to show </li>
+     * </ul>
      */
     SHOW_ITEM,
 
     /**
      * Requests the view to show the given text range, scrolling if required.
-     * Parameter: Integer (start offset), Integer (end offset)
+     * <p>Used by TextField and TextArea. </p>
+     * 
+     * Parameters:
+     * <ul>
+     * <li> {@link java.lang.Integer} the start offset </li>
+     * <li> {@link java.lang.Integer} the end offset </li>
+     * </ul>
      */
     SHOW_TEXT_RANGE,
 
     /**
-     * Sets the selected items.
+     * Requests the view to sets its selection to the given items.
      * Parameter: ObservableList&lt;Node&gt;
+     * <p>Used by ListView, TreeView, etc </p>
+     * 
+     * Parameters:
+     * <ul>
+     * <li> {@link javafx.collections.ObservableList}&lt;{@link Node}&gt; the items to select </li>
+     * </ul>
      */
     SET_SELECTED_ITEMS,
 
     /**
-     * Sets the text selection.
-     * Parameter: Integer (start offset), Integer (end offset)
+     * Sets the text selection for a node.
+     * <p>Used by TextField and TextArea. </p>
+     * 
+     * Parameters:
+     * <ul>
+     * <li> {@link java.lang.Integer} the start offset </li>
+     * <li> {@link java.lang.Integer} the end offset </li>
+     * </ul>
      */
     SET_TEXT_SELECTION,
 
     /**
      * Sets the text for a node.
-     * Parameter: String
+     * <p>Used by TextField and TextArea. </p>
+     * 
+     * Parameters:
+     * <ul>
+     * <li> {@link String} the new text</li>
+     * </ul>
      */
     SET_TEXT,
 
     /**
      * Sets the value for a node.
-     * Parameter: Double
+     * <p>Used by Slider, Scrollbars, etc </p>
+     * 
+     * Parameters:
+     * <ul>
+     * <li> {@link java.lang.Double} the new value </li>
+     * </ul>
      */
     SET_VALUE,
 
     /**
      * Request the receiver to show its menu.
+     * <p>Used by Node. </p>
+     * 
+     * Parameters:
+     * <ul>
+     * </ul>
      */
     SHOW_MENU,
 }
