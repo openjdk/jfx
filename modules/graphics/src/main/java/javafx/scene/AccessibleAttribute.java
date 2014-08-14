ddaaab38085811c26d248ccf0a3e99f8e78d15ec
Felipe Heidrich <fheidric@openjdk.org>
2014-08-14 13:42:57 -0700

    [Accessibility] Removed AccessibleAttribute#getName(), it was not used

--- modules/graphics/src/main/java/javafx/scene/AccessibleAttribute.java
+++ modules/graphics/src/main/java/javafx/scene/AccessibleAttribute.java
@@ -43,13 +43,13 @@ public enum AccessibleAttribute {
      * Returns the accelerator for the Node.
      * Type: KeyCombination
      */
-    ACCELERATOR("Accelerator", KeyCombination.class),
+    ACCELERATOR(KeyCombination.class),
 
     /**
      * Returns the bounds for the Node.
      * Type: Bounds
      */
-    BOUNDS("Bounds", Bounds.class),
+    BOUNDS(Bounds.class),
 
     /**
      * Returns the array of bounding rectangles for the given char range.
@@ -57,81 +57,81 @@ public enum AccessibleAttribute {
      * Parameter: Integer range length
      * Type: Bounds[]
      */
-    BOUNDS_FOR_RANGE("BoundsForRange", Bounds[].class),
+    BOUNDS_FOR_RANGE(Bounds[].class),
 
     /**
      * Returns the caret offset for the node.
      * Type: Integer
      */
-    CARET_OFFSET("CaretOffset", Integer.class),
+    CARET_OFFSET(Integer.class),
 
     /**
      * Returns the children for the Node.
      * Type: ObservableList&lt;Node&gt;
      */
-    CHILDREN("Children", ObservableList.class),
+    CHILDREN(ObservableList.class),
 
     /**
      * Returns the column at the given index
      * Parameter: Integer
      * Type: Node
      */
-    COLUMN_AT_INDEX("ColumnAtIndex", Node.class),
+    COLUMN_AT_INDEX(Node.class),
 
     /**
      * Returns the cell at the given row and column indices
      * Parameter: Integer, Integer
      * Type: Node
      */
-    CELL_AT_ROW_COLUMN("CellAtRowColumn", Node.class),
+    CELL_AT_ROW_COLUMN(Node.class),
 
     /**
      * Returns the column count
      * Type: Integer
      */
-    COLUMN_COUNT("ColumnCount", Integer.class),
+    COLUMN_COUNT(Integer.class),
 
     /**
      * Returns the column index of a cell
      * Type: Integer
      */
-    COLUMN_INDEX("ColumnIndex", Integer.class),
+    COLUMN_INDEX(Integer.class),
 
     /**
      * Returns the contents of a scroll pane
      * Type: Node
      */
-    CONTENTS("Contents", Node.class),
+    CONTENTS(Node.class),
 
     /**
      * Returns true if the Node is disabled.
      * Type: Boolean
      */
-    DISABLED("Disabled", Boolean.class),
+    DISABLED(Boolean.class),
 
     /**
      * Depth of a row in a disclosure hierarchy.
      * Type: Integer
      */
-    DISCLOSURE_LEVEL("DisclosureLevel", Integer.class),
+    DISCLOSURE_LEVEL(Integer.class),
 
     /**
      * Returns the local date for the node.
      * Type: LocalDate
      */
-    DATE("Date", LocalDate.class),
+    DATE(LocalDate.class),
 
     /**
      * Returns true if the node is editable.
      * Type: Boolean
      */
-    EDITABLE("Editable", Boolean.class),
+    EDITABLE(Boolean.class),
 
     /**
      * Indicates if a popup is expanded.
      * Type: Boolean
      */
-    EXPANDED("Expanded", Boolean.class),
+    EXPANDED(Boolean.class),
 
     /**
      * Returns the focus item.
@@ -140,7 +140,7 @@ public enum AccessibleAttribute {
      * Used for controls with items such TabPaneView, TableView, ListView, etc.
      * It returns the exact Node within the control that has the focus.
      */
-    FOCUS_ITEM("FocusItem", Node.class),
+    FOCUS_ITEM(Node.class),
 
     /**
      * Returns the focus Node.
@@ -149,224 +149,224 @@ public enum AccessibleAttribute {
      * This attribute is requested to the Scene, where it maps to {@link Scene#focusOwnerProperty()}
      * The Scene can delegate the request to its current transient focus container.
      */
-    FOCUS_NODE("FocusNode", Node.class),
+    FOCUS_NODE(Node.class),
 
     /**
      * Returns true if the Node is focused.
      * Type: Boolean
      */
-    FOCUSED("Focused", Boolean.class),
+    FOCUSED(Boolean.class),
 
     /**
      * Returns the font for the node
      * Type: Font
      */
-    FONT("Font", Font.class),
+    FONT(Font.class),
 
     /**
      * Returns the header for the node
      * Type: Node
      */
-    HEADER("Header", Node.class),
+    HEADER(Node.class),
 
     /**
      * Returns the help for the Node.
      * Type: String
      */
-    HELP("Help", String.class),
+    HELP(String.class),
 
     /**
      * Returns the horizontal scroll bar of a scroll pane
      * Type: Node
      */
-    HORIZONTAL_SCROLLBAR("HorizontalScrollBar", Node.class),
+    HORIZONTAL_SCROLLBAR(Node.class),
 
     /**
      * Returns the indeterminate state for the node.
      * Type: Boolean
      */
-    INDETERMINATE("Indeterminate", Boolean.class),
+    INDETERMINATE(Boolean.class),
 
     /**
      * Returns the item at the given index
      * Parameter: Integer
      * Type: Node
      */
-    ITEM_AT_INDEX("ItemAtIndex", Node.class),
+    ITEM_AT_INDEX(Node.class),
 
     /**
      * Returns the item count
      * Type: Integer
      */
-    ITEM_COUNT("ItemCount", Integer.class),
+    ITEM_COUNT(Integer.class),
 
     /**
      * Returns the index of a row or column
      * Type: Integer
      */
-    INDEX("Index", Integer.class),
+    INDEX(Integer.class),
 
     /**
      * Returns a Node that is a label for this control, or null.
      * Type: Node
      */
-    LABELED_BY("LabeledBy", Node.class),
+    LABELED_BY(Node.class),
 
     /**
      * Indicates whether a TreeItem is a leaf element or not.
      * Type: Boolean
      */
-    LEAF("Leaf", Boolean.class),
+    LEAF(Boolean.class),
 
     /**
      * Returns the line start of the given line index.
      * Parameter: Integer - line index
      * Type: Integer
      */
-    LINE_END("LineEnd", Integer.class),
+    LINE_END(Integer.class),
 
     /**
      * Returns the line index of the given character offset.
      * Parameter: Integer - character offset.
      * Type: Integer
      */
-    LINE_FOR_OFFSET("LineOffset", Integer.class),
+    LINE_FOR_OFFSET(Integer.class),
 
     /**
      * Returns the line end of the given line index.
      * Parameter: Integer - line index
      * Type: Integer
      */
-    LINE_START("LineStart", Integer.class),
+    LINE_START(Integer.class),
 
     /**
      * Returns the min value for the node.
      * Type: Double
      */
-    MIN_VALUE("MinValue", Double.class),
+    MIN_VALUE(Double.class),
 
     /**
      * Returns the max value for the node.
      * Type: Double
      */
-    MAX_VALUE("MaxValue", Double.class),
+    MAX_VALUE(Double.class),
 
     /**
      * Returns the mnemonic for the node.
      * Type: String
      */
-    MNEMONIC("Mnemonic", String.class),
+    MNEMONIC(String.class),
 
     /**
      * Returns whether the control allows for multiple selection.
      * Type: Boolean
      */
-    MULTIPLE_SELECTION("MultipleSelection", Boolean.class),
+    MULTIPLE_SELECTION(Boolean.class),
 
     /**
      * Returns the Node at the given point location.
      * Type: Node
      * Parameters: Point2D
      */
-    NODE_AT_POINT("NodeAtPoint", Node.class),
+    NODE_AT_POINT(Node.class),
 
     /**
      * Returns the char offset at the given point location.
      * Type: Integer
      * Parameters: Point2D
      */
-    OFFSET_AT_POINT("OffsetAtPoint", Integer.class),
+    OFFSET_AT_POINT(Integer.class),
 
     /**
      * Returns the orientation of a node
      * Type: Orientation
      */
-    ORIENTATION("Orientation", Orientation.class),
+    ORIENTATION(Orientation.class),
 
     /**
      * Return the overflow button for the Node.
      * Type: Node
      */
-    OVERFLOW_BUTTON("Overflow Button", Node.class),
+    OVERFLOW_BUTTON(Node.class),
 
     /**
      * Returns the parent for the Node.
      * Type: Parent
      */
-    PARENT("Parent", Parent.class),
+    PARENT(Parent.class),
 
     /**
      * Returns the parent menu.
      * Type: Node
      */
-    PARENT_MENU("ParentMenu", Node.class),
+    PARENT_MENU(Node.class),
 
     /**
      * Returns the role for the Node.
      * Type: Role
      */
-    ROLE("Role", AccessibleRole.class),
+    ROLE(AccessibleRole.class),
 
     /**
      * Returns the role description for the Node.
      * Type: String
      */
-    ROLE_DESCRIPTION("RoleDescription", String.class),
+    ROLE_DESCRIPTION(String.class),
 
     /**
      * Returns the row at the given index
      * Parameter: Integer
      * Type: Node
      */
-    ROW_AT_INDEX("RowAtIndex", Node.class),
+    ROW_AT_INDEX(Node.class),
 
     /**
      * Returns the row count
      * Type: Integer
      */
-    ROW_COUNT("RowCount", Integer.class),
+    ROW_COUNT(Integer.class),
 
     /**
      * Returns the row index of a cell
      * Type: Integer
      */
-    ROW_INDEX("RowIndex", Integer.class),
+    ROW_INDEX(Integer.class),
 
     /**
      * Returns the scene for the Node.
      * Type: Scene
      */
-    SCENE("Scene", Scene.class),
+    SCENE(Scene.class),
 
     /**
      * Returns if the item is selected (in a radio group for example)
      * Type: Boolean
      */
-    SELECTED("Selected", Boolean.class),
+    SELECTED(Boolean.class),
 
     /**
      * Returns the list of selected items
      * Type: ObservableList&lt;Node&gt;
      */
-    SELECTED_ITEMS("SelectedItems", ObservableList.class),
+    SELECTED_ITEMS(ObservableList.class),
 
     /**
      * Returns the selection end for the node.
      * Type: Integer
      */
-    SELECTION_END("SelectionEnd", Integer.class),
+    SELECTION_END(Integer.class),
 
     /**
      * Returns the selection start for the node.
      * Type: Integer
      */
-    SELECTION_START("SelectionStart", Integer.class),
+    SELECTION_START(Integer.class),
 
     /**
      * Returns the sub menu.
      * Type: Node
      */
-    SUBMENU("Submenu", Node.class),
+    SUBMENU(Node.class),
 
     /**
      * Returns the text for the Node.
@@ -378,7 +378,7 @@ public enum AccessibleAttribute {
      * </ul>
      * Type: String
      */
-    TEXT("Text", String.class),
+    TEXT(String.class),
 
     /**
      * Returns a tree item (Role.TREE_ITEM or Role.TREE_TABLE_ITEM) at the given
@@ -388,7 +388,7 @@ public enum AccessibleAttribute {
      * Parameter: Integer
      * Type: Node
      */
-    TREE_ITEM_AT_INDEX("TreeItemAtIndex", Node.class),
+    TREE_ITEM_AT_INDEX(Node.class),
 
     /**
      * Returns the number of tree items that are expanded descendants of the current
@@ -396,7 +396,7 @@ public enum AccessibleAttribute {
      * this will return the count from the root tree item.
      * Type: Node
      */
-    TREE_ITEM_COUNT("TreeItemCount", Integer.class),
+    TREE_ITEM_COUNT(Integer.class),
 
     /**
      * Returns the parent of a Role.TREE_ITEM (as another Role.TREE_ITEM, or if
@@ -404,44 +404,38 @@ public enum AccessibleAttribute {
      * node whatever it is (most probably Role.TREE_VIEW or Role.TREE_TABLE_VIEW)
      * Type: Node
      */
-    TREE_ITEM_PARENT("TreeItemParent", Node.class),
+    TREE_ITEM_PARENT(Node.class),
 
     /**
      * Returns the value for the node.
      * Type: Double
      */
-    VALUE("Value", Double.class),
+    VALUE(Double.class),
 
     /**
      * Returns the vertical scroll bar of a scroll pane
      * Type: Node
      */
-    VERTICAL_SCROLLBAR("VerticalScrollBar", Node.class),
+    VERTICAL_SCROLLBAR(Node.class),
 
     /**
      * Returns if the visibility for the node.
      * Type: Boolean
      */
-    VISIBLE("VISIBLE", Boolean.class),
+    VISIBLE(Boolean.class),
 
     /**
      * Indicates whether a Hyperlink has been visited or not.
      * This is an undocumented Mac-only attribute.
      * Type: Boolean
      */
-    VISITED("Visited", Boolean.class),
+    VISITED(Boolean.class),
     ;
 
-    private String name;
     private Class<?> returnClass;
 
-    AccessibleAttribute(String name, Class<?> returnClass) {
-        this.name = name;
+    AccessibleAttribute(Class<?> returnClass) {
         this.returnClass = returnClass;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public Class<?> getReturnType() {
