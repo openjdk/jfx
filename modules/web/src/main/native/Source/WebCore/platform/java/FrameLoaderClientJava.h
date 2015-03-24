535b07923dadbc9199e67460295e4a0d37b11bd3
Kevin Rushforth <kcr@openjdk.org>
2015-03-24 13:27:08 -0700

    RT-40341: Update copyright header for files modified in 2015
    Update files modified in 9-dev, but not yet modified in 8u-dev

--- modules/web/src/main/native/Source/WebCore/platform/java/FrameLoaderClientJava.h
+++ modules/web/src/main/native/Source/WebCore/platform/java/FrameLoaderClientJava.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
  */
 #ifndef FrameLoaderClientJava_H
 #define FrameLoaderClientJava_H
--- modules/web/src/main/native/Source/WebCore/platform/network/java/SocketStreamHandleJava.cpp
+++ modules/web/src/main/native/Source/WebCore/platform/network/java/SocketStreamHandleJava.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2015, Oracle and/or its affiliates. All rights reserved.
  */
 #include "config.h"
 #include "SocketStreamHandle.h"
