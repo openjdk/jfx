3f427b129d219e56a50477ec7467f238d81a3f3c
Kevin Rushforth <kcr@openjdk.org>
2015-11-12 12:39:11 -0800

    8141663: Update copyright header for files modified in 2015
    Reviewed-by: ckyang

--- modules/base/src/test/java/test/javafx/beans/property/PropertiesTest.java
+++ modules/base/src/test/java/test/javafx/beans/property/PropertiesTest.java
@@ -1,7 +1,7 @@
 package test.javafx.beans.property;
 
 /*
- * Copyright (c) 2011, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
