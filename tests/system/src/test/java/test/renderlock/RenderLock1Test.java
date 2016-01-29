6a525583df839d4250b2c19bbbb75a5d8861aace
David Hill <ddhill@openjdk.org>
2016-01-29 13:04:56 -0500

    8148489: conditional execution of unstable tests
    Reviewed-by: kcr

--- tests/system/src/test/java/test/renderlock/RenderLock1Test.java
+++ tests/system/src/test/java/test/renderlock/RenderLock1Test.java
@@ -25,11 +25,13 @@
 
 package test.renderlock;
 
+import static org.junit.Assume.assumeTrue;
 import org.junit.Test;
 
 public class RenderLock1Test extends RenderLockCommon {
     @Test
     public void windowCloseTest() throws Exception {
+        assumeTrue(Boolean.getBoolean("unstable.test"));
         doWindowCloseTest();
     }
 }
