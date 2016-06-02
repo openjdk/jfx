98a35a27cb1a8e39435c33d66b2fdf4cdf52802e
Phil Race <prr@openjdk.org>
2016-06-02 14:29:33 -0700

    8150181: javafx print jobs take 60 times longer than javax.print
    Reviewed-by: prr

--- modules/graphics/src/main/java/com/sun/prism/j2d/print/J2DPrinterJob.java
+++ modules/graphics/src/main/java/com/sun/prism/j2d/print/J2DPrinterJob.java
@@ -351,8 +351,6 @@ public class J2DPrinterJob implements PrinterJobImpl {
     }
 
     private void updatePageLayout() {
-
-        PageLayout oldLayout = null;
         Media media = (Media)printReqAttrSet.get(Media.class);
         Paper paper = j2dPrinter.getPaperForMedia(media);
         OrientationRequested o = (OrientationRequested)
@@ -932,8 +930,8 @@ public class J2DPrinterJob implements PrinterJobImpl {
             currPageInfo = null;
             pageDone = true;
             synchronized (monitor) {
-                if (jobDone) {
-                    return false;
+                if (newPageInfo == null) {
+                    monitor.notify(); // page is printed and no new page to print
                 }
                 while (newPageInfo == null && !jobDone && !jobError) {
                     try {
@@ -1084,7 +1082,9 @@ public class J2DPrinterJob implements PrinterJobImpl {
                 while (!pageDone && !jobDone && !jobError) {
                     synchronized (monitor) {
                         try {
-                            monitor.wait(1000);
+                            if (!pageDone) {
+                                monitor.wait(1000);
+                            }
                         } catch (InterruptedException e) {
                         }
                     }
