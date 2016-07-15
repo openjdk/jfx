41b86c74239ec8bb7cb5028cf8bfd097c57b9ea4
Pete Brunet <ptbrunet@openjdk.org>
2016-07-15 13:17:50 -0400

    8159723: Crash, JavaFX, JAWS - When pressing tab to change focus in HelloComboBox
    Reviewed-by: ddhill, jgiles

--- modules/graphics/src/main/native-glass/win/GlassAccessible.cpp
+++ modules/graphics/src/main/native-glass/win/GlassAccessible.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -133,7 +133,7 @@ static jfieldID fid_pDblVal;
 
 /* static */ HRESULT GlassAccessible::copyString(JNIEnv *env, jstring jString, BSTR* pbstrVal)
 {
-    if (pbstrVal != NULL) {
+    if (pbstrVal != NULL && jString != NULL) {
         UINT length = env->GetStringLength(jString);
         const jchar* ptr = env->GetStringCritical(jString, NULL);
         if (ptr != NULL) {
