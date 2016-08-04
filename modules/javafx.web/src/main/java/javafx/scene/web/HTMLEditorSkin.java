2fc318d72e7146ae34d63d896b370805642d5416
Murali Billa <mbilla@openjdk.org>
2016-08-04 18:38:31 +0530

    8090216: HTMLEditor: font bold doesn't work when an indent is set
    Reviewed-by: kcr, arajkumar

--- modules/javafx.web/src/main/java/javafx/scene/web/HTMLEditorSkin.java
+++ modules/javafx.web/src/main/java/javafx/scene/web/HTMLEditorSkin.java
@@ -943,7 +943,7 @@ public class HTMLEditorSkin extends SkinBase<HTMLEditor> {
 
             // if the format value is then we assume that we're dealing with a paragraph,
             // which seems to correspond with the HTML output we receive.
-            if ((resetToolbarState || htmlTag.equals("<>") || htmlTag.equalsIgnoreCase("<div>"))) {
+            if ((resetToolbarState || htmlTag.equals("<>") || htmlTag.equalsIgnoreCase("<div>") || htmlTag.equalsIgnoreCase("<blockquote>"))) {
                 formatComboBox.setValue(resources.getString("paragraph"));
             } else if (format != null && ! format.equalsIgnoreCase(comboFormatValue)) {
                 formatComboBox.setValue(comboFormatValue);
