8a5124b886deb8366435b2dde6f2c34c2f50c310
Arunprasad Rajkumar <arajkumar@openjdk.org>
2016-08-11 10:47:44 +0530

    8162715: Add regression test for JDK-8161258
    Reviewed-by: kcr, mbilla

--- modules/javafx.web/src/test/java/test/javafx/scene/web/MiscellaneousTest.java
+++ modules/javafx.web/src/test/java/test/javafx/scene/web/MiscellaneousTest.java
@@ -31,14 +31,17 @@ import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker.State;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import netscape.javascript.JSObject;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 import org.w3c.dom.Document;
@@ -143,6 +146,70 @@ public class MiscellaneousTest extends TestBase {
            // from someother thread. It should throw an exception.
            obj.resultObject.toString();
      }
+
+    // JDK-8162715
+    public class TimerCallback {
+        private static final int INTERVAL_COUNT = 20;
+        private final CountDownLatch latch = new CountDownLatch(INTERVAL_COUNT);
+        private class Stat {
+            private long firedTime;
+            private long createdTime;
+            private long interval;
+        }
+        private Stat[] stats = new Stat[INTERVAL_COUNT];
+
+        public void call(long createdTime, long interval, int index) {
+            Stat stat = new Stat();
+            stat.firedTime = System.currentTimeMillis();
+            stat.createdTime = createdTime;
+            stat.interval = interval;
+            stats[index] = stat;
+            latch.countDown();
+        }
+    }
+
+    @Test(timeout = 30000) public void testDOMTimer() {
+        final TimerCallback timer = new TimerCallback();
+        final WebEngine webEngine = createWebEngine();
+        submit(() -> {
+            final JSObject window = (JSObject) webEngine.executeScript("window");
+            assertNotNull(window);
+            window.setMember("timer", timer);
+            // Try various intervals
+            for (int i = 0; i < timer.INTERVAL_COUNT; i++) {
+                int timeout = i * (1000 / timer.INTERVAL_COUNT);
+                webEngine.executeScript("window.setTimeout("
+                                      + "timer.call.bind(timer, Date.now(),"
+                                      // pass 'i' to call to test time
+                                      + timeout +"," + i + "),"
+                                      // set 'i' as a timeout interval
+                                      + timeout + ")");
+            }
+
+        });
+
+        try {
+            timer.latch.await();
+        } catch (InterruptedException e) {
+            throw new AssertionError(e);
+        }
+        for (TimerCallback.Stat stat : timer.stats) {
+            assertNotNull(stat);
+            final String msg = String.format(
+                    "expected delta:%d, actual delta:%d",
+                    stat.interval,
+                    stat.firedTime - stat.createdTime);
+            // Timer should not fire too early. Added 20 ms offset to compensate
+            // the floating point approximation issues while dealing with timer.
+            assertTrue(msg,
+                    ((stat.firedTime + 20) - stat.createdTime) >= stat.interval);
+            // Timer should not be too late. Since it is not a real time system,
+            // we can't expect the timer to be fire at exactly on the requested
+            // time, give a 1000 ms extra time.
+            assertTrue(msg,
+                    (stat.firedTime - stat.createdTime) <= (stat.interval + 1000));
+        }
+    }
 
     private WebEngine createWebEngine() {
         return submit(() -> new WebEngine());
