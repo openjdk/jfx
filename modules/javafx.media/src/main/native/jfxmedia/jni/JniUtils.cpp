05ca6c3f728cbbef933b155d7533238c2213e042
Alexander Matveev <almatvee@openjdk.org>
2016-09-19 15:05:02 -0700

    8089301: [parfait] JNI exception pending in rt/modules/media/src/main/native/jfxmedia/platform/gstreamer/GstMedia.cpp
    Reviewed-by: ddehaven

--- modules/javafx.media/src/main/native/jfxmedia/jni/JniUtils.cpp
+++ modules/javafx.media/src/main/native/jfxmedia/jni/JniUtils.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -120,6 +120,12 @@ CJavaEnvironment::CJavaEnvironment(JavaVM *jvm) :
     if (jvm) {
         environment = GetJavaEnvironment(jvm, attached);
     }
+}
+
+CJavaEnvironment::CJavaEnvironment(JNIEnv *env) :
+    attached(false)
+{
+    environment = env;
 }
 
 CJavaEnvironment::~CJavaEnvironment()
