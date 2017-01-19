0aef33bb01d88dac1057139d87b05c890db97d98
Ajit Ghaisas <aghaisas@openjdk.org>
2017-01-19 21:10:58 +1300

    8171071: SplitPane is not visible after visible property changes
    Reviewed-by: jgiles

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/SplitPaneSkin.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/SplitPaneSkin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -126,9 +126,7 @@ public class SplitPaneSkin extends SkinBase<SplitPane> {
         final double sw = s.getWidth();
         final double sh = s.getHeight();
 
-        if (!s.isVisible() ||
-                (horizontal ? sw == 0 : sh == 0) ||
-                contentRegions.isEmpty()) {
+        if ((horizontal ? sw == 0 : sh == 0) || contentRegions.isEmpty()) {
             return;
         }
 
