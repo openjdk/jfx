472ba5e9287d547794949574d665e4de9611281b
Vivek Rao <vrao@openjdk.org>
2017-06-27 10:14:09 -0700

    8179399: Call trim() when loaded jre.list
    Reviewed-by: vdrozdov, cbensen

--- modules/jdk.packager/src/main/java/jdk/packager/internal/RedistributableModules.java
+++ modules/jdk.packager/src/main/java/jdk/packager/internal/RedistributableModules.java
@@ -25,15 +25,12 @@
 package jdk.packager.internal;
 
 
-import com.oracle.tools.packager.IOUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Optional;
 
 import jdk.tools.jlink.internal.packager.AppRuntimeImageBuilder;
@@ -44,14 +41,11 @@ import java.lang.module.ModuleReference;
 import java.lang.module.ModuleReader;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public final class RedistributableModules {
@@ -61,15 +55,12 @@ public final class RedistributableModules {
     private RedistributableModules() {}
 
     public static String stripComments(String line) {
-        String result = "";
-        int i = line.indexOf(";");
+        String result = line.trim();
+        int i = result.indexOf(";");
 
         if (i >= 0) {
-          result = line.substring(0, i);
+          result = result.substring(0, i);
           result = result.trim();
-        }
-        else {
-            result = line;
         }
 
         return result;
@@ -88,7 +79,7 @@ public final class RedistributableModules {
 
             try {
                 reader = mref.get().open();
-            } catch (IOException ex) {
+            } catch (NoSuchElementException | IOException ex) {
             }
 
             if (reader != null) {
@@ -117,7 +108,7 @@ public final class RedistributableModules {
                                     String module = stripComments(line);
 
                                     if (!module.isEmpty()) {
-                                        result.add(line);
+                                        result.add(module);
                                     }
                                 }
                             } catch (IOException ex) {
