aa7ba89ed18d2719ec46c397681065e3dffc4b8a
Prem Balakrishnan <pkbalakr@openjdk.org>
2017-11-17 10:48:22 +0530

    8184270: NullPointerException when using Combobox in combination with accessibility app
    Reviewed-by: aghaisas, kcr

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/ComboBoxListViewSkin.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/ComboBoxListViewSkin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -650,8 +650,10 @@ public class ComboBoxListViewSkin<T> extends ComboBoxPopupControl<T> {
                 }
                 return title;
             }
-            case SELECTION_START: return getEditor().getSelection().getStart();
-            case SELECTION_END: return getEditor().getSelection().getEnd();
+            case SELECTION_START:
+                return (getEditor() != null) ? getEditor().getSelection().getStart() : null;
+            case SELECTION_END:
+                return (getEditor() != null) ? getEditor().getSelection().getEnd() : null;
             default: return super.queryAccessibleAttribute(attribute, parameters);
         }
     }
