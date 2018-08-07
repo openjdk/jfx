dfb0ff8d022d042285851698f9748f7e55c80f73
Kevin Rushforth <kcr@openjdk.org>
2018-08-07 10:22:45 -0700

    8201563: Update copyright header for files modified in 2018 8202357: Extra chars in copyright header in ModuleHelper.java
    Reviewed-by: aghaisas

--- modules/javafx.web/src/main/native/Source/WebCore/platform/network/java/SocketStreamHandleImpl.h
+++ modules/javafx.web/src/main/native/Source/WebCore/platform/network/java/SocketStreamHandleImpl.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2009 Apple Inc. All rights reserved.
  * Copyright (C) 2009, 2011 Google Inc.  All rights reserved.
- * Copyright (c) 2012, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2018, Oracle and/or its affiliates. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
