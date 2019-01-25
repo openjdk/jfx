aa4b6590c9314223d7c7567bef2eeff3ecf8d190
Kevin Rushforth <kcr@openjdk.org>
2019-01-25 14:08:31 -0800

    8215567: Change JavaFX release version to 13
    Reviewed-by: prr, jvos

--- modules/javafx.base/src/test/java/test/com/sun/javafx/runtime/VersionInfoTest.java
+++ modules/javafx.base/src/test/java/test/com/sun/javafx/runtime/VersionInfoTest.java
@@ -89,7 +89,7 @@ public class VersionInfoTest {
         String version = VersionInfo.getVersion();
         // Need to update major version number when we develop the next
         // major release.
-        assertTrue(version.startsWith("12"));
+        assertTrue(version.startsWith("13"));
         String runtimeVersion = VersionInfo.getRuntimeVersion();
         assertTrue(runtimeVersion.startsWith(version));
     }
