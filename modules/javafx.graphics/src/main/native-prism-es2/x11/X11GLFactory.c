bd02deebf7f64dce66f8e4a2c07370e52dbefaa7
Johan Vos <jvos@openjdk.org>
2019-07-17 22:22:20 +0200

    8223760: support static builds
    Reviewed-by: kcr

--- modules/javafx.graphics/src/main/native-prism-es2/x11/X11GLFactory.c
+++ modules/javafx.graphics/src/main/native-prism-es2/x11/X11GLFactory.c
@@ -33,6 +33,22 @@
 
 #include "../PrismES2Defs.h"
 #include "com_sun_prism_es2_X11GLFactory.h"
+#ifdef STATIC_BUILD
+JNIEXPORT jint JNICALL
+JNI_OnLoad_prism_es2(JavaVM *vm, void * reserved) {
+#ifdef JNI_VERSION_1_8
+    //min. returned JNI_VERSION required by JDK8 for builtin libraries
+    JNIEnv *env;
+    if ((*vm)->GetEnv(vm, (void **)&env, JNI_VERSION_1_8) != JNI_OK) {
+        return JNI_VERSION_1_4;
+    }
+    return JNI_VERSION_1_8;
+#else
+    return JNI_VERSION_1_4;
+#endif
+}
+#endif
+
 
 void setGLXAttrs(jint *attrs, int *glxAttrs) {
     int index = 0;
