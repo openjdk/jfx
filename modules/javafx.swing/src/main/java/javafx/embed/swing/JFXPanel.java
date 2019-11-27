1d670f18d16d060dc40e83733cb3595c41df2a97
Florian Kirmaier <fkirmaier@openjdk.org>
2019-11-27 16:20:12 +0000

    8200224: Multiple press event when JFXPanel gains focus
    Reviewed-by: kcr, psadhukhan

--- modules/javafx.swing/src/main/java/javafx/embed/swing/JFXPanel.java
+++ modules/javafx.swing/src/main/java/javafx/embed/swing/JFXPanel.java
@@ -446,13 +446,11 @@ public class JFXPanel extends JComponent {
             (e.getButton() == MouseEvent.BUTTON1)) {
             if (isFocusable() && !hasFocus()) {
                 requestFocus();
-                // this focus request event goes to eventqueue and will be
-                // asynchronously handled so MOUSE_PRESSED event will not be
-                // honoured by FX immediately due to lack of focus in fx
-                // component. Fire the same MOUSE_PRESSED event after
-                // requestFocus() so that 2nd mouse press will be honoured
-                // since now fx have focus
-                jfxPanelIOP.postEvent(this, e);
+                // The extra simulated mouse pressed event is removed by making the JavaFX scene focused.
+                // It is safe, because in JavaFX only the method "setFocused(true)" is called,
+                // which doesn't have any side-effects when called multiple times.
+                int focusCause = AbstractEvents.FOCUSEVENT_ACTIVATED;
+                stagePeer.setFocused(true, focusCause);
             }
         }
 
