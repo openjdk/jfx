1c7a4eb1736567fe84984bbbe4044a80b7ba4ad5
Johan Vos <johan@lodgon.com>
2020-02-12 10:29:18 +0100

    Add support for seek when the protocol is "resource"

--- modules/javafx.media/src/main/java/com/sun/media/jfxmedia/locator/ConnectionHolder.java
+++ modules/javafx.media/src/main/java/com/sun/media/jfxmedia/locator/ConnectionHolder.java
@@ -258,7 +258,7 @@ public abstract class ConnectionHolder {
         boolean isSeekable() {
             return (urlConnection instanceof HttpURLConnection) ||
                    (urlConnection instanceof JarURLConnection) ||
-                   isJRT();
+                   isJRT() || isResource();
         }
 
         boolean isRandomAccess() {
@@ -299,7 +299,7 @@ public abstract class ConnectionHolder {
                         Locator.closeConnection(tmpURLConnection);
                     }
                 }
-            } else if ((urlConnection instanceof JarURLConnection) || isJRT()) {
+            } else if ((urlConnection instanceof JarURLConnection) || isJRT() || isResource()) {
                 try {
                     closeConnection();
 
@@ -342,6 +342,12 @@ public abstract class ConnectionHolder {
             String scheme = uri.getScheme().toLowerCase();
             return "jrt".equals(scheme);
         }
+
+        private boolean isResource() {
+            String scheme = uri.getScheme().toLowerCase();
+            return "resource".equals(scheme);
+        }
+
     }
 
     // A "ConnectionHolder" that "reads" from a ByteBuffer, generally loaded from
