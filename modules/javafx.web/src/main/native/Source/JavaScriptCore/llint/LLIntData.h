cf47a951ee9191761de3e2f0ff38c9719941c536
Arun Joseph <ajoseph@openjdk.org>
2020-02-20 05:33:04 +0000

    8239454: LLIntData : invalid opcode returned for 16 and 32 bit wide instructions
    Reviewed-by: ghb, kcr

--- modules/javafx.web/src/main/native/Source/JavaScriptCore/llint/LLIntData.h
+++ modules/javafx.web/src/main/native/Source/JavaScriptCore/llint/LLIntData.h
@@ -105,7 +105,7 @@ inline Opcode getOpcodeWide16(OpcodeID id)
 #if ENABLE(COMPUTED_GOTO_OPCODES)
     return g_opcodeMapWide16[id];
 #else
-    return static_cast<Opcode>(id - numOpcodeIDs);
+    return static_cast<Opcode>(id + numOpcodeIDs);
 #endif
 }
 
@@ -114,7 +114,7 @@ inline Opcode getOpcodeWide32(OpcodeID id)
 #if ENABLE(COMPUTED_GOTO_OPCODES)
     return g_opcodeMapWide32[id];
 #else
-    return static_cast<Opcode>(id - numOpcodeIDs);
+    return static_cast<Opcode>(id + numOpcodeIDs*2);
 #endif
 }
 
