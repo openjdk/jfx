c771c242b2809e0523fa5a0663aecbdb501cae92
John Neffenger <john@status6.com>
2020-06-25 20:35:01 -0700

    8248381: Create a daemon thread for MonocleTimer

--- modules/javafx.graphics/src/main/java/com/sun/glass/ui/monocle/MonocleTimer.java
+++ modules/javafx.graphics/src/main/java/com/sun/glass/ui/monocle/MonocleTimer.java
@@ -34,6 +34,9 @@ import java.util.concurrent.TimeUnit;
  * Monocle implementation class for Timer.
  */
 final class MonocleTimer extends Timer {
+    private static final String THREAD_NAME = "Monocle Timer";
+    private static final int POOL_SIZE = 1;
+
     private static ScheduledThreadPoolExecutor scheduler;
     private ScheduledFuture<?> task;
 
@@ -51,7 +54,11 @@ final class MonocleTimer extends Timer {
 
     @Override protected long _start(final Runnable runnable, int period) {
         if (scheduler == null) {
-            scheduler = new ScheduledThreadPoolExecutor(1);
+            scheduler = new ScheduledThreadPoolExecutor(POOL_SIZE, target -> {
+                Thread thread = new Thread(target, THREAD_NAME);
+                thread.setDaemon(true);
+                return thread;
+            });
         }
 
         task = scheduler.scheduleAtFixedRate(runnable, 0, period, TimeUnit.MILLISECONDS);
