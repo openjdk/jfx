0281aa12117871781f48f700dcb19805e2619e08
Matthias Bläsing <mblaesing@doppel-helix.eu>
2020-12-13 02:14:03 +0100

    8242361: Add missing copyright headers and integrate test into systemTests

--- tests/system/src/testapp7/java/mymod/module-info.java
+++ tests/system/src/testapp7/java/mymod/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,3 +23,11 @@
  * questions.
  */
 
+module mymod {
+    requires javafx.controls;
+    requires javafx.media;
+    requires javafx.web;
+    requires javafx.graphics;
+    requires javafx.base;
+    exports myapp7;
+}
