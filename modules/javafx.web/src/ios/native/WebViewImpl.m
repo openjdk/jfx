08963902aef7f21601de44be3ad93ff582513c7b
Kevin Rushforth <kcr@openjdk.org>
2020-12-16 05:09:17 -0800

    Respond to feedback: change masterWindow to mainWindow in iOS files

--- modules/javafx.web/src/ios/native/WebViewImpl.m
+++ modules/javafx.web/src/ios/native/WebViewImpl.m
@@ -62,8 +62,8 @@ jstring createJString(JNIEnv *env, NSString *nsStr) {
 
 @implementation WebViewImpl
 
-@synthesize window;     //known as masterWindow in glass
-@synthesize windowView; //known as masterWindowHost in glass
+@synthesize window;     //known as mainWindow in glass
+@synthesize windowView; //known as mainWindowHost in glass
 
 - (void)setWidth:(CGFloat)value {
     width = value;
@@ -141,8 +141,8 @@ jstring createJString(JNIEnv *env, NSString *nsStr) {
     loadingLabel.textAlignment = UITextAlignmentCenter;
     //[loadingLabel.layer setAnchorPoint:CGPointMake(0.0f, 0.0f)];
 
-    window = [self getWindow];                          //known as masterWindow in glass
-    windowView = [[window rootViewController] view];    //known as masterWindowHost in glass
+    window = [self getWindow];                          //known as mainWindow in glass
+    windowView = [[window rootViewController] view];    //known as mainWindowHost in glass
 }
 
 - (JNIEnv *)getJNIEnv {
