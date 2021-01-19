c8accadc748cdb0c2ac26d34b40d2c168ba29e3c
bourgesl <bourges.laurent@gmail.com>
2021-01-19 16:02:41 +0100

    updated Version

--- modules/javafx.graphics/src/main/java/com/sun/marlin/Version.java
+++ modules/javafx.graphics/src/main/java/com/sun/marlin/Version.java
@@ -27,7 +27,7 @@ package com.sun.marlin;
 
 public final class Version {
 
-    private static final String VERSION = "marlinFX-0.9.3.2-Unsafe-OpenJDK";
+    private static final String VERSION = "marlinFX-0.9.3.2-Unsafe-OpenJFX";
 
     public static String getVersion() {
         return VERSION;
