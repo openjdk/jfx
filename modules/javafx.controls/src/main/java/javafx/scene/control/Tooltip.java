bc6aaba73056199a61a1cfc55a8e07dafb2b2776
xardif <xardif@gmail.com>
2021-02-05 11:37:55 +0100

    8261221: Tooltip bigger than screen size blinks - shows and hides over and over again

--- modules/javafx.controls/src/main/java/javafx/scene/control/Tooltip.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/Tooltip.java
@@ -859,6 +859,7 @@ public class Tooltip extends PopupControl {
 
         private boolean hideOnExit;
         private boolean cssForced = false;
+        private boolean mouseInsideTooltip = true;
 
         TooltipBehavior(final boolean hideOnExit) {
             this.hideOnExit = hideOnExit;
@@ -933,6 +934,7 @@ public class Tooltip extends PopupControl {
                 visibleTooltip.hide();
                 visibleTooltip = null;
                 hoveredNode = null;
+                mouseInsideTooltip = false;
             });
 
             leftTimer.setOnFinished(event -> {
@@ -942,6 +944,7 @@ public class Tooltip extends PopupControl {
                     visibleTooltip.hide();
                     visibleTooltip = null;
                     hoveredNode = null;
+                    mouseInsideTooltip = false;
                 }
             });
         }
@@ -1028,6 +1031,11 @@ public class Tooltip extends PopupControl {
          * stop the HIDE_TIMER, hide the tooltip, and start the LEFT_TIMER.
          */
         private EventHandler<MouseEvent> LEAVING_HANDLER = (MouseEvent event) -> {
+            if( mouseInsideTooltip )
+            {
+                return;
+            }
+
             // detect bogus mouse exit events, if it didn't really move then ignore it
             if (activationTimer.getStatus() == Timeline.Status.RUNNING) {
                 activationTimer.stop();
@@ -1063,6 +1071,16 @@ public class Tooltip extends PopupControl {
             hoveredNode = null;
             activatedTooltip = null;
             visibleTooltip = null;
+            mouseInsideTooltip = false;
+        };
+
+        private EventHandler<MouseEvent> TOOLTIP_MOUSE_ENTERED = (MouseEvent event) -> {
+            mouseInsideTooltip = true;
+        };
+
+        private EventHandler<MouseEvent> TOOLTIP_MOUSE_EXITED = (MouseEvent event) -> {
+            mouseInsideTooltip = false;
+            KILL_HANDLER.handle( null );
         };
 
         private void install(Node node, Tooltip t) {
@@ -1074,6 +1092,9 @@ public class Tooltip extends PopupControl {
             node.addEventHandler(MouseEvent.MOUSE_MOVED, MOVE_HANDLER);
             node.addEventHandler(MouseEvent.MOUSE_EXITED, LEAVING_HANDLER);
             node.addEventHandler(MouseEvent.MOUSE_PRESSED, KILL_HANDLER);
+            t.addEventFilter( MouseEvent.MOUSE_ENTERED_TARGET, TOOLTIP_MOUSE_ENTERED );
+            t.addEventFilter( MouseEvent.MOUSE_EXITED_TARGET, TOOLTIP_MOUSE_EXITED );
+            t.addEventFilter( MouseEvent.MOUSE_PRESSED, KILL_HANDLER );
             node.getProperties().put(TOOLTIP_PROP_KEY, t);
         }
 
@@ -1084,6 +1105,9 @@ public class Tooltip extends PopupControl {
             node.removeEventHandler(MouseEvent.MOUSE_PRESSED, KILL_HANDLER);
             Tooltip t = (Tooltip)node.getProperties().get(TOOLTIP_PROP_KEY);
             if (t != null) {
+                t.removeEventFilter( MouseEvent.MOUSE_ENTERED_TARGET, TOOLTIP_MOUSE_ENTERED );
+                t.removeEventFilter( MouseEvent.MOUSE_EXITED_TARGET, TOOLTIP_MOUSE_EXITED );
+                t.removeEventFilter( MouseEvent.MOUSE_PRESSED, KILL_HANDLER );
                 node.getProperties().remove(TOOLTIP_PROP_KEY);
                 if (t.equals(visibleTooltip) || t.equals(activatedTooltip)) {
                     KILL_HANDLER.handle(null);
