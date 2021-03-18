4ed3f72f26806db056dcc809bd409fb17a7f6395
arapte <ambarish.rapte@oracle.com>
2021-03-18 18:00:35 +0530

    unit test

--- modules/javafx.controls/src/main/java/com/sun/javafx/scene/control/ControlAcceleratorSupport.java
+++ modules/javafx.controls/src/main/java/com/sun/javafx/scene/control/ControlAcceleratorSupport.java
@@ -272,4 +272,9 @@ public class ControlAcceleratorSupport {
 
         return null;
     }
+
+    // --- Test method
+    static int testGetListenerMapSize() {
+        return changeListenerMap.size();
+    }
 }
