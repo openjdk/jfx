015dad0726fbd9cff731219b0cd24f727fc6c9e4
John Hendrikx <jhendrikx@openjdk.org>
2021-04-01 12:49:39 +0000

    8264330: Scene MouseHandler is referencing removed nodes
    Reviewed-by: kcr

--- modules/javafx.graphics/src/main/java/javafx/scene/Scene.java
+++ modules/javafx.graphics/src/main/java/javafx/scene/Scene.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -3633,6 +3633,7 @@ public class Scene implements EventTarget {
             currentEventTarget = currentEventTargets.size() > 0
                     ? currentEventTargets.get(0) : null;
             pdrEventTarget.clear();
+            pdrEventTargets.clear();
         }
 
         public void enterFullPDR(EventTarget gestureSource) {
