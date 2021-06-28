b648e3749940c6b16d1ec4560a2d60ac1ab25ccb
Thiago M Sayao <thiago.sayao@gmail.com>
2021-06-28 13:38:33 -0300

    Fix for JDK-8210973

--- modules/javafx.graphics/src/main/native-glass/gtk/glass_window.cpp
+++ modules/javafx.graphics/src/main/native-glass/gtk/glass_window.cpp
@@ -706,7 +706,7 @@ WindowFrameExtents WindowContextTop::utility_extents = {28, 1, 1, 1};
 
 
 WindowContextTop::WindowContextTop(jobject _jwindow, WindowContext* _owner, long _screen,
-        WindowFrameType _frame_type, WindowType type, GdkWMFunction wmf) :
+        WindowFrameType _frame_type, WindowType type, GdkWMFunction wmf, bool _modal) :
             WindowContextBase(),
             screen(_screen),
             frame_type(_frame_type),
@@ -719,6 +719,7 @@ WindowContextTop::WindowContextTop(jobject _jwindow, WindowContext* _owner, long
             location_assigned(false),
             size_assigned(false),
             on_top(false),
+            modal(_modal),
             requested_bounds()
 {
     jwindow = mainEnv->NewGlobalRef(_jwindow);
@@ -739,6 +740,10 @@ WindowContextTop::WindowContextTop(jobject _jwindow, WindowContext* _owner, long
 
     if (type == UTILITY) {
         gtk_window_set_type_hint(GTK_WINDOW(gtk_widget), GDK_WINDOW_TYPE_HINT_UTILITY);
+    }
+
+    if (modal) {
+        gtk_window_set_keep_above(GTK_WINDOW(gtk_widget), TRUE);
     }
 
 //    glong xdisplay = (glong)mainEnv->GetStaticLongField(jApplicationCls, jApplicationDisplay);
@@ -1864,7 +1869,7 @@ void WindowContextChild::enter_fullscreen() {
     }
 
     full_screen_window = new WindowContextTop(jwindow, NULL, 0L, UNTITLED,
-                                                NORMAL, (GdkWMFunction) 0);
+                                                NORMAL, (GdkWMFunction) 0, false);
     int x, y, w, h;
     gdk_window_get_origin(gdk_window, &x, &y);
 #ifdef GLASS_GTK3
