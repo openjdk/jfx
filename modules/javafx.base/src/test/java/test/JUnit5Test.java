233fdb808e7abde013bd91975379345bb9bfdc37
John Hendrikx <hjohn@xs4all.nl>
2021-09-24 13:57:01 +0200

    Add JUnit 5

---
+++ modules/javafx.base/src/test/java/test/JUnit5Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnit5Test {
+
+    @Test
+    void junit5ShouldWork() {
+        System.err.println("JUnit 5 test working!");
+    }
+}
