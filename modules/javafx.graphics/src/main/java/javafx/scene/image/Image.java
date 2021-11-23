0167532bbdf8d6c558079681c95580843a0a3400
mstr2 <43553916+mstr2@users.noreply.github.com>
2021-11-23 17:07:34 +0100

    Added documentation for MIME/data mismatch

--- modules/javafx.graphics/src/main/java/javafx/scene/image/Image.java
+++ modules/javafx.graphics/src/main/java/javafx/scene/image/Image.java
@@ -93,6 +93,10 @@ import javafx.beans.property.SimpleIntegerProperty;
  * the protocol handlers that are registered for the application.
  * If a URL uses the "data" scheme, the data must be base64-encoded
  * and the MIME type must be a subtype of the {@code image} type.
+ * The MIME type must match the image format of the data contained in
+ * the URL. In case of a mismatch between MIME type and image format,
+ * the image will be loaded if the image format can be determined by
+ * JavaFX, and a warning will be logged.
  *
  * <p>Use {@link ImageView} for displaying images loaded with this
  * class. The same {@code Image} instance can be displayed by multiple
