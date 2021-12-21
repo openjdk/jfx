c7b22089802dbe6a1fbb5f5cc0ed5a49781e60aa
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-12-21 17:46:07 +0530

    Add license header and add space around operators

--- tests/manual/controls/SelectTableViewTest.java
+++ tests/manual/controls/SelectTableViewTest.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
 import javafx.application.Application;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
@@ -25,18 +50,18 @@ public class SelectTableViewTest extends Application {
 //      tableView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
         final ObservableList<TableColumn<String[], ?>> columns = tableView.getColumns();
-        for(int i=0; i<COL_COUNT; i++) {
+        for(int i = 0; i < COL_COUNT; i++) {
             TableColumn<String[], String> column = new TableColumn<>("Col"+i);
             final int colIndex=i;
-            column.setCellValueFactory((cell)->new SimpleStringProperty(cell.getValue()[colIndex]));
+            column.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue()[colIndex]));
             column.setPrefWidth(150);
             columns.add(column);
         }
 
         ObservableList<String[]> items = tableView.getItems();
-        for(int i=0; i<ROW_COUNT; i++) {
+        for(int i = 0; i < ROW_COUNT; i++) {
             String[] rec = new String[COL_COUNT];
-            for(int j=0; j<rec.length; j++) {
+            for(int j = 0; j < rec.length; j++) {
                 rec[j] = i+":"+j;
             }
             items.add(rec);
@@ -60,12 +85,12 @@ public class SelectTableViewTest extends Application {
         root.setRight(new VBox(6, selectAll, selectToStart, selectToEnd, selectPrevious, selectNext, clearSelection));
         stage.setScene(new Scene(root, 600, 600));
 
-        selectAll.setOnAction((e)->selectAll(tableView));
-        clearSelection.setOnAction((e)->clearSelection(tableView));
-        selectToStart.setOnAction((e)->selectToStart(tableView));
-        selectToEnd.setOnAction((e)->selectToLast(tableView));
-        selectPrevious.setOnAction((e)->selectPrevious(tableView));
-        selectNext.setOnAction((e)->selectNext(tableView));
+        selectAll.setOnAction(e -> selectAll(tableView));
+        clearSelection.setOnAction(e -> clearSelection(tableView));
+        selectToStart.setOnAction(e -> selectToStart(tableView));
+        selectToEnd.setOnAction(e -> selectToLast(tableView));
+        selectPrevious.setOnAction(e -> selectPrevious(tableView));
+        selectNext.setOnAction(e -> selectNext(tableView));
 
         stage.show();
     }
