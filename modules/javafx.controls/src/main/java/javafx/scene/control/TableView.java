590033f4cb2b5bbe02261a845c554dd08c7866ff
Jose Pereda <jpereda@openjdk.org>
2022-02-09 08:41:47 +0000

    8273336: Clicking a selected cell from a group of selected cells in a TableView clears the selected items list but remains selected
    Reviewed-by: mstrauss, aghaisas

--- modules/javafx.controls/src/main/java/javafx/scene/control/TableView.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/TableView.java
@@ -35,6 +35,7 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.WeakHashMap;
+import java.util.function.IntPredicate;
 
 import com.sun.javafx.logging.PlatformLogger.Level;
 import com.sun.javafx.scene.control.Logging;
@@ -3024,7 +3025,11 @@ public class TableView<S> extends Control {
         }
 
         private void fireCustomSelectedCellsListChangeEvent(ListChangeListener.Change<? extends TablePosition<S,?>> c) {
-            ControlUtils.updateSelectedIndices(this, c);
+            // Allow removing the row index if cell selection is not enabled or
+            // if such row doesn't have any selected cells
+            IntPredicate removeRowFilter = row -> !isCellSelectionEnabled() ||
+                    getSelectedCells().stream().noneMatch(tp -> tp.getRow() == row);
+            ControlUtils.updateSelectedIndices(this, c, removeRowFilter);
 
             if (isAtomic()) {
                 return;
