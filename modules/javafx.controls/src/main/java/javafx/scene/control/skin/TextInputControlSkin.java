a0bb545b21d1139dd95d7ee9dea9298e89666d9b
Eduard Sedov <eduard.sedov@zeiss.com>
2022-02-22 15:05:24 +0000

    8281953: NullPointer in InputMethod components in JFXPanel
    Reviewed-by: aghaisas

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/TextInputControlSkin.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/TextInputControlSkin.java
@@ -334,7 +334,10 @@ public abstract class TextInputControlSkin<T extends TextInputControl> extends S
         control.setInputMethodRequests(new ExtendedInputMethodRequests() {
             @Override public Point2D getTextLocation(int offset) {
                 Scene scene = getSkinnable().getScene();
-                Window window = scene.getWindow();
+                Window window = scene != null ? scene.getWindow() : null;
+                if (window == null) {
+                    return new Point2D(0, 0);
+                }
                 // Don't use imstart here because it isn't initialized yet.
                 Rectangle2D characterBounds = getCharacterBounds(control.getSelection().getStart() + offset);
                 Point2D p = getSkinnable().localToScene(characterBounds.getMinX(), characterBounds.getMaxY());
