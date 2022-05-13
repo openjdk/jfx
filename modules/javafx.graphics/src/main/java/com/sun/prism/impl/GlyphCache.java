ba82b03e88792579ea760ebe9c6acfde27564f89
Tomator <43985073+Tomator01@users.noreply.github.com>
2022-05-13 16:19:25 +0800

    Update GlyphCache.java
    When I used BlueJ, I found a problem with Chinese display. /javafx.graphics/com/sun/javafx/font/CompositeGlyphMapper.java#getGlyphCode may return a negative number when no font library is specified in Linux,and this could cause java.lang.ArrayIndexOutOfBoundsException error.So javafx.graphics/com/sun/prism/impl/GlyphCache.java#getCachedGlyph  shou check the glyphCode.

--- modules/javafx.graphics/src/main/java/com/sun/prism/impl/GlyphCache.java
+++ modules/javafx.graphics/src/main/java/com/sun/prism/impl/GlyphCache.java
@@ -239,6 +239,7 @@ public class GlyphCache {
     }
 
     private GlyphData getCachedGlyph(int glyphCode, int subPixel) {
+        if (glyphCode <= 0) {return null;}
         int segIndex = glyphCode >>> SEGSHIFT;
         int subIndex = glyphCode & SEGMASK;
         segIndex |= (subPixel << SUBPIXEL_SHIFT);
