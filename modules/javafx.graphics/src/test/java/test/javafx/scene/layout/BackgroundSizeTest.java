06df8c74e942f2b16838de37b464c2cbb6d927f3
Andy Goryachev <andy.goryachev@oracle.com>
2022-07-07 08:14:29 -0700

    8289394: fixed unlikely argument type warning

--- modules/javafx.graphics/src/test/java/test/javafx/scene/layout/BackgroundSizeTest.java
+++ modules/javafx.graphics/src/test/java/test/javafx/scene/layout/BackgroundSizeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -241,6 +241,7 @@ public class BackgroundSizeTest {
         assertFalse(a.equals(null));
     }
 
+    @SuppressWarnings("unlikely-arg-type")
     @Test public void notEqualToRandom() {
         BackgroundSize a = new BackgroundSize(1, .5, true, true, true, false);
         assertFalse(a.equals("Some random object"));
