13a55c954ddeb6cb6b687ed077419abbd35c337c
Andy Goryachev <andy.goryachev@oracle.com>
2022-07-19 15:27:14 +0000

    8289394: Fix warnings: Unlikely argument type
    Reviewed-by: nlisker, kcr

--- modules/javafx.graphics/src/test/java/test/javafx/scene/layout/BorderWidthsTest.java
+++ modules/javafx.graphics/src/test/java/test/javafx/scene/layout/BorderWidthsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -202,6 +202,7 @@ public class BorderWidthsTest {
         assertFalse(BorderWidths.DEFAULT.equals(null));
     }
 
+    @SuppressWarnings("unlikely-arg-type")
     @Test public void noEqualToRandom() {
         assertFalse(BorderWidths.DEFAULT.equals("Some random value"));
     }
