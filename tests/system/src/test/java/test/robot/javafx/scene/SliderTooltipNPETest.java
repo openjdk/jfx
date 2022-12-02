86e7823cf130e8f00c22d966ea23246c09220fa8
Karthik P K <karthik.p.k@oracle.com>
2022-12-02 16:37:48 +0530

    Address review comments

--- tests/system/src/test/java/test/robot/javafx/scene/SliderTooltipNPETest.java
+++ tests/system/src/test/java/test/robot/javafx/scene/SliderTooltipNPETest.java
@@ -22,19 +22,20 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 package test.robot.javafx.scene;
 
 import java.util.concurrent.CountDownLatch;
 
 import javafx.application.Application;
+import javafx.scene.input.MouseButton;
 import javafx.application.Platform;
+import javafx.scene.robot.Robot;
 import javafx.scene.Scene;
 import javafx.scene.control.Slider;
-import javafx.scene.control.Tooltip;
-import javafx.scene.input.MouseButton;
-import javafx.scene.robot.Robot;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.scene.control.Tooltip;
 import javafx.stage.WindowEvent;
 
 import org.junit.AfterClass;
@@ -44,52 +45,51 @@ import org.junit.Test;
 import test.util.Util;
 
 /*
- * Unit test for verifying Slider NPE error.
+ * Test for verifying Slider NPE error.
  *
- * There is 1 tests in this file.
+ * There is 1 test in this file.
  * Steps for testSliderTooltipNPE()
  * 1. Create a slider and tooltip.
  * 2. Make setAutoHide of tooltip as true and add tooltip to slider.
  * 3. Hover over slider thumb and wait for tooltip. Drag the thumb.
  * 4. Verify that NullPointerException is not thrown.
  */
+
 public class SliderTooltipNPETest {
     static CountDownLatch startupLatch = new CountDownLatch(1);
+    static CountDownLatch tooltipLatch = new CountDownLatch(1);
+    static Robot robot;
+    static Slider slider;
+
     static volatile Throwable exception;
     static volatile Stage stage;
     static volatile Scene scene;
-    static Robot robot;
-    static Slider slider;
+
     static final int SCENE_WIDTH = 250;
     static final int SCENE_HEIGHT = SCENE_WIDTH;
-    final int DRAG_DISTANCE = 10;
-
-    public static void main(String[] args) {
-        initFX();
-        try {
-            SliderTooltipNPETest test = new SliderTooltipNPETest();
-            test.testSliderTooltipNPE();
-        } catch (Throwable e) {
-            e.printStackTrace();
-        } finally {
-            exit();
-        }
-    }
+    static final int DRAG_DISTANCE = 10;
 
     @Test
     public void testSliderTooltipNPE() throws Throwable {
-        dragSliderAfterTooltipDisplayed(DRAG_DISTANCE, true);
+        dragSliderAfterTooltipDisplayed(DRAG_DISTANCE);
+        if (exception != null) {
+            exception.printStackTrace();
+            throw exception;
+        }
     }
 
-    private void dragSliderAfterTooltipDisplayed(int dragDistance, boolean xIncr) throws Throwable {
+    private void dragSliderAfterTooltipDisplayed(int dragDistance) throws Exception {
         Thread.sleep(1000); // Wait for slider to layout
 
         Util.runAndWait(() -> {
-            robot.mouseMove((int)(scene.getWindow().getX() + scene.getX() + SCENE_WIDTH/2),
-                (int)(scene.getWindow().getY() + scene.getY() + SCENE_HEIGHT/2));
+            robot.mouseMove((int)(scene.getWindow().getX() + scene.getX() +
+                                slider.getLayoutX() + slider.getLayoutBounds().getWidth()/2),
+                            (int)(scene.getWindow().getY() + scene.getY() +
+                                slider.getLayoutY() + slider.getLayoutBounds().getHeight()/2));
         });
 
-        Thread.sleep(3000); // Wait for tooltip to be displayed
+        Util.waitForLatch(tooltipLatch, 5, "Timeout waiting for tooltip to display");
+        Thread.sleep(2000); // Wait for tooltip to display
 
         Util.runAndWait(() -> {
             robot.mousePress(MouseButton.PRIMARY);
@@ -98,41 +98,39 @@ public class SliderTooltipNPETest {
         for (int i = 0; i < dragDistance; i++) {
             final int c = i;
             Util.runAndWait(() -> {
-                if (xIncr) {
-                robot.mouseMove((int)(scene.getWindow().getX() + scene.getX() + SCENE_WIDTH/2) + c,
-                    (int)(scene.getWindow().getY() + scene.getY() + SCENE_HEIGHT/2));
-                } else {
-                    robot.mouseMove((int)(scene.getWindow().getX() + scene.getX() + SCENE_WIDTH/2),
-                        (int)(scene.getWindow().getY() + scene.getY() + SCENE_HEIGHT/2) + c);
-                }
+            robot.mouseMove((int)(scene.getWindow().getX() + scene.getX() +
+                                slider.getLayoutX() + slider.getLayoutBounds().getWidth()/2) + c,
+                            (int)(scene.getWindow().getY() + scene.getY() +
+                                slider.getLayoutY() + slider.getLayoutBounds().getHeight()/2));
             });
         }
 
         Util.runAndWait(() -> {
             robot.mouseRelease(MouseButton.PRIMARY);
         });
-
-        if (exception != null) {
-            throw exception;
-        }
     }
 
     public static class TestApp extends Application {
+
         @Override
         public void start(Stage primaryStage) {
             robot = new Robot();
             stage = primaryStage;
-            slider = new Slider(0, 100, 50);
+
             Tooltip tooltip = new Tooltip("Autohide tooltip");
             tooltip.setAutoHide(true);
+            tooltip.setOnShown(event -> Platform.runLater(tooltipLatch::countDown));
+
+            slider = new Slider(0, 100, 50);
             slider.setTooltip(tooltip);
+
             scene = new Scene(slider, SCENE_WIDTH, SCENE_HEIGHT);
             stage.setScene(scene);
             stage.initStyle(StageStyle.UNDECORATED);
-            stage.addEventHandler(WindowEvent.WINDOW_SHOWN, e ->
-                    Platform.runLater(startupLatch::countDown));
             stage.setAlwaysOnTop(true);
+            stage.setOnShown(event -> Platform.runLater(startupLatch::countDown));
             stage.show();
+
             Thread.currentThread().setUncaughtExceptionHandler((t2, e) -> {
                 exception = e;
             });
