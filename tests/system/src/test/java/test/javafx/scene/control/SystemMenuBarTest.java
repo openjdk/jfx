5654002912c64b4162c25ea4df83e286c827ad3b
x1fkirma <florian.kirmaier.ext@zeiss.com>
2022-12-30 10:35:42 +0100

    JDK-8299423 Fixing leak in the mac system menu bar

---
+++ tests/system/src/test/java/test/javafx/scene/control/SystemMenuBarTest.java
@@ -0,0 +1,86 @@
+package test.javafx.scene.control;
+
+import com.sun.javafx.tk.Toolkit;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.junit.*;
+import test.com.sun.javafx.pgstub.StubToolkit;
+import test.util.Util;
+import test.util.memory.JMemoryBuddy;
+import javafx.application.Platform;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+
+public class SystemMenuBarTest {
+
+    @BeforeClass
+    public static void initFX() throws Exception {
+        CountDownLatch startupLatch = new CountDownLatch(1);
+        Platform.setImplicitExit(false);
+        Util.startup(startupLatch, startupLatch::countDown);
+    }
+
+    @AfterClass
+    public static void teardownOnce() {
+        Util.shutdown();
+    }
+
+    @Test
+    public void test_JDK_8299423() {
+        /**
+         * We want to test, if we add a menu to the system-menu, whether it's callable get's collected after the entry is removed.
+         *
+         */
+        JMemoryBuddy.memoryTest((checker) -> {
+            CountDownLatch latch = new CountDownLatch(1);
+            Platform.runLater(() -> {
+                Stage stage = new Stage();
+                MenuBar menuBar = new MenuBar();
+                VBox root = new VBox(menuBar);
+                stage.setScene(new Scene(root));
+                menuBar.setUseSystemMenuBar(true);
+                Menu menu1 = new Menu("menu-1");
+                var item2 = new MenuItem("remove above");
+                menu1.getItems().add(item2);
+                menuBar.getMenus().add(menu1);
+                EventHandler<ActionEvent> listener = new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        System.out.println("I'm the listener, but no one calls me.");
+                    }
+                };
+                item2.setOnAction(listener);
+                stage.show();
+
+                checker.assertCollectable(listener);
+
+                new Thread(() -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Platform.runLater(() -> {
+                        menu1.getItems().clear();
+                        latch.countDown();
+                    });
+                }).start();
+            });
+
+            try {
+                assertTrue("Timeout waiting for setOnShown", latch.await(15, TimeUnit.SECONDS));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+}
