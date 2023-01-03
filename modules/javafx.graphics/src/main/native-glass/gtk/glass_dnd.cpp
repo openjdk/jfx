c827893a5c4aa4bf1d94e847d7efd644000956b0
Thiago Milczarek Sayão <thiago.sayao@gmail.com>
2023-01-03 20:25:34 -0300

    Simplify DragView paint

--- modules/javafx.graphics/src/main/native-glass/gtk/glass_dnd.cpp
+++ modules/javafx.graphics/src/main/native-glass/gtk/glass_dnd.cpp
@@ -1124,8 +1124,15 @@ GdkPixbuf* DragView::get_drag_image(gboolean* is_raw_image, gint* width, gint* h
                     guchar* data = (guchar*) g_try_malloc0(nraw - whsz);
                     if (data) {
                         memcpy(data, (raw + whsz), nraw - whsz);
+
+                        if (is_raw_image) {
+                            guchar* origdata = data;
+                            data = (guchar*) convert_BGRA_to_RGBA((const int*) data, w * 4, h);
+                            g_free(origdata);
+                        }
+
                         pixbuf = gdk_pixbuf_new_from_data(data, GDK_COLORSPACE_RGB, TRUE, 8,
-                                w, h, w * 4, pixbufDestroyNotifyFunc, NULL);
+                                                          w, h, w * 4, pixbufDestroyNotifyFunc, NULL);
                     }
                 }
             }
@@ -1235,37 +1242,15 @@ void DragView::expose() {
     gdk_window_begin_paint_region(window, region);
 #endif
 
-    cairo_t *context;
-    cairo_surface_t *cairo_surface;
-
-    context = gdk_cairo_create(window);
-
-    guchar* pixels = is_raw_image
-            ? (guchar*) convert_BGRA_to_RGBA((const int*) gdk_pixbuf_get_pixels(pixbuf),
-                                                gdk_pixbuf_get_rowstride(pixbuf),
-                                                height)
-            : gdk_pixbuf_get_pixels(pixbuf);
-
-    cairo_surface = cairo_image_surface_create_for_data(
-            pixels,
-            CAIRO_FORMAT_ARGB32,
-            width, height, width * 4);
-
-    cairo_set_source_surface(context, cairo_surface, 0, 0);
-    cairo_set_operator(context, CAIRO_OPERATOR_SOURCE);
+    cairo_t *context = gdk_cairo_create(window);
+    gdk_cairo_set_source_pixbuf(context, pixbuf, 0, 0);
     cairo_paint(context);
-
-    if (is_raw_image) {
-        g_free(pixels);
-    }
+    cairo_destroy(context);
 
 #ifdef GLASS_GTK3
     gdk_window_end_paint(window);
     cairo_region_destroy(region);
 #endif
-
-    cairo_surface_destroy(cairo_surface);
-    cairo_destroy(context);
 }
 
 void DragView::move(gint x, gint y) {
