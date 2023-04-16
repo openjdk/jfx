80f639d7d1ceb3dd2308ef68748586653d8eebe7
John Hendrikx <hjohn@xs4all.nl>
2023-04-16 09:09:44 +0200

    Fix incorrect import

--- modules/javafx.base/src/test/java/test/com/sun/javafx/binding/ArrayManagerTest.java
+++ modules/javafx.base/src/test/java/test/com/sun/javafx/binding/ArrayManagerTest.java
@@ -1,9 +1,9 @@
 package test.com.sun.javafx.binding;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
