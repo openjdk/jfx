11a840fdf170e36805dea56691f939b3f90549df
mstr2 <43553916+mstr2@users.noreply.github.com>
2023-04-24 00:07:50 +0200

    Added documentation

--- modules/javafx.graphics/src/main/java/javafx/scene/image/Image.java
+++ modules/javafx.graphics/src/main/java/javafx/scene/image/Image.java
@@ -65,21 +65,24 @@ import javafx.beans.property.SimpleIntegerProperty;
  * images from a specified URL.
  *
  * <p>
- * Supported image formats are:
- * <ul>
- * <li><a href="http://msdn.microsoft.com/en-us/library/dd183376(v=vs.85).aspx">BMP</a></li>
- * <li><a href="http://www.w3.org/Graphics/GIF/spec-gif89a.txt">GIF</a></li>
- * <li><a href="http://www.ijg.org">JPEG</a></li>
- * <li><a href="http://www.libpng.org/pub/png/spec/">PNG</a></li>
- * </ul>
+ * JavaFX supports all image formats that can be loaded using the
+ * <a href="https://docs.oracle.com/en/java/javase/20/docs/api/java.desktop/javax/imageio/package-summary.html">Image I/O API</a>,
+ * which includes
+ * <a href="https://msdn.microsoft.com/en-us/library/dd183391.aspx">BMP</a>,
+ * <a href="http://www.w3.org/Graphics/GIF/spec-gif89a.txt">GIF</a>,
+ * <a href="http://www.jpeg.org/">JPEG</a>,
+ * <a href="http://www.libpng.org/pub/png/spec/">PNG</a>,
+ * <a href="https://www.itu.int/itudoc/itu-t/com16/tiff-fx/docs/tiff6.pdf">TIFF</a>, and
+ * <a href="http://www.wapforum.org/what/technical/SPEC-WAESpec-19990524.pdf">WBMP</a>.
+ * Image I/O is also extensible so that developers can "plug-in" support for additional formats.
  *
  * <p>
  * Images can be resized as they are loaded (for example to reduce the amount of
  * memory consumed by the image). The application can specify the quality of
- * filtering used when scaling, and whether or not to preserve the original
+ * filtering used when scaling, and whether to preserve the original
  * image's aspect ratio.
  *
- * <p>If a URL string is passed to a constructor, it be any of the following:
+ * <p>If a URL string is passed to a constructor, it can be any of the following:
  * <ol>
  *     <li>the name of a resource that can be resolved by the context
  *         {@link ClassLoader} for this thread
