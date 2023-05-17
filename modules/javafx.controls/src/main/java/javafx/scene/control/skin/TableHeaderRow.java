86f854f5f7ff36c0c5a68eb26090194afd3d4fcc
Marius Hanl <mariushanl@web.de>
2023-05-17 21:17:54 +0200

    JDK-8091153: Improve javadoc

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/TableHeaderRow.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/TableHeaderRow.java
@@ -465,11 +465,11 @@ public class TableHeaderRow extends StackPane {
     }
 
     /**
-     * Shows a menu containing all leaf columns as item.
+     * Shows a menu containing all leaf columns as items.
      * An item can be selected/deselected to make the corresponding column visible/invisible.
      *
-     * @implNote This method can be overridden to create and show a custom menu.
-     * @param mouseEvent the {@link MouseEvent} which was generated when the table menu button was pressed.
+     * @implNote This method can be overridden to create and show a custom menu
+     * @param mouseEvent the {@link MouseEvent} which was generated when the table menu button was pressed
      * @since 21
      */
     protected void showColumnMenu(MouseEvent mouseEvent) {
