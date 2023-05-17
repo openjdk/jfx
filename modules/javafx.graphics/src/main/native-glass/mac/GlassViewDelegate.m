384310f9f00f8ceadede51e790616265c56b850e
Lukasz Kostyra <lukasz.kostyra@oracle.com>
2023-05-17 09:29:48 +0200

    Revert "GlassViewDelegate: Return to deprecated *PboardType symbols"
    This reverts commit fa42b1b13755dec05d77b3865ed657e954854286.
    
    fa42b1b13 is not needed anymore - JFX will target macOS 11 soon.

--- modules/javafx.graphics/src/main/native-glass/mac/GlassViewDelegate.m
+++ modules/javafx.graphics/src/main/native-glass/mac/GlassViewDelegate.m
@@ -25,7 +25,6 @@
 
 #include "GlassDraggingSource.h"
 #import "common.h"
-#include <AppKit/AppKit.h>
 #import "com_sun_glass_events_ViewEvent.h"
 #import "com_sun_glass_events_MouseEvent.h"
 #import "com_sun_glass_events_KeyEvent.h"
@@ -971,12 +970,25 @@ static jint getSwipeDirFromEvent(NSEvent *theEvent)
             image = [[NSImage alloc] initWithData:[pbItem dataForType:DRAG_IMAGE_MIME]];
         }
 
-        if (image == nil && [pbItemTypes containsObject:NSURLPboardType])
+        if (image == nil && [pbItemTypes containsObject:NSPasteboardTypeFileURL])
         {
             // create an image with contents of URL
-            image = [[NSImage alloc] initByReferencingURL:[NSURL URLWithString:[[NSString alloc] initWithData:[pbItem dataForType:NSURLPboardType] encoding:NSUTF8StringEncoding]]];
+            image = [[NSImage alloc] initByReferencingFile:[[NSString alloc] initWithData:[pbItem dataForType:NSPasteboardTypeFileURL] encoding:NSUTF8StringEncoding]];
 
-            // URL can be invalid
+            // this only works if we reference image files though, so make sure the image is valid
+            if ([image isValid] == NO)
+            {
+                [image release];
+                image = nil;
+            }
+        }
+
+        if (image == nil && [pbItemTypes containsObject:NSPasteboardTypeURL])
+        {
+            // create an image with contents of URL
+            image = [[NSImage alloc] initByReferencingURL:[NSURL URLWithString:[[NSString alloc] initWithData:[pbItem dataForType:NSPasteboardTypeURL] encoding:NSUTF8StringEncoding]]];
+
+            // same as with File URL, regular URL can also be invalid
             if ([image isValid] == NO)
             {
                 [image release];
