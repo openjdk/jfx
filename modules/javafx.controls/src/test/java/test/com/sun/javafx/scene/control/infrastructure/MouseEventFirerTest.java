7eb9a1c47926277223fb947d90f2e08e00a9aafd
Thiago Milczarek Sayao <tsayao@openjdk.org>
2023-06-15 19:04:48 +0000

    8306121: Scene not rendered initially when changing scenes after fix for JDK-8296621
    Reviewed-by: jpereda, kcr

--- modules/javafx.controls/src/test/java/test/com/sun/javafx/scene/control/infrastructure/MouseEventFirerTest.java
+++ modules/javafx.controls/src/test/java/test/com/sun/javafx/scene/control/infrastructure/MouseEventFirerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -274,7 +274,7 @@ public class MouseEventFirerTest {
         setBottomAnchor(bottomRight, 0.);
         setLeftAnchor(bottomRight, 0.);
 
-        content = new AnchorPane(topLeft, center, bottomRight);
+        content = new AnchorPane(center, topLeft, bottomRight);
         scene = new Scene(content);
         stage = new Stage();
         stage.setScene(scene);
