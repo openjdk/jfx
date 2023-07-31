719817d774295f708ebbe2605a600189f12582d5
jose.pereda <jose.pereda@gluonhq.com>
2023-07-31 19:37:16 +0200

    Removed extra lines

--- tests/system/src/test/java/test/com/sun/javafx/animation/AnimationTest.java
+++ tests/system/src/test/java/test/com/sun/javafx/animation/AnimationTest.java
@@ -113,5 +113,4 @@ public class AnimationTest {
         });
         assertThrows(IllegalStateException.class, pause::stop);
     }
-
 }
--- tests/system/src/test/java/test/com/sun/javafx/animation/AnimationTimerTest.java
+++ tests/system/src/test/java/test/com/sun/javafx/animation/AnimationTimerTest.java
@@ -109,5 +109,4 @@ public class AnimationTimerTest {
         };
         Platform.runLater(timer::start);
     }
-
 }
