0de3f7a6fa10aa3fb86b049100fba95586e7bf74
Marius Hanl <mariushanl@web.de>
2023-08-03 17:08:17 +0200

    JDK-8311983: remove JUnit5 import

--- modules/javafx.controls/src/test/java/test/javafx/scene/control/skin/VirtualFlowTest.java
+++ modules/javafx.controls/src/test/java/test/javafx/scene/control/skin/VirtualFlowTest.java
@@ -34,7 +34,6 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.Iterator;
 import java.util.LinkedList;
