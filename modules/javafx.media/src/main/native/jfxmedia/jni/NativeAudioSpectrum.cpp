96b054fc38af4dd4f959d13dc3b11187aa13f3ac
alexander_matveev <alexander.matveev@oracle.com>
2023-08-07 16:22:00 -0700

    8313900: Possible NULL pointer access in NativeAudioSpectrum and NativeVideoBuffer

--- modules/javafx.media/src/main/native/jfxmedia/jni/NativeAudioSpectrum.cpp
+++ modules/javafx.media/src/main/native/jfxmedia/jni/NativeAudioSpectrum.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -55,6 +55,10 @@ Java_com_sun_media_jfxmediaimpl_NativeAudioSpectrum_nativeSetBands(JNIEnv *env, 
 {
     CAudioSpectrum *pSpectrum = (CAudioSpectrum*)jlong_to_ptr(nativeRef);
     CJavaBandsHolder *pHolder = new (std::nothrow) CJavaBandsHolder();
+    if (pHolder == NULL) {
+        return;
+    }
+
     if (!pHolder->Init(env, bands, magnitudes, phases)) {
         delete pHolder;
         pHolder = NULL;
