826d159c077e05cb015336cf0b99e1e4c1ced606
Andy Goryachev <andy.goryachev@oracle.com>
2023-08-21 14:41:26 -0700

    request layout when changing font

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/TextAreaSkin.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/TextAreaSkin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -271,6 +271,10 @@ public class TextAreaSkin extends TextInputControlSkin<TextArea> {
         registerChangeListener(control.prefRowCountProperty(), e -> {
             invalidateMetrics();
             updatePrefViewportHeight();
+        });
+
+        registerChangeListener(control.fontProperty(), e -> {
+            contentView.requestLayout();
         });
 
         updateFontMetrics();
