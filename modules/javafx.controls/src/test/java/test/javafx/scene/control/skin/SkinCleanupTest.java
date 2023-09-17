1a72a4ace3faaaccebd316105d1234052a21df7f
Thiago Milczarek Sayão <thiago.sayao@gmail.com>
2023-09-17 19:45:04 -0300

    Fix Tests

--- modules/javafx.controls/src/test/java/test/javafx/scene/control/skin/SkinCleanupTest.java
+++ modules/javafx.controls/src/test/java/test/javafx/scene/control/skin/SkinCleanupTest.java
@@ -1398,6 +1398,11 @@ public class SkinCleanupTest {
             }
 
             @Override
+            public Point2D getTextLocationRelative(int offset) {
+                return new Point2D(0, 0);
+            }
+
+            @Override
             public int getLocationOffset(int x, int y) {
                 return 0;
             }
