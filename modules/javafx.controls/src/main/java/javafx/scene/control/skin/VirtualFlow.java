ec9a11bc2b5f2f01c3109a13ccc03274e8ed7678
Marius Hanl <mhanl@openjdk.org>
2023-10-09 21:00:03 +0000

    8316590: Rendering artifact after JDK-8311983
    Reviewed-by: jvos, kpk, angorya

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/VirtualFlow.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/VirtualFlow.java
@@ -1671,11 +1671,10 @@ public class VirtualFlow<T extends IndexedCell> extends Region {
 
             // Add any necessary leading cells
             if (firstCell != null) {
-                int firstIndex = getCellIndex(firstCell);
-                int index = Math.max(0, firstIndex - 1);
-
-                double prevIndexSize = getCellLength(index);
-                addLeadingCells(index, getCellPosition(firstCell) - prevIndexSize);
+                // NOTE: The index might be -1, but the call to addLeadingCells() is still required.
+                int previousIndex = getCellIndex(firstCell) - 1;
+                double prevIndexSize = getCellLength(previousIndex);
+                addLeadingCells(previousIndex, getCellPosition(firstCell) - prevIndexSize);
             } else {
                 int currentIndex = computeCurrentIndex();
 
