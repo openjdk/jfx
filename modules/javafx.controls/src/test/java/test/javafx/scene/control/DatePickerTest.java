7e2361a5d79869aa793885e734945cd0272ac357
brunesto <bruno.carle@gmail.com>
2023-11-10 17:39:36 +0100

    replaced robot test by plain junit

--- modules/javafx.controls/src/test/java/test/javafx/scene/control/DatePickerTest.java
+++ modules/javafx.controls/src/test/java/test/javafx/scene/control/DatePickerTest.java
@@ -737,6 +737,36 @@ public class DatePickerTest {
         stageLoader.dispose();
     }
 
+    /**
+     * When DatePicker looses focus with misformatted text in the editor,
+     * checks that the value is not changed, and the text is reverted to the value
+     */
+    @Test
+    public void testFocusLostWithTypo() {
+        //datePicker.setEditable(true); // other test cases have it .is this necessary?
+
+        Button button = new Button();
+        StageLoader stageLoader = new StageLoader(new HBox(datePicker, button));
+
+        // initial value
+        datePicker.setValue(LocalDate.of(2015, 03, 25));
+        assertEquals("3/25/2015",datePicker.getEditor().getText());
+
+        // set misformatted text
+        //stageLoader.getStage().requestFocus(); // other test cases have it . is this necessary?
+        datePicker.requestFocus();
+        datePicker.getEditor().setText("11/24/20xx");
+
+        // loosing focus triggers cancelEdit() because the text cannot be parsed
+        button.requestFocus();
+
+        // check that value remains unchanged, and text is reverted
+        assertEquals(LocalDate.of(2015, 03, 25),datePicker.getValue());
+        assertEquals("3/25/2015",datePicker.getEditor().getText());
+
+        stageLoader.dispose();
+    }
+
     private class InvalidChronology extends AbstractChronology {
         @Override
         public String getId() {
