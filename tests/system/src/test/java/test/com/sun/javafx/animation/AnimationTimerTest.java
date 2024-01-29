b3bd557e1886cfead04dcc6bd89e327431fe140b
nlisker <37422899+nlisker@users.noreply.github.com>
2024-01-29 18:38:45 +0200

    Update tests

--- tests/system/src/test/java/test/com/sun/javafx/animation/AnimationTimerTest.java
+++ tests/system/src/test/java/test/com/sun/javafx/animation/AnimationTimerTest.java
@@ -1,11 +1,36 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
 package test.com.sun.javafx.animation;
 
 import org.junit.jupiter.api.Test;
 
 import javafx.animation.AnimationTimer;
+import test.util.Util;
 
-// Based on https://bugs.openjdk.org/browse/JDK-8159048
-public class AnimationTimerTest extends SynchronisityTest {
+public class AnimationTimerTest extends SynchronizationTest {
 
     @Test
     public void testAnimationTimer() throws InterruptedException {
@@ -19,16 +44,17 @@ public class AnimationTimerTest extends SynchronisityTest {
 
             @Override
             public void handle(long now) {
-                sleep(10); // Some intensive processing
+                // Simulate intensive processing
+                Util.sleep(10);
             }
         };
 
+        // Start and stop continuously until aborted by the test harness
         while (true) {
-            // Initialize the concurrent starts and stops
             timer.start();
-            sleep(10);
+            Util.sleep(10);
             timer.stop();
-            sleep(10);
+            Util.sleep(10);
         }
     }
 }
