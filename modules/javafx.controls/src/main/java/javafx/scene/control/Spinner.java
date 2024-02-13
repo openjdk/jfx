5330939e9a8b44d8eef43595e8dcb46a43e0a897
Marius Hanl <mariushanl@web.de>
2024-02-13 21:37:25 +0100

    JDK-8325798: Spinner throws uncatchable exception on tab out from garbled text

--- modules/javafx.controls/src/main/java/javafx/scene/control/Spinner.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/Spinner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -166,7 +166,11 @@ public class Spinner<T> extends Control {
 
         focusedProperty().addListener(o -> {
             if (!isFocused()) {
-                commitValue();
+                try {
+                    commitValue();
+                } catch (Exception e) {
+                    cancelEdit();
+                }
             }
         });
     }
