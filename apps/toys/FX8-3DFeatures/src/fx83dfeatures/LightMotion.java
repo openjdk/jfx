570503c57fb45b650b6ed4c103e79a2738286e72
Andy Goryachev <andy.goryachev@oracle.com>
2024-03-22 08:22:33 -0700

    part 3

--- apps/toys/FX8-3DFeatures/src/fx83dfeatures/LightMotion.java
+++ apps/toys/FX8-3DFeatures/src/fx83dfeatures/LightMotion.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -148,6 +148,7 @@ public class LightMotion extends Application {
         scene.setFill(Color.rgb(10, 10, 40));
 
         scene.setOnKeyTyped(new EventHandler<KeyEvent>() {
+            @Override
             public void handle(KeyEvent e) {
                 switch (e.getCharacter()) {
                     case "[":
