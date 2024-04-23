9fdc5c644f868a304a46fd6cbb7958db9a2527a5
Oliver Kopp <kopp.dev@gmail.com>
2024-04-23 13:04:55 +0200

    Try to use StubToolkit

--- tests/system/src/test/java/test/com/sun/glass/ui/win/WinTextRangeProviderTest.java
+++ tests/system/src/test/java/test/com/sun/glass/ui/win/WinTextRangeProviderTest.java
@@ -24,16 +24,56 @@
  */
 package test.com.sun.glass.ui.win;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import com.sun.javafx.PlatformUtil;
 import com.sun.glass.ui.win.WinTextRangeProviderShim;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+import javafx.application.Platform;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.Stream;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@EnabledOnOs({OS.WINDOWS})
 public class WinTextRangeProviderTest {
+
+    private static final CountDownLatch startupLatch = new CountDownLatch(1);
+    private static final PrintStream defaultErrorStream = System.err;
+    protected static final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @BeforeAll
+    static void doSetup() throws Exception {
+        System.setErr(new PrintStream(out, true));
+
+        Platform.startup(() -> {
+            startupLatch.countDown();
+        });
+
+        if (!startupLatch.await(15, TimeUnit.SECONDS)) {
+            System.setErr(defaultErrorStream);
+            System.err.println(out);
+            fail("Timeout waiting for FX runtime to start");
+        }
+
+        Thread.sleep(250);
+        System.setErr(defaultErrorStream);
+    }
+
+    @AfterAll
+    static void doTeardown() {
+        Platform.exit();
+    }
 
     static Stream<Arguments> getEndIndex() {
         return Stream.of(
