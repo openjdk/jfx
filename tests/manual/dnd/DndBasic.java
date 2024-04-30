4eed575a2b5b48ca082533724ebb024ea1ecd8d0
Martin Fox <beldenfox@users.noreply.github.com>
2024-04-30 10:37:23 -0700

    Fixed whitespace errors

--- tests/manual/dnd/DndBasic.java
+++ tests/manual/dnd/DndBasic.java
@@ -43,184 +43,184 @@ import javafx.stage.Stage;
 
 public class DndBasic extends Application {
 
-	ArrayList<Text> sourceResults = new ArrayList<Text>();
-	ArrayList<Text> targetResults = new ArrayList<Text>();
-	Rectangle lastDropTarget = null;
+    ArrayList<Text> sourceResults = new ArrayList<Text>();
+    ArrayList<Text> targetResults = new ArrayList<Text>();
+    Rectangle lastDropTarget = null;
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-		TransferMode[] copyOnly = new TransferMode[]{TransferMode.COPY};
-		TransferMode[] moveOnly = new TransferMode[]{TransferMode.MOVE};
-		TransferMode[] linkOnly = new TransferMode[]{TransferMode.LINK};
+        TransferMode[] copyOnly = new TransferMode[]{TransferMode.COPY};
+        TransferMode[] moveOnly = new TransferMode[]{TransferMode.MOVE};
+        TransferMode[] linkOnly = new TransferMode[]{TransferMode.LINK};
 
-		Label sourcesLabel = new Label("Sources");
-    	Group sourceAll = createSource(TransferMode.ANY);
-		Group sourceCopyMove = createSource(TransferMode.COPY_OR_MOVE);
-		Group sourceCopyOnly = createSource(copyOnly);
-		Group sourceMoveOnly = createSource(moveOnly);
-		Group sourceLinkOnly = createSource(linkOnly);
+        Label sourcesLabel = new Label("Sources");
+        Group sourceAll = createSource(TransferMode.ANY);
+        Group sourceCopyMove = createSource(TransferMode.COPY_OR_MOVE);
+        Group sourceCopyOnly = createSource(copyOnly);
+        Group sourceMoveOnly = createSource(moveOnly);
+        Group sourceLinkOnly = createSource(linkOnly);
 
-		VBox sources = new VBox(sourcesLabel,
-			sourceAll, sourceCopyMove, sourceCopyOnly,
-			sourceMoveOnly, sourceLinkOnly);
-		sources.setSpacing(10);
+        VBox sources = new VBox(sourcesLabel,
+            sourceAll, sourceCopyMove, sourceCopyOnly,
+            sourceMoveOnly, sourceLinkOnly);
+        sources.setSpacing(10);
 
-		Label targetLabel = new Label("Destinations");
-		Group targetAll = createTarget(TransferMode.ANY);
-		Group targetCopyMove = createTarget(TransferMode.COPY_OR_MOVE);
-		Group targetCopyOnly = createTarget(copyOnly);
-		Group targetMoveOnly = createTarget(moveOnly);
-		Group targetLinkOnly = createTarget(linkOnly);
+        Label targetLabel = new Label("Destinations");
+        Group targetAll = createTarget(TransferMode.ANY);
+        Group targetCopyMove = createTarget(TransferMode.COPY_OR_MOVE);
+        Group targetCopyOnly = createTarget(copyOnly);
+        Group targetMoveOnly = createTarget(moveOnly);
+        Group targetLinkOnly = createTarget(linkOnly);
 
-		VBox targets = new VBox(targetLabel,
-			targetAll, targetCopyMove, targetCopyOnly,
-			targetMoveOnly, targetLinkOnly);
-		targets.setSpacing(10);
+        VBox targets = new VBox(targetLabel,
+            targetAll, targetCopyMove, targetCopyOnly,
+            targetMoveOnly, targetLinkOnly);
+        targets.setSpacing(10);
 
-		HBox columns = new HBox(sources, targets);
-		columns.setSpacing(10);
+        HBox columns = new HBox(sources, targets);
+        columns.setSpacing(10);
 
-		Label instructions = new Label("Drag from a source to a destination (the desktop can be either)");
-		VBox withInstructions = new VBox(instructions, columns);
-		withInstructions.setSpacing(20);
-		withInstructions.setPadding(new Insets(10, 10, 10, 10));
-		Scene s = new Scene(withInstructions);
-		primaryStage.setScene(s);
-		primaryStage.show();
-	}
+        Label instructions = new Label("Drag from a source to a destination (the desktop can be either)");
+        VBox withInstructions = new VBox(instructions, columns);
+        withInstructions.setSpacing(20);
+        withInstructions.setPadding(new Insets(10, 10, 10, 10));
+        Scene s = new Scene(withInstructions);
+        primaryStage.setScene(s);
+        primaryStage.show();
+    }
 
-	private Text modesToText(String l, TransferMode[] modes) {
-		String label = "";
-		for (TransferMode c : modes) {
-			if (label != "") {
-				label = label + " ";
-			}
-			label += c;
-		}
-		Text text = new Text(label);
-		return text;
-	}
+    private Text modesToText(String l, TransferMode[] modes) {
+        String label = "";
+        for (TransferMode c : modes) {
+            if (label != "") {
+                label = label + " ";
+            }
+            label += c;
+        }
+        Text text = new Text(label);
+        return text;
+    }
 
-	private String modesToString(Set<TransferMode> modes) {
-		String label = "";
-		if (modes.contains(TransferMode.COPY)) {
-			label += " " + TransferMode.COPY;
-		}
-		if (modes.contains(TransferMode.MOVE)) {
-			label += " " + TransferMode.MOVE;
-		}
-		if (modes.contains(TransferMode.LINK)) {
-			label += " " + TransferMode.LINK;
-		}
-		return label;
-	}
+    private String modesToString(Set<TransferMode> modes) {
+        String label = "";
+        if (modes.contains(TransferMode.COPY)) {
+            label += " " + TransferMode.COPY;
+        }
+        if (modes.contains(TransferMode.MOVE)) {
+            label += " " + TransferMode.MOVE;
+        }
+        if (modes.contains(TransferMode.LINK)) {
+            label += " " + TransferMode.LINK;
+        }
+        return label;
+    }
 
-	private void clearSourceResults() {
-		for (Text t : sourceResults) {
-	        t.setText("");
-	    }
-	}
+    private void clearSourceResults() {
+        for (Text t : sourceResults) {
+            t.setText("");
+        }
+    }
 
-	private void clearTargetResults() {
-		for (Text t : targetResults) {
-			t.setText("");
-		}
-	}
+    private void clearTargetResults() {
+        for (Text t : targetResults) {
+            t.setText("");
+        }
+    }
 
-	private Group createSource(TransferMode[] modes) {
-		Text title = modesToText("Source", modes);
-		Text result = new Text("");
-		title.setMouseTransparent(true);
-		result.setMouseTransparent(true);
-		sourceResults.add(result);
+    private Group createSource(TransferMode[] modes) {
+        Text title = modesToText("Source", modes);
+        Text result = new Text("");
+        title.setMouseTransparent(true);
+        result.setMouseTransparent(true);
+        sourceResults.add(result);
 
-		VBox labels = new VBox(title, result);
+        VBox labels = new VBox(title, result);
         labels.setMouseTransparent(true);
         labels.setSpacing(3);
-		labels.setPadding(new Insets(5, 5, 5, 5));
+        labels.setPadding(new Insets(5, 5, 5, 5));
 
-		Rectangle source = new Rectangle(300, 75);
-		source.setOnDragDetected( evt -> {
-			Dragboard dragboard = source.startDragAndDrop(modes);
-			ClipboardContent content = new ClipboardContent();
-	        content.putString(UUID.randomUUID().toString());
-	        dragboard.setContent(content);
-	        evt.consume();
-	        clearSourceResults();
-	        clearTargetResults();
-			result.setText("Dragging");
-		});
-		source.setOnDragDone( evt -> {
-			String message = "Drag done, ";
-			message = message + (evt.isAccepted() ? "accepted" : "rejected");
-			if (evt.getTransferMode() != null) {
-			    message = message + " " + evt.getTransferMode();
-			}
-		    result.setText(message);
-			evt.consume();
-		});
-		source.setFill(Color.LIGHTGREEN);
+        Rectangle source = new Rectangle(300, 75);
+        source.setOnDragDetected( evt -> {
+            Dragboard dragboard = source.startDragAndDrop(modes);
+            ClipboardContent content = new ClipboardContent();
+            content.putString(UUID.randomUUID().toString());
+            dragboard.setContent(content);
+            evt.consume();
+            clearSourceResults();
+            clearTargetResults();
+            result.setText("Dragging");
+        });
+        source.setOnDragDone( evt -> {
+            String message = "Drag done, ";
+            message = message + (evt.isAccepted() ? "accepted" : "rejected");
+            if (evt.getTransferMode() != null) {
+                message = message + " " + evt.getTransferMode();
+            }
+            result.setText(message);
+            evt.consume();
+        });
+        source.setFill(Color.LIGHTGREEN);
 
-		return new Group(source, labels);
-	}
+        return new Group(source, labels);
+    }
 
-	private Group createTarget(TransferMode[] modes)
-	{
-		Text title = modesToText("Destination", modes);
-		Text available = new Text("");
-		Text result = new Text("");
-		targetResults.add(result);
-		title.setMouseTransparent(true);
+    private Group createTarget(TransferMode[] modes)
+    {
+        Text title = modesToText("Destination", modes);
+        Text available = new Text("");
+        Text result = new Text("");
+        targetResults.add(result);
+        title.setMouseTransparent(true);
         available.setMouseTransparent(true);
         result.setMouseTransparent(true);
 
         VBox labels = new VBox(title, available, result);
         labels.setMouseTransparent(true);
         labels.setSpacing(3);
-		labels.setPadding(new Insets(5, 5, 5, 5));
+        labels.setPadding(new Insets(5, 5, 5, 5));
 
-		Rectangle target = new Rectangle(300, 75);
-		target.setOnDragOver(evt -> {
-			evt.acceptTransferModes(modes);
-			clearTargetResults();
-			if (evt.getGestureSource() == null) {
-				clearSourceResults();
-			}
-			if (evt.getTransferMode() != null) {
-				result.setText("Proposed " + evt.getTransferMode());
-			}
-			else {
-				result.setText("");
-			}
-			// Note: we always show the source modes to illustrate how the Mac filters the
-			// available modes based on the modifier key states.
-			available.setText("Source" + modesToString(evt.getDragboard().getTransferModes()));
-			evt.consume();
-		});
-		target.setOnDragDropped(evt -> {
-			lastDropTarget = target;
-			String text = "Dropped here";
-			if (evt.getTransferMode() != null) {
-				text += ", proposed " + evt.getTransferMode();
-			}
-			result.setText(text);
-			evt.setDropCompleted(true);
-			evt.consume();
-		});
-		target.setOnDragExited(evt -> {
-			available.setText("");
-			if (target != lastDropTarget) {
-			    result.setText("");
-			}
-			evt.consume();
-		});
-		target.setFill(Color.LIGHTBLUE);
+        Rectangle target = new Rectangle(300, 75);
+        target.setOnDragOver(evt -> {
+            evt.acceptTransferModes(modes);
+            clearTargetResults();
+            if (evt.getGestureSource() == null) {
+                clearSourceResults();
+            }
+            if (evt.getTransferMode() != null) {
+                result.setText("Proposed " + evt.getTransferMode());
+            }
+            else {
+                result.setText("");
+            }
+            // Note: we always show the source modes to illustrate how the Mac filters the
+            // available modes based on the modifier key states.
+            available.setText("Source" + modesToString(evt.getDragboard().getTransferModes()));
+            evt.consume();
+        });
+        target.setOnDragDropped(evt -> {
+            lastDropTarget = target;
+            String text = "Dropped here";
+            if (evt.getTransferMode() != null) {
+                text += ", proposed " + evt.getTransferMode();
+            }
+            result.setText(text);
+            evt.setDropCompleted(true);
+            evt.consume();
+        });
+        target.setOnDragExited(evt -> {
+            available.setText("");
+            if (target != lastDropTarget) {
+                result.setText("");
+            }
+            evt.consume();
+        });
+        target.setFill(Color.LIGHTBLUE);
 
-		return new Group(target, labels);
-	}
+        return new Group(target, labels);
+    }
 
-	public static void main(String[] args) {
-		launch(DndBasic.class, args);
-	}
+    public static void main(String[] args) {
+        launch(DndBasic.class, args);
+    }
 }
