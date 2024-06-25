1a853794bf833a69085766c0d7f41edc449a1370
drmarmac <6900949+drmarmac@users.noreply.github.com>
2024-06-25 23:22:19 +0200

    cleanup imports

--- modules/javafx.controls/src/test/java/test/javafx/scene/chart/XYNumberLineChartsTest.java
+++ modules/javafx.controls/src/test/java/test/javafx/scene/chart/XYNumberLineChartsTest.java
@@ -34,8 +34,6 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.Axis;
 import javafx.scene.chart.Chart;
