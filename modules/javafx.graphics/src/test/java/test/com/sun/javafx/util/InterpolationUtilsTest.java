b545617f333722f08f9e90bf3306807fa0549c77
mstr2 <43553916+mstr2@users.noreply.github.com>
2024-08-04 18:01:06 +0200

    add tests

--- modules/javafx.graphics/src/test/java/test/com/sun/javafx/util/InterpolationUtilsTest.java
+++ modules/javafx.graphics/src/test/java/test/com/sun/javafx/util/InterpolationUtilsTest.java
@@ -26,31 +26,114 @@
 package test.com.sun.javafx.util;
 
 import com.sun.javafx.util.InterpolationUtils;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Paint;
 import javafx.scene.paint.RadialGradient;
 import javafx.scene.paint.Stop;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
+import static javafx.scene.paint.Color.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InterpolationUtilsTest {
+
+    @Nested
+    class PairwiseListInterpolationTest {
+        @Test
+        void differentValuesReturnsNewInstance() {
+            var startValue = List.<Paint>of(RED, BLUE);
+            var endValue = List.<Paint>of(GREEN, YELLOW);
+            var expected = List.of(RED.interpolate(GREEN, 0.5), BLUE.interpolate(YELLOW, 0.5));
+            var actual = InterpolationUtils.interpolateListsPairwise(startValue, endValue, 0.5);
+            assertEquals(expected, actual);
+            assertNotSame(expected, startValue);
+            assertNotSame(expected, endValue);
+            assertThrows(UnsupportedOperationException.class, () -> actual.add(RED));
+        }
+
+        @Test
+        void sameValuesReturnsStartInstance() {
+            var startValue = List.<Paint>of(RED, BLUE);
+            var endValue = List.<Paint>of(RED, BLUE);
+            var actual = InterpolationUtils.interpolateListsPairwise(startValue, endValue, 0.5);
+            assertSame(startValue, actual);
+        }
+
+        @Test
+        void secondListHasMoreElements() {
+            var startValue = List.<Paint>of(RED, BLUE);
+            var endValue = List.<Paint>of(GREEN, YELLOW, PURPLE);
+            var expected = List.of(RED.interpolate(GREEN, 0.5), BLUE.interpolate(YELLOW, 0.5), PURPLE);
+            var actual = InterpolationUtils.interpolateListsPairwise(startValue, endValue, 0.5);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void secondListHasLessElements() {
+            var startValue = List.<Paint>of(RED, BLUE);
+            var endValue = List.<Paint>of(GREEN);
+            var expected = List.of(RED.interpolate(GREEN, 0.5));
+            var actual = InterpolationUtils.interpolateListsPairwise(startValue, endValue, 0.5);
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Nested
+    class PairwiseArrayInterpolationTest {
+        @Test
+        void differentValuesReturnsNewInstance() {
+            var startValue = arrayOf(RED, BLUE);
+            var endValue = arrayOf(GREEN, YELLOW);
+            var expected = arrayOf(RED.interpolate(GREEN, 0.5), BLUE.interpolate(YELLOW, 0.5));
+            var actual = InterpolationUtils.interpolateArraysPairwise(startValue, endValue, 0.5);
+            assertArrayEquals(expected, actual);
+            assertNotSame(expected, startValue);
+            assertNotSame(expected, endValue);
+        }
+
+        @Test
+        void sameValuesReturnsStartInstance() {
+            var startValue = arrayOf(RED, BLUE);
+            var endValue = arrayOf(RED, BLUE);
+            var actual = InterpolationUtils.interpolateArraysPairwise(startValue, endValue, 0.5);
+            assertSame(startValue, actual);
+        }
+
+        @Test
+        void secondArrayHasMoreElements() {
+            var startValue = arrayOf(RED, BLUE);
+            var endValue = arrayOf(GREEN, YELLOW, PURPLE);
+            var expected = arrayOf(RED.interpolate(GREEN, 0.5), BLUE.interpolate(YELLOW, 0.5), PURPLE);
+            var actual = InterpolationUtils.interpolateArraysPairwise(startValue, endValue, 0.5);
+            assertArrayEquals(expected, actual);
+        }
+
+        @Test
+        void secondArrayHasLessElements() {
+            var startValue = arrayOf(RED, BLUE);
+            var endValue = arrayOf(GREEN);
+            var expected = arrayOf(RED.interpolate(GREEN, 0.5));
+            var actual = InterpolationUtils.interpolateArraysPairwise(startValue, endValue, 0.5);
+            assertArrayEquals(expected, actual);
+        }
+    }
 
     @Nested
     class InterpolateColorTest {
         @Test
         void toLinearGradient() {
             var paint = InterpolationUtils.interpolatePaint(
-                Color.BLUE,
+                BLUE,
                 new LinearGradient(0, 0, 1, 1, false, CycleMethod.NO_CYCLE,
-                                   new Stop(0, Color.RED), new Stop(1, Color.GREEN)),
+                                   new Stop(0, RED), new Stop(1, GREEN)),
                 0.5);
 
             var expected = new LinearGradient(0, 0, 1, 1, false, CycleMethod.NO_CYCLE,
-                                              new Stop(0, Color.BLUE.interpolate(Color.RED, 0.5)),
-                                              new Stop(1, Color.BLUE.interpolate(Color.GREEN, 0.5)));
+                                              new Stop(0, BLUE.interpolate(RED, 0.5)),
+                                              new Stop(1, BLUE.interpolate(GREEN, 0.5)));
 
             assertEquals(expected, paint);
         }
@@ -58,14 +141,14 @@ public class InterpolationUtilsTest {
         @Test
         void toRadialGradient() {
             var paint = InterpolationUtils.interpolatePaint(
-                Color.BLUE,
+                BLUE,
                 new RadialGradient(0, 0, 0, 0, 10, false, CycleMethod.NO_CYCLE,
-                                   new Stop(0, Color.RED), new Stop(1, Color.GREEN)),
+                                   new Stop(0, RED), new Stop(1, GREEN)),
                 0.5);
 
             var expected = new RadialGradient(0, 0, 0, 0, 10, false, CycleMethod.NO_CYCLE,
-                                              new Stop(0, Color.BLUE.interpolate(Color.RED, 0.5)),
-                                              new Stop(1, Color.BLUE.interpolate(Color.GREEN, 0.5)));
+                                              new Stop(0, BLUE.interpolate(RED, 0.5)),
+                                              new Stop(1, BLUE.interpolate(GREEN, 0.5)));
 
             assertEquals(expected, paint);
         }
@@ -77,13 +160,13 @@ public class InterpolationUtilsTest {
         void toColor() {
             var paint = InterpolationUtils.interpolatePaint(
                 new LinearGradient(0, 0, 1, 1, false, CycleMethod.NO_CYCLE,
-                                   new Stop(0, Color.RED), new Stop(1, Color.GREEN)),
-                Color.BLUE,
+                                   new Stop(0, RED), new Stop(1, GREEN)),
+                BLUE,
                 0.5);
 
             var expected = new LinearGradient(0, 0, 1, 1, false, CycleMethod.NO_CYCLE,
-                                              new Stop(0, Color.RED.interpolate(Color.BLUE, 0.5)),
-                                              new Stop(1, Color.GREEN.interpolate(Color.BLUE, 0.5)));
+                                              new Stop(0, RED.interpolate(BLUE, 0.5)),
+                                              new Stop(1, GREEN.interpolate(BLUE, 0.5)));
 
             assertEquals(expected, paint);
         }
@@ -91,11 +174,11 @@ public class InterpolationUtilsTest {
         @Test
         void toRadialGradient() {
             var toPaint = new RadialGradient(0, 0, 0, 0, 10, false, CycleMethod.NO_CYCLE,
-                                             new Stop(0, Color.RED), new Stop(1, Color.GREEN));
+                                             new Stop(0, RED), new Stop(1, GREEN));
 
             var actual = InterpolationUtils.interpolatePaint(
                 new LinearGradient(0, 0, 1, 1, false, CycleMethod.NO_CYCLE,
-                                   new Stop(0, Color.RED), new Stop(1, Color.GREEN)),
+                                   new Stop(0, RED), new Stop(1, GREEN)),
                 toPaint,
                 0.5);
 
@@ -109,13 +192,13 @@ public class InterpolationUtilsTest {
         void toColor() {
             var paint = InterpolationUtils.interpolatePaint(
                 new RadialGradient(0, 0, 0, 0, 10, false, CycleMethod.NO_CYCLE,
-                                   new Stop(0, Color.RED), new Stop(1, Color.GREEN)),
-                Color.BLUE,
+                                   new Stop(0, RED), new Stop(1, GREEN)),
+                BLUE,
                 0.5);
 
             var expected = new RadialGradient(0, 0, 0, 0, 10, false, CycleMethod.NO_CYCLE,
-                                              new Stop(0, Color.RED.interpolate(Color.BLUE, 0.5)),
-                                              new Stop(1, Color.GREEN.interpolate(Color.BLUE, 0.5)));
+                                              new Stop(0, RED.interpolate(BLUE, 0.5)),
+                                              new Stop(1, GREEN.interpolate(BLUE, 0.5)));
 
             assertEquals(expected, paint);
         }
@@ -123,15 +206,20 @@ public class InterpolationUtilsTest {
         @Test
         void toLinearGradient() {
             var toPaint = new LinearGradient(0, 0, 1, 1, false, CycleMethod.NO_CYCLE,
-                                             new Stop(0, Color.RED), new Stop(1, Color.GREEN));
+                                             new Stop(0, RED), new Stop(1, GREEN));
 
             var actual = InterpolationUtils.interpolatePaint(
                 new RadialGradient(0, 0, 0, 0, 10, false, CycleMethod.NO_CYCLE,
-                                   new Stop(0, Color.RED), new Stop(1, Color.GREEN)),
+                                   new Stop(0, RED), new Stop(1, GREEN)),
                 toPaint,
                 0.5);
 
             assertSame(actual, toPaint);
         }
+    }
+
+    @SafeVarargs
+    private static <T> T[] arrayOf(T... values) {
+        return values;
     }
 }
