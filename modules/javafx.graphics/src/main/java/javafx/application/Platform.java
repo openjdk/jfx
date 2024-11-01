00de674f8a5c30250bf1516188bd9c876ad9a557
mstr2 <43553916+mstr2@users.noreply.github.com>
2024-11-01 18:41:59 +0100

    add platform keys to javadoc

--- modules/javafx.graphics/src/main/java/javafx/application/Platform.java
+++ modules/javafx.graphics/src/main/java/javafx/application/Platform.java
@@ -497,6 +497,7 @@ public final class Platform {
      *         <tr><td>{@code Windows.UIColor.AccentLight2}</td><td>{@link Color}</td></tr>
      *         <tr><td>{@code Windows.UIColor.AccentLight3}</td><td>{@link Color}</td></tr>
      *         <tr><td>{@code Windows.UISettings.AdvancedEffectsEnabled}</td><td>{@link Boolean}</td></tr>
+     *         <tr><td>{@code Windows.UISettings.AutoHideScrollBars}</td><td>{@link Boolean}</td></tr>
      *         <tr></tr>
      *     </tbody>
      * </table>
@@ -551,6 +552,7 @@ public final class Platform {
      *         <tr><td>{@code macOS.NSColor.systemYellowColor}</td><td>{@link Color}</td></tr>
      *         <tr><td>{@code macOS.NSWorkspace.accessibilityDisplayShouldReduceMotion}</td><td>{@link Boolean}</td></tr>
      *         <tr><td>{@code macOS.NSWorkspace.accessibilityDisplayShouldReduceTransparency}</td><td>{@link Boolean}</td></tr>
+     *         <tr><td>{@code macOS.NSScroller.preferredScrollerStyle}</td><td>{@link String}</td></tr>
      *         <tr></tr>
      *     </tbody>
      * </table>
@@ -577,6 +579,7 @@ public final class Platform {
      *         <tr><td>{@code GTK.error_color}</td><td>{@link Color}</td></tr>
      *         <tr><td>{@code GTK.success_color}</td><td>{@link Color}</td></tr>
      *         <tr><td>{@code GTK.enable_animations}</td><td>{@link Boolean}</td></tr>
+     *         <tr><td>{@code GTK.overlay_scrolling}</td><td>{@link Boolean}</td></tr>
      *         <tr></tr>
      *     </tbody>
      * </table>
