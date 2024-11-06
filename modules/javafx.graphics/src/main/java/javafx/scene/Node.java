3d034276a0ef80d7dbec2a2091e09a2a14baa2e8
Andy Goryachev <andy.goryachev@oracle.com>
2024-11-06 14:05:13 -0800

    review comments

--- modules/javafx.graphics/src/main/java/javafx/scene/Node.java
+++ modules/javafx.graphics/src/main/java/javafx/scene/Node.java
@@ -8532,11 +8532,11 @@ public abstract class Node implements EventTarget, Styleable {
     }
 
     /**
-     * Tries to move the focus from this {@code Node} in the specified direction.
+     * Requests to move the focus from this {@code Node} in the specified direction.
      * The {@code Node} serves as a reference point and does not have to be focused or focusable.
      * A successful traversal results in a new {@code Node} being focused.
      * <p>
-     * This method is expected to be called in response to a {@code KeyEvent}, since the {@code Node}
+     * This method is expected to be called in response to a {@code KeyEvent}; therefore the {@code Node}
      * receiving focus will have the {@link #focusVisibleProperty() focusVisible} property set.
      *
      * @param direction the direction of focus traversal
