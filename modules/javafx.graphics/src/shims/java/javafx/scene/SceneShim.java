198596ba8d23ac4895a6b464b0c8071bbe051af4
mstr2 <43553916+mstr2@users.noreply.github.com>
2024-11-11 00:25:17 +0100

    added tests

--- modules/javafx.graphics/src/shims/java/javafx/scene/SceneShim.java
+++ modules/javafx.graphics/src/shims/java/javafx/scene/SceneShim.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,13 @@
 
 package javafx.scene;
 
+import javafx.scene.input.KeyEvent;
+
 public class SceneShim {
+
+    public static void processKeyEvent(Scene s, KeyEvent e) {
+        s.processKeyEvent(e);
+    }
 
     public static void focusCleanup(Scene s) {
         s.focusCleanup();
