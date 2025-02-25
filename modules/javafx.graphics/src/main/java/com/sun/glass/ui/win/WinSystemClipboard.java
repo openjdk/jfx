c686954ca37173d8b6f49f767dbb35ef78c276af
Oliver Schmidtmer <O.Schmidtmer@elo.com>
2025-02-25 14:15:27 +0100

    JDK-8281384: Random chars on paste from Windows clipboard

--- modules/javafx.graphics/src/main/java/com/sun/glass/ui/win/WinSystemClipboard.java
+++ modules/javafx.graphics/src/main/java/com/sun/glass/ui/win/WinSystemClipboard.java
@@ -252,7 +252,15 @@ class WinSystemClipboard extends SystemClipboard {
             if (TEXT_TYPE.equals(mime) || URI_TYPE.equals(mime)) {
                 try {
                     // JDK-8118474 - internal Windows data null terminated
-                    return new String(data, 0, data.length - 2, defaultCharset);
+                    // JDK-8281384 - buffer might be larger than data and null terminator not at the end
+                    int nullTerm = data.length - 2;
+                    for (int i = 0; i < data.length; i += 2) {
+                        if (data[i] == 0) {
+                            nullTerm = i;
+                            break;
+                        }
+                    }
+                    return new String(data, 0, nullTerm, defaultCharset);
                 } catch (UnsupportedEncodingException ex) {
                     //never happen
                 }
