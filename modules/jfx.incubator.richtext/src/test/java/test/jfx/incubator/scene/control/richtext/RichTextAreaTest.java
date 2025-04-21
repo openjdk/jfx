0fb16fdc265d7aab81db5585a5175ec52806c7ec
Andy Goryachev <andy.goryachev@oracle.com>
2025-04-21 08:14:41 -0700

    rm junit4

--- modules/jfx.incubator.richtext/src/test/java/test/jfx/incubator/scene/control/richtext/RichTextAreaTest.java
+++ modules/jfx.incubator.richtext/src/test/java/test/jfx/incubator/scene/control/richtext/RichTextAreaTest.java
@@ -25,9 +25,9 @@
 
 package test.jfx.incubator.scene.control.richtext;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
