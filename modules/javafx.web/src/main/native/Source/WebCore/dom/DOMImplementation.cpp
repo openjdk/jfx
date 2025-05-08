2f4039262eea6cd3f2f682083fc4b90909190daa
Gopal Pattnaik <gopal.pattnaik@oracle.com>
2025-05-08 11:24:27 +0530

    JDK-8260020 Code review points addressed

--- modules/javafx.web/src/main/native/Source/WebCore/dom/DOMImplementation.cpp
+++ modules/javafx.web/src/main/native/Source/WebCore/dom/DOMImplementation.cpp
@@ -164,8 +164,11 @@ Ref<Document> DOMImplementation::createDocument(const String& contentType, Local
         return XMLDocument::createXHTML(frame, settings, url);
     if (equalLettersIgnoringASCIICase(contentType, "text/plain"_s))
         return TextDocument::create(frame, settings, url, documentIdentifier);
+
+#if PLATFORM(JAVA)
     if (equalLettersIgnoringASCIICase(contentType, "application/octet-stream"_s))
         return TextDocument::create(frame, settings, url, documentIdentifier);
+#endif
 
 #if ENABLE(PDFJS)
     if (frame && settings.pdfJSViewerEnabled() && MIMETypeRegistry::isPDFMIMEType(contentType))
