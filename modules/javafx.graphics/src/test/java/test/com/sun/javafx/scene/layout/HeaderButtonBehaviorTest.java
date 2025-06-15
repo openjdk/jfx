b3d7ae624dadbd4e102f125b56112f13928978cd
mstr2 <43553916+mstr2@users.noreply.github.com>
2025-06-15 21:22:42 +0200

    Fix disabled states of custom window buttons

---
+++ modules/javafx.graphics/src/test/java/test/com/sun/javafx/scene/layout/HeaderButtonBehaviorTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package test.com.sun.javafx.scene.layout;
+
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.HeaderBar;
+import javafx.scene.layout.HeaderButtonType;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HeaderButtonBehaviorTest {
+
+    enum ButtonDisabledStateTest {
+        RESIZABLE(true, false, false, false),
+        UNRESIZABLE(false, false, false, true),
+        MODAL_RESIZABLE(true, true, true, false),
+        MODAL_UNRESIZABLE(false, true, true, true);
+
+        ButtonDisabledStateTest(boolean resizable, boolean modal, boolean iconifyDisabled, boolean maximizeDisabled) {
+            this.resizable = resizable;
+            this.modal = modal;
+            this.iconifyDisabled = iconifyDisabled;
+            this.maximizeDisabled = maximizeDisabled;
+        }
+
+        final boolean resizable;
+        final boolean modal;
+        final boolean iconifyDisabled;
+        final boolean maximizeDisabled;
+    }
+
+    /**
+     * Tests the disabled states of the iconify and maximize buttons for all combinations
+     * of resizable and modal window attributes.
+     */
+    @ParameterizedTest
+    @EnumSource(ButtonDisabledStateTest.class)
+    void buttonDisabledStateIsCorrect(ButtonDisabledStateTest test) {
+        Node iconify = new Group(), maximize = new Group(), close = new Group();
+        HeaderBar.setButtonType(iconify, HeaderButtonType.ICONIFY);
+        HeaderBar.setButtonType(maximize, HeaderButtonType.MAXIMIZE);
+        HeaderBar.setButtonType(close, HeaderButtonType.CLOSE);
+
+        var stage = new Stage();
+        stage.initModality(test.modal ? Modality.WINDOW_MODAL : Modality.NONE);
+        stage.setResizable(test.resizable);
+        stage.setScene(new Scene(new Group(iconify, maximize, close)));
+        stage.show();
+
+        assertEquals(test.iconifyDisabled, iconify.isDisabled());
+        assertEquals(test.maximizeDisabled, maximize.isDisabled());
+        assertFalse(close.isDisabled());
+    }
+}
