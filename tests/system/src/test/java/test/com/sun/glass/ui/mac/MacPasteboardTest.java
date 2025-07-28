ceec1c3e7f7f27e8527afbc44e3f95c907d0a972
Johan Vos <johan@lodgon.com>
2025-07-28 22:38:53 +0200

    Add PlatformUtil.isHeadless() method. Use that in tests to conditionally skip tests.
    Fix JDK-8364203

--- tests/system/src/test/java/test/com/sun/glass/ui/mac/MacPasteboardTest.java
+++ tests/system/src/test/java/test/com/sun/glass/ui/mac/MacPasteboardTest.java
@@ -67,7 +67,7 @@ public class MacPasteboardTest {
 
     @Test
     public void testValidLocalImageURLMacPasteboard() throws Exception {
-        assumeTrue(PlatformUtil.isMac());
+        assumeTrue(PlatformUtil.isMac() && !PlatformUtil.isHeadless());
         final String localImage = getClass().getResource("blue.png").toURI().toURL().toString();
         runAndWait(() -> {
             macPasteboardShim.pushMacPasteboard(new HashMap<>(Map.of(Clipboard.URI_TYPE, localImage)));
@@ -82,7 +82,7 @@ public class MacPasteboardTest {
 
     @Test
     public void testDataBase64ImageMacPasteboard() {
-        assumeTrue(PlatformUtil.isMac());
+        assumeTrue(PlatformUtil.isMac() && !PlatformUtil.isHeadless());
         final String encodedImage = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAKCAIAAAA7N+mxAAAAAXNSR0IArs4c6QAAAAR"
                 + "nQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAcSURBVChTY/jPwADBZACyNMHAqGYSwZDU/P8/ABieT81GAGKoAAAAAElFTkSuQmCC";
         runAndWait(() -> {
@@ -94,7 +94,7 @@ public class MacPasteboardTest {
 
     @Test
     public void testNotAnImageURLMacPasteboard() {
-        assumeTrue(PlatformUtil.isMac());
+        assumeTrue(PlatformUtil.isMac() && !PlatformUtil.isHeadless());
         final String invalidImage = "not.an.image.url";
         runAndWait(() -> {
             macPasteboardShim.pushMacPasteboard(new HashMap<>(Map.of(Clipboard.URI_TYPE, invalidImage)));
