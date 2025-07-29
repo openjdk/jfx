2d832143e14452585433f299728fb516bc062896
Ziad El Midaoui <ziad.el.midaoui@oracle.com>
2025-07-29 12:28:49 +0100

    Update TreeCellSkin.java

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/TreeCellSkin.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/TreeCellSkin.java
@@ -359,8 +359,6 @@ public class TreeCellSkin<T> extends CellSkinBase<TreeCell<T>> {
     }
 
     private void updateDisclosureNode() {
-//        JDK-8341281 : Root TreeItem with null value breaks TreeTableView
-//        if (getSkinnable().isEmpty()) return;
 
         Node disclosureNode = getSkinnable().getDisclosureNode();
         if (disclosureNode == null) return;
