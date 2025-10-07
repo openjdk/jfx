42c035cf406261d5ad70c88296e37396a1194b3b
Kevin Rushforth <kcr@openjdk.org>
2025-10-07 08:35:36 -0700

    review comments: Dialog --> Stage and copyright year

--- apps/toys/Hello/src/main/java/hello/HelloModality.java
+++ apps/toys/Hello/src/main/java/hello/HelloModality.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -121,7 +121,7 @@ public class HelloModality extends Application {
         root.getChildren().add(stButton);
         root.getChildren().add(suButton);
 
-        Button button = new Button("Create Dialog");
+        Button button = new Button("Create Stage");
         button.setTooltip(new Tooltip("Creates a new stage"));
         button.setLayoutX(100);
         button.setLayoutY(200);
