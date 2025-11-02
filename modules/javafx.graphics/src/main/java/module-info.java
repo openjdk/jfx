07971235c847f29b1d54d5da2f8a55ed0a46a2a7
Marius Hanl <mariushanl@web.de>
2025-11-02 20:12:35 +0100

    8240844: Remove dependency on java.desktop from javafx.base

--- modules/javafx.graphics/src/main/java/module-info.java
+++ modules/javafx.graphics/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -40,9 +40,10 @@
  * @since 9
  */
 module javafx.graphics {
-    requires java.desktop;
     requires java.xml;
     requires jdk.unsupported;
+
+    requires static java.desktop;
 
     requires transitive javafx.base;
 
