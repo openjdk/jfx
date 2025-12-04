7fcadcddddc2929761bb74e1eb425e478e20e621
Kevin Rushforth <kcr@openjdk.org>
2025-12-04 15:40:37 -0800

    Add a comment

--- modules/javafx.graphics/src/main/java/com/sun/javafx/css/InterpolatorConverter.java
+++ modules/javafx.graphics/src/main/java/com/sun/javafx/css/InterpolatorConverter.java
@@ -114,6 +114,7 @@ public class InterpolatorConverter extends StyleConverter<Object, Interpolator> 
                 });
 
                 default -> throw new AssertionError();
+                // This is the end of the switch expr
             };
         }
 
