69a422ad41fc7e80e49619edcd6b303e7c27f059
Andy Goryachev <andy.goryachev@oracle.com>
2025-12-12 15:53:16 -0800

    junit

--- modules/javafx.graphics/src/test/java/test/javafx/scene/input/DataFormatTest.java
+++ modules/javafx.graphics/src/test/java/test/javafx/scene/input/DataFormatTest.java
@@ -25,10 +25,10 @@
 
 package test.javafx.scene.input;
 
-import static org.junit.Assert.assertSame;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.stream.Stream;
