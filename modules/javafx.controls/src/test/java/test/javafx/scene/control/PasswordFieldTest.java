855c092ef53fe9c805af0f68cee8bcedd33f06a7
Christopher Schnick <crschnick@xpipe.io>
2025-12-24 18:41:30 +0100

    Fix test format

--- modules/javafx.controls/src/test/java/test/javafx/scene/control/PasswordFieldTest.java
+++ modules/javafx.controls/src/test/java/test/javafx/scene/control/PasswordFieldTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -98,7 +98,8 @@ public class PasswordFieldTest {
         assertEquals(string.length()-1, pwdField.getLength());
     }
 
-    @Test public void accessibleAttributeText() {
+    @Test
+    public void accessibleAttributeText() {
         pwdField.setText("secret");
         assertEquals("", pwdField.queryAccessibleAttribute(AccessibleAttribute.TEXT));
 
