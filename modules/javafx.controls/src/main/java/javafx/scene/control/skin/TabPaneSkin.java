85a2926a4e3c67fc3a29caa73f2293c4382d6e0d
Andy Goryachev <andy.goryachev@oracle.com>
2026-01-08 10:05:50 -0800

    2026

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/TabPaneSkin.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/TabPaneSkin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -266,7 +266,7 @@ public class TabPaneSkin extends SkinBase<TabPane> {
      * <p>
      * Changing this property while the menu is shown has no effect.
      *
-     * @since 25
+     * @since 27
      * @defaultValue null
      */
     private ObjectProperty<Function<Tab, Node>> menuGraphicFactory;
