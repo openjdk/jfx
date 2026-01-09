e08227ca5aa665412741c3dcb19b7f2bfdc2095e
mstr2 <43553916+mstr2@users.noreply.github.com>
2026-01-09 03:47:04 +0100

    update copyright years

--- modules/javafx.graphics/src/test/java/test/javafx/css/StylesheetTest.java
+++ modules/javafx.graphics/src/test/java/test/javafx/css/StylesheetTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,7 +28,6 @@ package test.javafx.css;
 import com.sun.javafx.css.RuleHelper;
 import com.sun.javafx.css.SimpleSelector;
 import com.sun.javafx.css.StyleManager;
-import com.sun.javafx.css.media.MediaQuery;
 import com.sun.javafx.css.media.expression.FunctionExpression;
 import javafx.application.ColorScheme;
 import javafx.css.StyleConverter.StringStore;
