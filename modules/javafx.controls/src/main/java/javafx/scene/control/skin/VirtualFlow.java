openjdk/jfx

commit 1b12c8a490a87691e7e815a7e768854354cb54aa

8185887: TableRowSkinBase fails to correctly virtualize cells in horizontal direction 8276326: Empty Cells in TableView supposedly after using setFixedCellSize() 8252566: TreeTableView: broken row layout for fixedCellSize 8346824: Collapsing tree view causes rendering issues
Reviewed-by: angorya, mstrauss

modules/javafx.controls/src/main/java/javafx/scene/control/skin/VirtualFlow.java
@@ -1081,7 +1081,7 @@ public class VirtualFlow<T extends IndexedCell> extends Region {
             lastWidth = -1;
             lastHeight = -1;
             releaseCell(accumCell);
-            sheet.getChildren().clear();
+            sheetChildren.clear();
 
             resetIndex(cells);
             resetIndex(pile);
@@ -1138,6 +1138,13 @@ public class VirtualFlow<T extends IndexedCell> extends Region {
                     cell.requestLayout();
                 }
             }
+
+            // Also request layout for cells in the pile. As soon as those are reused (and therefore added),
+            // they will do their layout.
+            for (T cell : pile) {
+                cell.requestLayout();
+            }
+
             needsCellsLayout = false;
             // yes, we return here - if needsCellsLayout was set to true, we
             // only did it to do the above - not rerun the entire layout.
@@ -1916,7 +1923,7 @@ public class VirtualFlow<T extends IndexedCell> extends Region {
     private final void setViewportBreadth(double value) {
         this.viewportBreadth = value;
     }
-    private final double getViewportBreadth() {
+    final double getViewportBreadth() {
         return viewportBreadth;
     }
 
@@ -2841,6 +2848,11 @@ public class VirtualFlow<T extends IndexedCell> extends Region {
             T cell = pile.get(i);
             wasFocusOwner = wasFocusOwner || doesCellContainFocus(cell);
             cell.setVisible(false);
+        }
+
+        // Remove all cells that are in the pile and therefore not relevant anymore.
+        if (sheetChildren.size() != cells.size()) {
+            sheetChildren.removeAll(pile);
         }
 
         // Fix for JDK-8095710: Rather than have the cells do weird things with

modules/javafx.controls/src/main/java/javafx/scene/control/skin/TableRowSkin.java
@@ -32,7 +32,6 @@ import java.util.List;
 import com.sun.javafx.scene.control.behavior.BehaviorBase;
 import com.sun.javafx.scene.control.behavior.TableRowBehavior;
 
-import javafx.beans.property.DoubleProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.AccessibleAttribute;
@@ -93,34 +92,41 @@ public class TableRowSkin<T> extends TableRowSkinBase<T, TableRow<T>, TableCell<
             }
         });
 
-        setupTreeTableViewListeners();
+        setupTableViewListeners();
     }
 
     // FIXME: replace listener to fixedCellSize with direct lookup - JDK-8277000
-    private void setupTreeTableViewListeners() {
+    private void setupTableViewListeners() {
         TableView<T> tableView = getSkinnable().getTableView();
         if (tableView == null) {
             registerInvalidationListener(getSkinnable().tableViewProperty(), e -> {
                 unregisterInvalidationListeners(getSkinnable().tableViewProperty());
-                setupTreeTableViewListeners();
+                setupTableViewListeners();
             });
         } else {
-            DoubleProperty fixedCellSizeProperty = tableView.fixedCellSizeProperty();
-            if (fixedCellSizeProperty != null) {
-                registerChangeListener(fixedCellSizeProperty, e -> {
-                    fixedCellSize = fixedCellSizeProperty.get();
-                    fixedCellSizeEnabled = fixedCellSize > 0;
-                });
-                fixedCellSize = fixedCellSizeProperty.get();
-                fixedCellSizeEnabled = fixedCellSize > 0;
-
-                // JDK-8144500:
-                // When in fixed cell size mode, we must listen to the width of the virtual flow, so
-                // that when it changes, we can appropriately add / remove cells that may or may not
-                // be required (because we remove all cells that are not visible).
+            registerChangeListener(tableView.fixedCellSizeProperty(), e -> {
                 VirtualFlow<TableRow<T>> virtualFlow = getVirtualFlow();
                 if (virtualFlow != null) {
-                    registerChangeListener(virtualFlow.widthProperty(), e -> tableView.requestLayout());
+                    unregisterChangeListeners(virtualFlow.widthProperty());
+                }
+
+                updateCachedFixedSize();
+            });
+
+            updateCachedFixedSize();
+        }
+    }
+
+    private void updateCachedFixedSize() {
+        TableView<T> tableView = getSkinnable().getTableView();
+        if (tableView != null) {
+            fixedCellSize = tableView.getFixedCellSize();
+            fixedCellSizeEnabled = fixedCellSize > 0;
+
+            if (fixedCellSizeEnabled) {
+                VirtualFlow<TableRow<T>> virtualFlow = getVirtualFlow();
+                if (virtualFlow != null) {
+                    registerChangeListener(virtualFlow.widthProperty(), e -> getSkinnable().requestLayout());
                 }
             }
         }

modules/javafx.controls/src/main/java/javafx/scene/control/skin/TableRowSkinBase.java
@@ -36,7 +36,6 @@ import javafx.beans.property.ObjectProperty;
 import javafx.collections.ObservableList;
 import javafx.css.StyleOrigin;
 import javafx.css.StyleableObjectProperty;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -95,11 +94,6 @@ public abstract class TableRowSkinBase<T,
      */
     static final Map<TableColumnBase<?,?>, Double> maxDisclosureWidthMap = new WeakHashMap<>();
 
-    // Specifies the number of times we will call 'recreateCells()' before we blow
-    // out the cellsMap structure and rebuild all cells. This helps to prevent
-    // against memory leaks in certain extreme circumstances.
-    private static final int DEFAULT_FULL_REFRESH_COUNTER = 100;
-
 
     /* *************************************************************************
      *                                                                         *
@@ -113,21 +107,13 @@ public abstract class TableRowSkinBase<T,
      * efficiency we create cells for all columns, even if they aren't visible,
      * and we only create new cells if we don't already have it cached in this
      * map.
-     *
-     * Note that this means that it is possible for this map to therefore be
-     * a memory leak if an application uses TableView and is creating and removing
-     * a large number of tableColumns. This is mitigated in the recreateCells()
-     * function below - refer to that to learn more.
      */
     WeakHashMap<TableColumnBase, Reference<R>> cellsMap;
 
     // This observableArrayList contains the currently visible table cells for this row.
     final List<R> cells = new ArrayList<>();
 
-    private int fullRefreshCounter = DEFAULT_FULL_REFRESH_COUNTER;
-
     boolean isDirty = false;
-    boolean updateCells = false;
 
     // FIXME: replace cached values with direct lookup - JDK-8277000
     double fixedCellSize;
@@ -152,7 +138,7 @@ public abstract class TableRowSkinBase<T,
         getSkinnable().setPickOnBounds(false);
 
         recreateCells();
-        updateCells(true);
+        updateCells();
 
         // init bindings
         // watches for any change in the leaf columns observableArrayList - this will indicate
@@ -176,7 +162,7 @@ public abstract class TableRowSkinBase<T,
      *                                                                         *
      **************************************************************************/
 
-    private void updateLeafColumns() {
+    void updateLeafColumns() {
         isDirty = true;
         getSkinnable().requestLayout();
     }
@@ -289,7 +275,6 @@ public abstract class TableRowSkinBase<T,
                         // earlier rows to update themselves to take into account
                         // this increased indentation.
                         final VirtualFlow<C> flow = getVirtualFlow();
-                        final int thisIndex = getSkinnable().getIndex();
                         for (int i = 0; i < flow.cells.size(); i++) {
                             C cell = flow.cells.get(i);
                             if (cell == null || cell.isEmpty()) continue;
@@ -318,9 +303,12 @@ public abstract class TableRowSkinBase<T,
         int index = control.getIndex();
         if (index < 0/* || row >= itemsProperty().get().size()*/) return;
 
+        VirtualFlow<C> virtualFlow = getVirtualFlow();
         for (int column = 0, max = cells.size(); column < max; column++) {
             R tableCell = cells.get(column);
             TableColumnBase<T, ?> tableColumn = getTableColumn(tableCell);
+
+            width = snapSizeX(tableColumn.getWidth());
 
             boolean isVisible = true;
             if (fixedCellSizeEnabled) {
@@ -333,7 +321,7 @@ public abstract class TableRowSkinBase<T,
                 // provided by the developer, and this means that we do not have
                 // to concern ourselves with the possibility that the height
                 // may be variable and / or dynamic.
-                isVisible = isColumnPartiallyOrFullyVisible(tableColumn);
+                isVisible = isColumnPartiallyOrFullyVisible(x, width, virtualFlow);
 
                 y = 0;
                 height = fixedCellSize;
@@ -342,12 +330,9 @@ public abstract class TableRowSkinBase<T,
             }
 
             if (isVisible) {
-                if (fixedCellSizeEnabled && tableCell.getParent() == null) {
+                if (tableCell.getParent() == null) {
                     getChildren().add(tableCell);
                 }
-                // Note: prefWidth() has to be called only after the tableCell is added to the tableRow, if it wasn't
-                // already. Otherwise, it might not have its skin yet, and its pref width is therefore 0.
-                width = tableCell.prefWidth(height);
 
                 // Added for JDK-8115536, and then updated for JDK-8122708.
                 // We change the alignment from CENTER_LEFT to TOP_LEFT if the
@@ -419,11 +404,7 @@ public abstract class TableRowSkinBase<T,
                 // This does not appear to impact performance...
                 tableCell.requestLayout();
             } else {
-                width = tableCell.prefWidth(height);
-                if (fixedCellSizeEnabled) {
-                    // we only add/remove to the scenegraph if the fixed cell
-                    // length support is enabled - otherwise we keep all
-                    // TableCells in the scenegraph
+                if (tableCell.getParent() != null) {
                     getChildren().remove(tableCell);
                 }
             }
@@ -473,21 +454,9 @@ public abstract class TableRowSkinBase<T,
         return true;
     }
 
-    void updateCells(boolean resetChildren) {
-        // To avoid a potential memory leak (when the TableColumns in the
-        // TableView are created/inserted/removed/deleted, we have a 'refresh
-        // counter' that when we reach 0 will delete all cells in this row
-        // and recreate all of them.
-        if (resetChildren) {
-            if (fullRefreshCounter == 0) {
-                recreateCells();
-            }
-            fullRefreshCounter--;
-        }
-
+    void updateCells() {
         // if clear isn't called first, we can run into situations where the
         // cells aren't updated properly.
-        final boolean cellsEmpty = cells.isEmpty();
         cells.clear();
 
         final C skinnable = getSkinnable();
@@ -518,23 +487,7 @@ public abstract class TableRowSkinBase<T,
             cells.add(cell);
         }
 
-        // update children of each row
-        if (fixedCellSizeEnabled) {
-            // we leave the adding / removing up to the layoutChildren method mostly, but here we remove any children
-            // cells that refer to columns that are removed or not visible.
-            List<Node> toRemove = new ArrayList<>();
-            for (Node cell : getChildren()) {
-                if (!(cell instanceof IndexedCell)) continue;
-                TableColumnBase<T, ?> tableColumn = getTableColumn((R) cell);
-                if (!getVisibleLeafColumns().contains(tableColumn)) {
-                    toRemove.add(cell);
-                }
-            }
-            getChildren().removeAll(toRemove);
-        }
-        if (resetChildren || cellsEmpty) {
-            getChildren().setAll(cells);
-        }
+        getChildren().setAll(cells);
     }
 
     VirtualFlow<C> getVirtualFlow() {
@@ -630,12 +583,8 @@ public abstract class TableRowSkinBase<T,
 
     final void checkState() {
         if (isDirty) {
-            updateCells(true);
+            updateCells();
             isDirty = false;
-            updateCells = false;
-        } else if (updateCells) {
-            updateCells(false);
-            updateCells = false;
         }
     }
 
@@ -655,33 +604,18 @@ public abstract class TableRowSkinBase<T,
      *                                                                         *
      **************************************************************************/
 
-    private boolean isColumnPartiallyOrFullyVisible(TableColumnBase col) {
-        if (col == null || !col.isVisible()) return false;
+    private boolean isColumnPartiallyOrFullyVisible(double start, double width, VirtualFlow<C> virtualFlow) {
+        double end = start + width;
 
-        final VirtualFlow<?> virtualFlow = getVirtualFlow();
         double scrollX = virtualFlow == null ? 0.0 : virtualFlow.getHbar().getValue();
+        double headerWidth = virtualFlow == null ? 0.0 : virtualFlow.getViewportBreadth();
+        double virtualFlowWidth = headerWidth + scrollX;
 
-        // work out where this column header is, and it's width (start -> end)
-        double start = 0;
-        final ObservableList<? extends TableColumnBase> visibleLeafColumns = getVisibleLeafColumns();
-        for (int i = 0, max = visibleLeafColumns.size(); i < max; i++) {
-            TableColumnBase<?,?> c = visibleLeafColumns.get(i);
-            if (c.equals(col)) break;
-            start += c.getWidth();
-        }
-        double end = start + col.getWidth();
-
-        // determine the width of the table
-        final Insets padding = getSkinnable().getPadding();
-        double headerWidth = getSkinnable().getWidth() - padding.getLeft() + padding.getRight();
-
-        return (start >= scrollX || end > scrollX) && (start < (headerWidth + scrollX) || end <= (headerWidth + scrollX));
+        return (start >= scrollX || end > scrollX) && (start < virtualFlowWidth || end <= virtualFlowWidth);
     }
 
     private void requestCellUpdate() {
-        updateCells = true;
         getSkinnable().requestLayout();
-
         // update the index of all children cells (JDK-8119094).
         // Note that we do this after the TableRow item has been updated,
         // rather than when the TableRow index has changed (as this will be
@@ -713,7 +647,6 @@ public abstract class TableRowSkinBase<T,
         ObservableList<? extends TableColumnBase/*<T,?>*/> columns = getVisibleLeafColumns();
 
         cellsMap = new WeakHashMap<>(columns.size());
-        fullRefreshCounter = DEFAULT_FULL_REFRESH_COUNTER;
         getChildren().clear();
 
         for (TableColumnBase col : columns) {

modules/javafx.controls/src/main/java/javafx/scene/control/skin/TreeTableRowSkin.java
@@ -72,7 +72,6 @@ public class TreeTableRowSkin<T> extends TableRowSkinBase<TreeItem<T>, TreeTable
     private boolean disclosureNodeDirty = true;
     private Node graphic;
     private final BehaviorBase<TreeTableRow<T>> behavior;
-    private boolean childrenDirty = false;
 
     /* *************************************************************************
      *                                                                         *
@@ -97,10 +96,6 @@ public class TreeTableRowSkin<T> extends TableRowSkinBase<TreeItem<T>, TreeTable
 
         ListenerHelper lh = ListenerHelper.get(this);
 
-        lh.addChangeListener(control.indexProperty(), (ev) -> {
-            updateCells = true;
-        });
-
         lh.addChangeListener(control.treeItemProperty(), (ev) -> {
             updateTreeItem();
             // There used to be an isDirty = true statement here, but this was
@@ -121,36 +116,18 @@ public class TreeTableRowSkin<T> extends TableRowSkinBase<TreeItem<T>, TreeTable
             });
         } else {
             registerChangeListener(treeTableView.treeColumnProperty(), (x) -> {
-                // Fix for JDK-8124861: Need to set isDirty to true, rather than the
-                // cheaper updateCells, as otherwise the text indentation will not
-                // be recalculated in TreeTableCellSkin.calculateIndentation()
-                isDirty = true;
-                if (getSkinnable() != null) {
-                    getSkinnable().requestLayout();
-                }
+                updateLeafColumns();
             });
 
-            DoubleProperty fixedCellSizeProperty = getTreeTableView().fixedCellSizeProperty();
-            if (fixedCellSizeProperty != null) {
-                registerChangeListener(fixedCellSizeProperty, (x) -> {
-                    updateCachedFixedSize();
-                });
-                updateCachedFixedSize();
-
-                // JDK-8144500:
-                // When in fixed cell size mode, we must listen to the width of the virtual flow, so
-                // that when it changes, we can appropriately add / remove cells that may or may not
-                // be required (because we remove all cells that are not visible).
+            registerChangeListener(getTreeTableView().fixedCellSizeProperty(), e -> {
                 VirtualFlow<TreeTableRow<T>> virtualFlow = getVirtualFlow();
                 if (virtualFlow != null) {
-                    registerChangeListener(getVirtualFlow().widthProperty(), (x) -> {
-                        if (getSkinnable() != null) {
-                            TreeTableView<T> t = getSkinnable().getTreeTableView();
-                            t.requestLayout();
-                        }
-                    });
+                    unregisterChangeListeners(virtualFlow.widthProperty());
                 }
-            }
+
+                updateCachedFixedSize();
+            });
+            updateCachedFixedSize();
         }
     }
 
@@ -160,6 +137,13 @@ public class TreeTableRowSkin<T> extends TableRowSkinBase<TreeItem<T>, TreeTable
             if (t != null) {
                 fixedCellSize = t.getFixedCellSize();
                 fixedCellSizeEnabled = fixedCellSize > 0.0;
+
+                if (fixedCellSizeEnabled) {
+                    VirtualFlow<TreeTableRow<T>> virtualFlow = getTableViewSkin().getVirtualFlow();
+                    if (virtualFlow != null) {
+                        registerChangeListener(virtualFlow.widthProperty(), ev -> getSkinnable().requestLayout());
+                    }
+                }
             }
         }
     }
@@ -229,29 +213,16 @@ public class TreeTableRowSkin<T> extends TableRowSkinBase<TreeItem<T>, TreeTable
         super.updateChildren();
 
         updateDisclosureNodeAndGraphic();
-
-        if (childrenDirty) {
-            childrenDirty = false;
-            if (cells.isEmpty()) {
-                getChildren().clear();
-            } else {
-                // TODO we can optimise this by only showing cells that are
-                // visible based on the table width and the amount of horizontal
-                // scrolling.
-                getChildren().addAll(cells);
-            }
-        }
     }
 
     /** {@inheritDoc} */
     @Override protected void layoutChildren(double x, double y, double w, double h) {
-        if (disclosureNodeDirty) {
-            updateDisclosureNodeAndGraphic();
-            disclosureNodeDirty = false;
+        Node disclosureNode = getDisclosureNode();
+        if (disclosureNode != null && disclosureNode.getParent() == null) {
+            disclosureNodeDirty = true;
         }
 
-        Node disclosureNode = getDisclosureNode();
-        if (disclosureNode != null && disclosureNode.getScene() == null) {
+        if (disclosureNodeDirty) {
             updateDisclosureNodeAndGraphic();
         }
 
@@ -277,13 +248,10 @@ public class TreeTableRowSkin<T> extends TableRowSkinBase<TreeItem<T>, TreeTable
     }
 
     /** {@inheritDoc} */
-    @Override void updateCells(boolean resetChildren) {
-        super.updateCells(resetChildren);
+    @Override void updateCells() {
+        super.updateCells();
 
-        if (resetChildren) {
-            childrenDirty = true;
-            updateChildren();
-        }
+        updateDisclosureNodeAndGraphic();
     }
 
     /** {@inheritDoc} */
@@ -352,6 +320,8 @@ public class TreeTableRowSkin<T> extends TableRowSkinBase<TreeItem<T>, TreeTable
     }
 
     private void updateDisclosureNodeAndGraphic() {
+        disclosureNodeDirty = false;
+
         if (getSkinnable().isEmpty()) {
             getChildren().remove(graphic);
             return;
@@ -375,14 +345,13 @@ public class TreeTableRowSkin<T> extends TableRowSkinBase<TreeItem<T>, TreeTable
         // check disclosure node
         Node disclosureNode = getSkinnable().getDisclosureNode();
         if (disclosureNode != null) {
-            boolean disclosureVisible = treeItem != null && ! treeItem.isLeaf();
+            boolean disclosureVisible = isDisclosureNodeVisible();
             disclosureNode.setVisible(disclosureVisible);
 
-            if (! disclosureVisible) {
+            if (!disclosureVisible) {
                 getChildren().remove(disclosureNode);
             } else if (disclosureNode.getParent() == null) {
                 getChildren().add(disclosureNode);
-                disclosureNode.toFront();
             } else {
                 disclosureNode.toBack();
             }

modules/javafx.controls/src/test/java/test/com/sun/javafx/scene/control/infrastructure/VirtualFlowTestUtils.java
@@ -209,11 +209,6 @@ public class VirtualFlowTestUtils {
 
             int _column = column;
 
-            // we need to account for TreeTableView having LabeledText node in the TreeTableRow
-            if (indexedCell instanceof TreeTableRow) {
-                _column++;
-            }
-
             IndexedCell cell = (IndexedCell) indexedCell.getChildrenUnmodifiable().get(_column);
             assertEquals(expected, cell.getText());
             return null;
@@ -336,14 +331,10 @@ public class VirtualFlowTestUtils {
         }
     }
 
-    public static boolean BLOCK_STAGE_LOADER_DISPOSE = false;
-
     public static VirtualFlow<?> getVirtualFlow(Control control) {
         StageLoader sl = null;
-        boolean stageLoaderCreated = false;
         if (control.getScene() == null) {
             sl = new StageLoader(control);
-            stageLoaderCreated = true;
         }
 
         VirtualFlow<?> flow;
@@ -355,7 +346,7 @@ public class VirtualFlowTestUtils {
 
         flow = (VirtualFlow<?>)control.lookup("#virtual-flow");
 
-        if (stageLoaderCreated && sl != null && ! BLOCK_STAGE_LOADER_DISPOSE) {
+        if (sl != null) {
             sl.dispose();
         }
 

modules/javafx.controls/src/test/java/test/javafx/scene/control/TableViewTest.java
@@ -2196,9 +2196,8 @@ public class TableViewTest {
         table.getColumns().addAll(first);
 
         // get the cell at (0,0)
-        VirtualFlowTestUtils.BLOCK_STAGE_LOADER_DISPOSE = true;
+        stageLoader = new StageLoader(table);
         TableCell cell = (TableCell) VirtualFlowTestUtils.getCell(table, 0, 0);
-        VirtualFlowTestUtils.BLOCK_STAGE_LOADER_DISPOSE = false;
         assertTrue(cell.getSkin() instanceof TableCellSkin);
         assertNull(cell.getGraphic());
         assertEquals("John", cell.getText());

commit 1ff2f8838ab1f87477624c2c7bead78f7c334662

8185887: Fix VirtualFlow performance regression in ListView scrolling
The commit 1b12c8a490 introduced two performance issues in VirtualFlow:

1. Calling requestLayout() on all cells in the pile on every layout pass
   This causes unnecessary layout calculations for 50-100 recycled cells
   per frame during scrolling.

2. Using sheetChildren.removeAll(pile) which has O(n*m) time complexity
   This becomes extremely slow with large cell pools.

These changes caused severe scrolling lag in ListView/TableView with
many items (e.g., 1000+ items). The fix removes both problematic code
blocks while preserving the getViewportBreadth() visibility change
needed by TableRowSkinBase.

Tested with ListView containing 1000 items - scrolling is now smooth
again, matching JavaFX 24 performance.

modules/javafx.controls/src/main/java/javafx/scene/control/skin/VirtualFlow.java
@@ -1138,13 +1138,6 @@ public class VirtualFlow<T extends IndexedCell> extends Region {
                     cell.requestLayout();
                 }
             }
-
-            // Also request layout for cells in the pile. As soon as those are reused (and therefore added),
-            // they will do their layout.
-            for (T cell : pile) {
-                cell.requestLayout();
-            }
-
             needsCellsLayout = false;
             // yes, we return here - if needsCellsLayout was set to true, we
             // only did it to do the above - not rerun the entire layout.
@@ -2848,11 +2841,6 @@ public class VirtualFlow<T extends IndexedCell> extends Region {
             T cell = pile.get(i);
             wasFocusOwner = wasFocusOwner || doesCellContainFocus(cell);
             cell.setVisible(false);
-        }
-
-        // Remove all cells that are in the pile and therefore not relevant anymore.
-        if (sheetChildren.size() != cells.size()) {
-            sheetChildren.removeAll(pile);
         }
 
         // Fix for JDK-8095710: Rather than have the cells do weird things with