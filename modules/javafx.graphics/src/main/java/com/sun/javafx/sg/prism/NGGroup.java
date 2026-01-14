65aaaabe788ee140dc67457e9ddfcbd20176993b
Christopher Schnick <crschnick@xpipe.io>
2026-01-14 22:37:24 +0100

    Make fix less invasive

--- modules/javafx.graphics/src/main/java/com/sun/javafx/sg/prism/NGGroup.java
+++ modules/javafx.graphics/src/main/java/com/sun/javafx/sg/prism/NGGroup.java
@@ -246,9 +246,6 @@ public class NGGroup extends NGNode {
             if (renderRoot.hasNext()) {
                 renderRoot.next();
                 startPos = orderedChildren.indexOf(renderRoot.getCurrentNode());
-                if (startPos == -1) {
-                    startPos = 0;
-                }
             } else {
                 g.setRenderRoot(null);
             }
@@ -256,8 +253,7 @@ public class NGGroup extends NGNode {
 
         if (blendMode == Blend.Mode.SRC_OVER ||
                 orderedChildren.size() < 2) {  // Blend modes only work "between" siblings
-
-            for (int i = startPos; i < orderedChildren.size(); i++) {
+            for (int i = startPos == -1 ? 0 : startPos; i < orderedChildren.size(); i++) {
                 NGNode child;
                 try {
                     child = orderedChildren.get(i);
