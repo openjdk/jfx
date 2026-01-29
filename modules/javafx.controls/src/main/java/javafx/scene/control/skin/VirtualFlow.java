f8a60547d6aa1383be9506f029efa0b8165fc37b
Huang Xiao <youngledo@qq.com>
2026-01-30 00:06:11 +0800

    8185887: Make it visible

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/VirtualFlow.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/VirtualFlow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1421,6 +1421,7 @@ public class VirtualFlow<T extends IndexedCell> extends Region {
         if (cell.getParent() == null) {
             sheetChildren.add(cell);
         }
+        cell.setVisible(true);
 
         return cell;
     }
