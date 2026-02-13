3a204d740e40aceedf5562689243a79cff58fbb2
Dmitrii Markov <dmitry.markov@oracle.com>
2026-02-13 11:34:26 +0000

    8376492: NullPointer in ContextMenu sub-menu when graphic and style classes are changed while the menu is open

--- modules/javafx.controls/src/main/java/com/sun/javafx/scene/control/ContextMenuContent.java
+++ modules/javafx.controls/src/main/java/com/sun/javafx/scene/control/ContextMenuContent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1289,8 +1289,10 @@ public class ContextMenuContent extends Region {
                 // into the label, which is what we do below
                 ListChangeListener<String> itemStyleClassListener = c -> {
                     while (c.next()) {
-                        label.getStyleClass().removeAll(c.getRemoved());
-                        label.getStyleClass().addAll(c.getAddedSubList());
+                        if (label != null) {
+                            label.getStyleClass().removeAll(c.getRemoved());
+                            label.getStyleClass().addAll(c.getAddedSubList());
+                        }
                     }
                 };
                 item.getStyleClass().addListener(itemStyleClassListener);
