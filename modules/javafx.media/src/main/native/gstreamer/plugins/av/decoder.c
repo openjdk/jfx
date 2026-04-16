cbef9e26aad5d8c112034c253b5d20b4c999f988
Alexander Matveev <alexander.matveev@oracle.com>
2026-04-15 18:02:45 -0700

    8378510: Provide media support for libavcodec version 62

--- modules/javafx.media/src/main/native/gstreamer/plugins/av/decoder.c
+++ modules/javafx.media/src/main/native/gstreamer/plugins/av/decoder.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -187,8 +187,13 @@ void basedecoder_close_decoder(BaseDecoder *decoder)
 {
     if (decoder->context)
     {
+#if USE_FREE_CONTEXT
+        // avcodec_free_context() will set pointer to NULL
+        avcodec_free_context(&decoder->context);
+#else
         avcodec_close(decoder->context);
         av_free(decoder->context);
+#endif
     }
     decoder->context = NULL;
 
