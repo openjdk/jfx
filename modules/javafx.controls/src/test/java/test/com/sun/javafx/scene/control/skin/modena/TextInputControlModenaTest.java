edf8e8f92e4aa1971d62bbe1d8adab6594fa4ea8
Ziad El Midaoui <elmidaoui.ziad@gmail.com>
2026-06-23 14:18:33 +0100

    Removed Application.setUserAgentStylesheet

--- modules/javafx.controls/src/test/java/test/com/sun/javafx/scene/control/skin/modena/TextInputControlModenaTest.java
+++ modules/javafx.controls/src/test/java/test/com/sun/javafx/scene/control/skin/modena/TextInputControlModenaTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -149,8 +149,6 @@ public class TextInputControlModenaTest {
         Stage stage = new Stage();
 
         try {
-            Application.setUserAgentStylesheet(Application.STYLESHEET_MODENA);
-
             TextInputControl control = type.getDeclaredConstructor().newInstance();
             control.setPromptText("Prompt text");
 
