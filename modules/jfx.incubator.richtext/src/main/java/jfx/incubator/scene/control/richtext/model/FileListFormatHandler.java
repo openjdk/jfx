f2e97b41023b108e31727526314a6ad6347eb353
Andy Goryachev <andy.goryachev@oracle.com>
2026-07-14 10:39:21 -0700

    private

--- modules/jfx.incubator.richtext/src/main/java/jfx/incubator/scene/control/richtext/model/FileListFormatHandler.java
+++ modules/jfx.incubator.richtext/src/main/java/jfx/incubator/scene/control/richtext/model/FileListFormatHandler.java
@@ -50,7 +50,7 @@ public class FileListFormatHandler extends DataFormatHandler {
     /**
      * Constructor.
      */
-    public FileListFormatHandler() {
+    private FileListFormatHandler() {
         super(DataFormat.FILES);
     }
 
