b4fb814f72d97b8cb525dc1439704529409296b9
Eduard Sedov <eduard.sedov@zeiss.com>
2026-08-18 16:57:34 +0200

    8389970: Revert comment

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/LabeledSkinBase.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/LabeledSkinBase.java
@@ -548,15 +548,11 @@ public abstract class LabeledSkinBase<C extends Labeled> extends SkinBase<C> {
             graphicHeight = graphic.getLayoutBounds().getHeight();
         }
 
-        // Have to do this just in case it needs to be recomputed
-        // This is especially important because it also recomputes the 'containsMnemonic' value,
-        // which is used by this method later on.
-
         if (ignoreText) {
             textWidth  = textHeight = 0;
             text.setText("");
         } else {
-            updateDisplayedText(w, h);
+            updateDisplayedText(w, h); // Have to do this just in case it needs to be recomputed
             textWidth  = snapSizeX(Math.min(text.getLayoutBounds().getWidth(),  wrapWidth));
             textHeight = snapSizeY(Math.min(text.getLayoutBounds().getHeight(), wrapHeight));
         }
