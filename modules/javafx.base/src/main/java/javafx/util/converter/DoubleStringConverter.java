f844fd0ce3e81149d37c9c48c039b538b7b62ed1
Nir Lisker <nlisker@openjdk.org>
2026-08-18 15:24:57 +0000

    8250802: Refactor StringConverter and its subclasses
    Reviewed-by: angorya, kcr, jhendrikx

--- modules/javafx.base/src/main/java/javafx/util/converter/DoubleStringConverter.java
+++ modules/javafx.base/src/main/java/javafx/util/converter/DoubleStringConverter.java
@@ -25,44 +25,28 @@
 
 package javafx.util.converter;
 
-import javafx.util.StringConverter;
+/// A `StringConverter` implementation for `Double` (and `double`) values. Formatting is done by [Double#toString()] for
+/// non-`null` values, and parsing with [Double#valueOf(String)] for non-`null` non-empty strings.
+///
+/// @since JavaFX 2.1
+public class DoubleStringConverter extends BaseStringConverter<Double> {
 
-/**
- * <p>{@link StringConverter} implementation for {@link Double}
- * (and double primitive) values.</p>
- * @since JavaFX 2.1
- */
-public class DoubleStringConverter extends StringConverter<Double> {
-
-    /**
-     * Creates a default {@code DoubleStringConverter}.
-     */
+    /// Creates a default `DoubleStringConverter`.
     public DoubleStringConverter() {
     }
 
-    /** {@inheritDoc} */
-    @Override public Double fromString(String value) {
-        // If the specified value is null or zero-length, return null
-        if (value == null) {
-            return null;
-        }
-
-        value = value.trim();
-
-        if (value.length() < 1) {
-            return null;
-        }
-
-        return Double.valueOf(value);
+    @Override
+    public Double fromString(String value) {
+        return super.fromString(value);
     }
 
-    /** {@inheritDoc} */
-    @Override public String toString(Double value) {
-        // If the specified value is null, return a zero-length String
-        if (value == null) {
-            return "";
-        }
+    @Override
+    Double fromNonEmptyString(String string) {
+        return Double.valueOf(string);
+    }
 
-        return Double.toString(value.doubleValue());
+    @Override
+    public String toString(Double value) {
+        return super.toString(value);
     }
 }
