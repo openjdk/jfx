bbef1845b906df21cd41a44b470aa4fbbd59475d
Andy Goryachev <andy.goryachev@oracle.com>
2026-08-24 08:00:45 -0700

    rm export in swing

--- modules/javafx.swing/src/main/java/module-info.java
+++ modules/javafx.swing/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -42,7 +42,4 @@ module javafx.swing {
     requires transitive javafx.graphics;
 
     exports javafx.embed.swing;
-
-    exports com.sun.javafx.embed.swing to
-        javafx.graphics;
 }
