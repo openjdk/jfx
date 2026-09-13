242944e4945d8cba096d81125913f7d67612870e
Marius Hanl <mariushanl@web.de>
2026-09-13 14:18:25 +0200

    revert back

--- modules/javafx.base/src/main/java/com/sun/javafx/PlatformUtil.java
+++ modules/javafx.base/src/main/java/com/sun/javafx/PlatformUtil.java
@@ -36,12 +36,6 @@ import java.util.Properties;
 
 public class PlatformUtil {
 
-    public static final String MAC = "Mac";
-    public static final String WINDOWS = "Win";
-    public static final String GTK = "Gtk";
-    public static final String IOS = "Ios";
-    public static final String HEADLESS = "Headless";
-
     private static final String os = System.getProperty("os.name");
     private static final boolean embedded;
     private static final String embeddedType;
@@ -59,35 +53,34 @@ public class PlatformUtil {
         useEGL = Boolean.getBoolean("use.egl");
     }
 
-    private static final boolean IS_ANDROID = "android".equals(javafxPlatform) || "Dalvik".equals(System.getProperty("java.vm.name"));
-    private static final boolean IS_WINDOWS = os.startsWith("Windows");
-    private static final boolean IS_MAC = os.startsWith("Mac");
-    private static final boolean IS_LINUX = os.startsWith("Linux") && !IS_ANDROID;
-    private static final boolean IS_SOLARIS = os.startsWith("SunOS");
-    private static final boolean IS_IOS = os.startsWith("iOS");
-    private static final boolean IS_STATIC_BUILD = "Substrate VM".equals(System.getProperty("java.vm.name"));
-    private static final boolean IS_HEADLESS = "headless".equals(embeddedType);
-    private static final String GLASS_PLATFORM = determineGlassPlatform();
+    private static final boolean ANDROID = "android".equals(javafxPlatform) || "Dalvik".equals(System.getProperty("java.vm.name"));
+    private static final boolean WINDOWS = os.startsWith("Windows");
+    private static final boolean MAC = os.startsWith("Mac");
+    private static final boolean LINUX = os.startsWith("Linux") && !ANDROID;
+    private static final boolean SOLARIS = os.startsWith("SunOS");
+    private static final boolean IOS = os.startsWith("iOS");
+    private static final boolean STATIC_BUILD = "Substrate VM".equals(System.getProperty("java.vm.name"));
+    private static final boolean HEADLESS = "headless".equals(embeddedType);
 
     /**
      * Returns true if the operating system is a form of Windows.
      */
     public static boolean isWindows(){
-        return IS_WINDOWS;
+        return WINDOWS;
     }
 
     /**
      * Returns true if the operating system is a form of Mac OS.
      */
     public static boolean isMac(){
-        return IS_MAC;
+        return MAC;
     }
 
     /**
      * Returns true if the operating system is a form of Linux.
      */
     public static boolean isLinux(){
-        return IS_LINUX;
+        return LINUX;
     }
 
     public static boolean useEGL() {
@@ -98,7 +91,7 @@ public class PlatformUtil {
      * Returns true if the operating system is a form of Linux or Solaris
      */
     public static boolean isUnix(){
-        return IS_LINUX || IS_SOLARIS;
+        return LINUX || SOLARIS;
     }
 
     /**
@@ -119,61 +112,25 @@ public class PlatformUtil {
      * Returns true if the Headless glass platform is selected
      */
     public static boolean isHeadless(){
-        return IS_HEADLESS;
+        return HEADLESS;
     }
 
     /**
      * Returns true if the operating system is iOS
      */
     public static boolean isIOS(){
-        return IS_IOS;
+        return IOS;
     }
 
     public static boolean isAndroid() {
-        return IS_ANDROID;
+        return ANDROID;
     }
 
     /**
      * Returns true if the current runtime is a statically linked image
      */
     public static boolean isStaticBuild(){
-        return IS_STATIC_BUILD;
-    }
-
-    /**
-     * Returns the platform name as String.
-     */
-    public static String getPlatform() {
-        return GLASS_PLATFORM;
-    }
-
-    private static String determineGlassPlatform() {
-        // Provide for a runtime override, allowing EGL for example
-        String userPlatform = System.getProperty("glass.platform");
-        if (userPlatform != null) {
-            return switch (userPlatform) {
-                case "macosx" -> MAC;
-                case "windows" -> WINDOWS;
-                case "linux", "gtk" -> GTK;
-                case "ios" -> IOS;
-                case "headless" -> HEADLESS;
-                default -> userPlatform;
-            };
-        }
-
-        if (IS_MAC) {
-            return MAC;
-        }
-        if (IS_WINDOWS) {
-            return WINDOWS;
-        }
-        if (IS_LINUX) {
-            return GTK;
-        }
-        if (IS_IOS) {
-            return IOS;
-        }
-        return null;
+        return STATIC_BUILD;
     }
 
     private static void loadPropertiesFromFile(final File file) {
--- modules/javafx.graphics/src/main/java/com/sun/glass/ui/View.java
+++ modules/javafx.graphics/src/main/java/com/sun/glass/ui/View.java
@@ -26,7 +26,6 @@ package com.sun.glass.ui;
 
 import com.sun.glass.events.MouseEvent;
 import com.sun.glass.events.ViewEvent;
-import com.sun.javafx.PlatformUtil;
 import com.sun.javafx.tk.HeaderAreaType;
 import java.lang.annotation.Native;
 import java.lang.ref.WeakReference;
@@ -50,12 +49,12 @@ public abstract class View {
 
         /* By default accessibility is enabled for Mac 10.9 or greater and Windows 7 or greater. */
         try {
-            String platform = PlatformUtil.getPlatform();
+            String platform = Platform.determinePlatform();
             String major = System.getProperty("os.version").replaceFirst("(\\d+)\\.\\d+.*", "$1");
             String minor = System.getProperty("os.version").replaceFirst("\\d+\\.(\\d+).*", "$1");
             int v = Integer.parseInt(major) * 100 + Integer.parseInt(minor);
-            return (platform.equals(PlatformUtil.MAC) && v >= 1009) ||
-                   (platform.equals(PlatformUtil.WINDOWS) && v >= 601);
+            return (platform.equals(Platform.MAC) && v >= 1009) ||
+                   (platform.equals(Platform.WINDOWS) && v >= 601);
         } catch (Exception e) {
             return false;
         }
